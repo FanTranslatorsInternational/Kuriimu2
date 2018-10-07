228542613f1aeb42cca0e3e00f5c09a689e495ce
onepiecefreak3 <alberteinstein1980@gmx.de>
2018-10-07 22:53:30 +0200

    Updated XtsStream;

--- src/Komponent/Cryptography/AES/XtsStream.cs
+++ src/Komponent/Cryptography/AES/XtsStream.cs
@@ -13,37 +13,31 @@ namespace Komponent.Cryptography.AES
     {
         public int SectorSize { get; }
 
-        public int CurrentSector => CalculateCurrentSector((int)Position);
+        private Stream _stream;
+        private Xts _xts;
+        private long _sectorsBetweenLengthPosition = 0;
+        private long _sectorPosition = 0;
+        private long _bytesIntoSector = 0;
+
+        private long _offset;
+        private long _length;
+        private bool _fixedLength = false;
 
         public override int BlockSize => 128;
-
-        public override int BlockSizeBytes => BlockSize / 8;
+        public override int BlockSizeBytes => 16;
+        public override bool CanRead => true;
+        public override bool CanSeek => true;
+        public override bool CanWrite => true;
 
         public override List<byte[]> Keys { get; }
-
         public override int KeySize => Keys?[0]?.Length ?? 0;
-
         public override byte[] IV => throw new NotImplementedException();
-
-        public override bool CanRead => _stream.CanRead;
-
-        public override bool CanSeek => _stream.CanSeek;
-
-        public override bool CanWrite => _stream.CanWrite;
-
-        private long _offset = 0;
-        private long _length = 0;
         public override long Length => _length;
-
         public override long Position { get => _stream.Position - _offset; set => Seek(value, SeekOrigin.Begin); }
+        private long TotalSectors => GetSectorCount(Length);
 
-        public int TotalSectors { get => CalculateSectorCount((int)Length); }
-        private int CalculateSectorCount(int input) => (int)Math.Ceiling((double)input / SectorSize);
-        private int CalculateCurrentSector(int input) => (int)Math.Floor((double)input / SectorSize);
-
-        private Stream _stream;
-        private Xts _xtsContext;
-        private byte[] _finalSector;
+        private long GetSectorCount(long input) => (long)Math.Ceiling((double)input / SectorSize);
+        private long GetCurrentSector(long input) => input / SectorSize;
 
         public XtsStream(byte[] input, long offset, long length, byte[] key1, bool nintendo_tweak = false) : this(input, offset, length, key1, null, 512, nintendo_tweak) { }
 
@@ -62,9 +56,9 @@ namespace Komponent.Cryptography.AES
         public XtsStream(Stream input, long offset, long length, byte[] key1, byte[] key2, int sectorSize, bool nintendo_tweak = false)
         {
             _stream = input;
-            _finalSector = new byte[sectorSize];
-            _length = length;
             _offset = offset;
+            _length = length;
+            _fixedLength = true;
 
             SectorSize = sectorSize;
 
@@ -77,11 +71,11 @@ namespace Komponent.Cryptography.AES
             {
                 if (key1.Length == 128 / 8 && key2.Length == 128 / 8)
                 {
-                    _xtsContext = XtsAes128.Create(key1, key2, nintendo_tweak);
+                    _xts = XtsAes128.Create(key1, key2, nintendo_tweak);
                 }
                 else if (key1.Length == 256 / 8 && key2.Length == 256 / 8)
                 {
-                    _xtsContext = XtsAes256.Create(key1, key2, nintendo_tweak);
+                    _xts = XtsAes256.Create(key1, key2, nintendo_tweak);
                 }
                 else
                     throw new InvalidDataException("Key1 or Key2 have invalid size.");
@@ -90,17 +84,15 @@ namespace Komponent.Cryptography.AES
             {
                 if (key1.Length == 256 / 8)
                 {
-                    _xtsContext = XtsAes128.Create(key1, nintendo_tweak);
+                    _xts = XtsAes128.Create(key1, nintendo_tweak);
                 }
                 else if (key1.Length == 512 / 8)
                 {
-                    _xtsContext = XtsAes256.Create(key1, nintendo_tweak);
+                    _xts = XtsAes256.Create(key1, nintendo_tweak);
                 }
                 else
                     throw new InvalidDataException("Key1 has invalid size.");
             }
-
-            Position = input.Position;
         }
 
         public XtsStream(byte[] input, byte[] key1, bool nintendo_tweak = false) : this(input, key1, null, 512, nintendo_tweak) { }
@@ -120,8 +112,9 @@ namespace Komponent.Cryptography.AES
         public XtsStream(Stream input, byte[] key1, byte[] key2, int sectorSize, bool nintendo_tweak = false)
         {
             _stream = input;
-            _finalSector = new byte[sectorSize];
-            _length = _stream.Length;
+            _offset = 0;
+            _length = input.Length;
+            _fixedLength = false;
 
             SectorSize = sectorSize;
 
@@ -134,11 +127,11 @@ namespace Komponent.Cryptography.AES
             {
                 if (key1.Length == 128 / 8 && key2.Length == 128 / 8)
                 {
-                    _xtsContext = XtsAes128.Create(key1, key2, nintendo_tweak);
+                    _xts = XtsAes128.Create(key1, key2, nintendo_tweak);
                 }
                 else if (key1.Length == 256 / 8 && key2.Length == 256 / 8)
                 {
-                    _xtsContext = XtsAes256.Create(key1, key2, nintendo_tweak);
+                    _xts = XtsAes256.Create(key1, key2, nintendo_tweak);
                 }
                 else
                     throw new InvalidDataException("Key1 or Key2 have invalid size.");
@@ -147,17 +140,15 @@ namespace Komponent.Cryptography.AES
             {
                 if (key1.Length == 256 / 8)
                 {
-                    _xtsContext = XtsAes128.Create(key1, nintendo_tweak);
+                    _xts = XtsAes128.Create(key1, nintendo_tweak);
                 }
                 else if (key1.Length == 512 / 8)
                 {
-                    _xtsContext = XtsAes256.Create(key1, nintendo_tweak);
+                    _xts = XtsAes256.Create(key1, nintendo_tweak);
                 }
                 else
                     throw new InvalidDataException("Key1 has invalid size.");
             }
-
-            Position = input.Position;
         }
 
         public override void Flush()
@@ -166,57 +157,48 @@ namespace Komponent.Cryptography.AES
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            ValidateInput(buffer, offset, count);
+            ValidateRead(buffer, offset, count);
 
-            var decrypted = ReadDecrypted(count);
+            return ReadDecrypted(buffer, offset, count);
+        }
 
-            long offsetIntoSector = 0;
-            if (Position % SectorSize > 0)
-                offsetIntoSector = Position % SectorSize;
-            Array.Copy(decrypted.Skip((int)offsetIntoSector).Take(count).ToArray(), 0, buffer, offset, count);
-            Position += count;
+        private void ValidateRead(byte[] buffer, int offset, int count)
+        {
+            if (!CanRead)
+                throw new NotSupportedException("Reading is not supported.");
+            if (offset < 0 || count < 0)
+                throw new ArgumentOutOfRangeException("Offset or count can't be negative.");
+            if (offset + count > buffer.Length)
+                throw new InvalidDataException("Buffer too short.");
+        }
+
+        private int ReadDecrypted(byte[] buffer, int offset, int count)
+        {
+            var originalPosition = Position;
+
+            count = (int)Math.Min(count, Length - Position);
+            var alignedCount = (int)GetSectorCount(_bytesIntoSector + count) * SectorSize;
+
+            if (alignedCount == 0) return alignedCount;
+
+            var decData = Decrypt(_sectorPosition, alignedCount);
+
+            Array.Copy(decData, _bytesIntoSector, buffer, offset, count);
+
+            Seek(originalPosition + count, SeekOrigin.Begin);
 
             return count;
         }
 
-        private void ValidateInput(byte[] buffer, int offset, int count)
+        private byte[] Decrypt(long begin, int alignedCount)
         {
-            if (offset < 0 || count < 0)
-                throw new InvalidDataException("Offset and count can't be negative.");
-            if (offset + count > buffer.Length)
-                throw new InvalidDataException("Buffer too short.");
-            if (count > Length - Position)
-                throw new InvalidDataException($"Can't read {count} bytes from position {Position} in stream with length {Length}.");
-        }
+            _stream.Position = begin + _offset;
+            var readData = new byte[alignedCount];
+            _stream.Read(readData, 0, alignedCount);
 
-        private byte[] ReadDecrypted(int count)
-        {
-            long offsetIntoSector = 0;
-            if (Position % SectorSize > 0)
-                offsetIntoSector = Position % SectorSize;
+            var decData = DecryptSectors(readData, (ulong)GetCurrentSector(begin));
 
-            var sectorsToRead = CalculateSectorCount((int)offsetIntoSector + count);
-            var sectorPaddedCount = sectorsToRead * SectorSize;
-
-            if (Length <= 0 || count <= 0)
-                return new byte[sectorPaddedCount];
-
-            var originalPosition = Position;
-            Position -= offsetIntoSector;
-
-            var minimalDecryptableSize = Math.Min(CalculateSectorCount((int)Length) * SectorSize, sectorPaddedCount);
-            var bytesRead = new byte[minimalDecryptableSize];
-            _stream.Read(bytesRead, 0, minimalDecryptableSize);
-            Position = originalPosition;
-
-            if (CalculateSectorCount((int)Position + count) >= TotalSectors)
-                Array.Copy(_finalSector, 0, bytesRead, bytesRead.Length - BlockSizeBytes, _finalSector.Length);
-
-            var decrypted = DecryptSectors(bytesRead, (ulong)CurrentSector);
-            var result = new byte[sectorPaddedCount];
-            Array.Copy(decrypted, 0, result, 0, minimalDecryptableSize);
-
-            return result;
+            return decData;
         }
 
         private byte[] DecryptSectors(byte[] sectors, ulong sectorId)
@@ -225,15 +207,68 @@ namespace Komponent.Cryptography.AES
                 throw new InvalidDataException($"{nameof(DecryptSectors)} needs sector padded amount of data.");
 
             byte[] result = new byte[sectors.Length];
-            for (int i = 0; i < CalculateSectorCount(sectors.Length); i++)
-                _xtsContext.CreateDecryptor().TransformBlock(sectors, i * SectorSize, SectorSize, result, i * SectorSize, sectorId++);
+            for (int i = 0; i < GetSectorCount(sectors.Length); i++)
+                _xts.CreateDecryptor().TransformBlock(sectors, i * SectorSize, SectorSize, result, i * SectorSize, sectorId++);
 
             return result;
         }
 
         public override long Seek(long offset, SeekOrigin origin)
         {
-            return _stream.Seek(offset + _offset, origin);
+            ValidateSeek(offset, origin);
+
+            var result = _stream.Seek(offset + _offset, origin);
+
+            UpdateSeekable();
+
+            return result;
+        }
+
+        private void ValidateSeek(long offset, SeekOrigin origin)
+        {
+            if (!CanSeek)
+                throw new NotSupportedException("Seek is not supported.");
+            if (_fixedLength)
+                switch (origin)
+                {
+                    case SeekOrigin.Begin:
+                        if (_offset + offset >= Length)
+                            throw new InvalidDataException("Position can't be set outside set stream length.");
+                        break;
+                    case SeekOrigin.Current:
+                        if (_offset + Position + offset >= Length)
+                            throw new InvalidDataException("Position can't be set outside set stream length.");
+                        break;
+                    case SeekOrigin.End:
+                        if (Length + offset >= Length)
+                            throw new InvalidDataException("Position can't be set outside set stream length.");
+                        break;
+                }
+        }
+
+        private void UpdateSeekable()
+        {
+            _sectorsBetweenLengthPosition = GetSectorsBetween(Position);
+            _sectorPosition = Position / SectorSize * SectorSize;
+            _bytesIntoSector = Position % SectorSize;
+        }
+
+        private long GetSectorsBetween(long position)
+        {
+            if (Length <= 0)
+                return GetCurrentSector(position);
+
+            var offsetBlock = GetCurrentSector(position);
+            var lengthBlock = GetCurrentSector(Length);
+
+            if (offsetBlock == lengthBlock)
+                return 0;
+
+            var lengthRest = Length % SectorSize;
+            if (lengthRest == 0 && position > Length)
+                return Math.Max(offsetBlock, lengthBlock) - Math.Min(offsetBlock, lengthBlock);
+
+            return Math.Max(offsetBlock, lengthBlock) - (Math.Min(offsetBlock, lengthBlock) + 1);
         }
 
         public override void SetLength(long value)
@@ -243,38 +278,64 @@ namespace Komponent.Cryptography.AES
 
         public override void Write(byte[] buffer, int offset, int count)
         {
-            long offsetIntoSector = 0;
-            if (Position % SectorSize > 0)
-                offsetIntoSector = Position % SectorSize;
+            ValidateWrite(buffer, offset, count);
 
-            var sectorToWrite = CalculateSectorCount((int)offsetIntoSector + count);
-            var sectorPaddedCount = sectorToWrite * SectorSize;
+            if (_fixedLength && Position + count >= Length)
+                count = (int)(Length - Position);
+            if (count == 0) return;
+            var readBuffer = GetInitializedReadBuffer(count, out var dataStart);
 
-            byte[] decrypted = ReadDecrypted(count);
-            Array.Copy(buffer, offset, decrypted, offsetIntoSector, count);
+            PeakOverlappingData(readBuffer, (int)dataStart, count);
 
-            if (CalculateSectorCount((int)Length) < CalculateSectorCount((int)Position) - 1)
-            {
-                var betweenSectors = CalculateSectorCount((int)Position) - CalculateSectorCount((int)Length) - 1;
-                var newDecrypted = new byte[betweenSectors * SectorSize + decrypted.Length];
-                Array.Copy(decrypted, 0, newDecrypted, betweenSectors * SectorSize, decrypted.Length);
-                decrypted = newDecrypted;
-            }
+            Array.Copy(buffer, offset, readBuffer, dataStart, count);
 
             var originalPosition = Position;
-            if (CalculateSectorCount((int)Length) < CalculateSectorCount((int)Position) - 1)
-                Position -= (CalculateSectorCount((int)Position) - CalculateSectorCount((int)Length) - 1) * SectorSize;
-            Position -= offsetIntoSector;
+            Position -= dataStart;
 
-            var encrypted = EncryptSectors(decrypted, (ulong)CalculateCurrentSector((int)Position));
+            var encBuffer = EncryptSectors(readBuffer, (ulong)GetCurrentSector(Position));
+            _stream.Write(encBuffer, 0, encBuffer.Length);
 
-            if (CalculateSectorCount((int)originalPosition + count) >= TotalSectors)
-                Array.Copy(encrypted.Skip(encrypted.Length - SectorSize).Take(SectorSize).ToArray(), _finalSector, SectorSize);
+            if (originalPosition + count > _length)
+                _length = originalPosition + count;
 
-            _stream.Write(encrypted, 0, encrypted.Length);
+            Seek(originalPosition + count, SeekOrigin.Begin);
+        }
 
-            _length = Math.Max(_length, Position + count);
-            Position = originalPosition + count;
+        private void ValidateWrite(byte[] buffer, int offset, int count)
+        {
+            if (!CanWrite)
+                throw new NotSupportedException("Write is not supported");
+            if (offset < 0 || count < 0)
+                throw new ArgumentOutOfRangeException("Offset or count can't be negative.");
+            if (offset + count > buffer.Length)
+                throw new InvalidDataException("Buffer too short.");
+        }
+
+        private byte[] GetInitializedReadBuffer(int count, out long dataStart)
+        {
+            dataStart = _bytesIntoSector;
+
+            var bufferSectors = GetSectorCount(_bytesIntoSector + count);
+            if (Position >= Length)
+            {
+                bufferSectors += _sectorsBetweenLengthPosition;
+                dataStart += _sectorsBetweenLengthPosition * BlockSizeBytes;
+            }
+
+            var bufferLength = bufferSectors * SectorSize;
+
+            return new byte[bufferLength];
+        }
+
+        private void PeakOverlappingData(byte[] buffer, int offset, int count)
+        {
+            if (Position - offset < Length)
+            {
+                long originalPosition = Position;
+                var readBuffer = Decrypt(Position - offset, (int)GetSectorCount(Math.Min(Length - (Position - offset), count)) * SectorSize);
+                Array.Copy(readBuffer, 0, buffer, 0, readBuffer.Length);
+                Position = originalPosition;
+            }
         }
 
         private byte[] EncryptSectors(byte[] sectors, ulong sectorId)
@@ -283,8 +344,8 @@ namespace Komponent.Cryptography.AES
                 throw new InvalidDataException($"{nameof(EncryptSectors)} needs sector padded amount of data.");
 
             byte[] result = new byte[sectors.Length];
-            for (int i = 0; i < CalculateSectorCount(sectors.Length); i++)
-                _xtsContext.CreateEncryptor().TransformBlock(sectors, i * SectorSize, SectorSize, result, i * SectorSize, sectorId++);
+            for (int i = 0; i < GetSectorCount(sectors.Length); i++)
+                _xts.CreateEncryptor().TransformBlock(sectors, i * SectorSize, SectorSize, result, i * SectorSize, sectorId++);
 
             return result;
         }
