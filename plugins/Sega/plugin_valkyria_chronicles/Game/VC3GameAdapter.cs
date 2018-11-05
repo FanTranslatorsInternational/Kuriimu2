0fff6752c1bbaab7dbe3fe509b9b411ca5aefe8e
IcySon55 <icyson55@gmail.com>
2018-11-05 09:14:47 -0500

    Implemented an image slicing format to make it easier to draw slices. Implemented a paired file class to use a Bitmap and slice file together. Drew a simple text balloon.

--- plugins/Sega/plugin_valkyria_chronicles/Game/VC3GameAdapter.cs
+++ plugins/Sega/plugin_valkyria_chronicles/Game/VC3GameAdapter.cs
@@ -7,6 +7,7 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Komponent.IO;
 using Kontract.Attributes;
 using Kontract.Interfaces;
 using plugin_valkyria_chronicles.SFNT;
@@ -61,38 +62,82 @@ namespace plugin_valkyria_chronicles.Game
             if (!Font.Characters.Any()) return null;
 
             // Paths
-            var bgPath = Path.Combine(BasePath, "BG01.HTX.png");
-            if (!File.Exists(bgPath)) return null;
+            var bg01Path = Path.Combine(BasePath, "BG01.HTX.png");
 
             // Setup
-            var bg = new Bitmap(bgPath);
-            var img = new Bitmap(bg.Width, bg.Height, PixelFormat.Format32bppArgb);
+            var BG01 = new Bitmap(bg01Path);
+
+            var img = new Bitmap(BG01.Width, BG01.Height, PixelFormat.Format32bppArgb);
 
             // Colors
             var cDefault = Color.FromArgb(90, 47, 22);
             var cNumber = Color.FromArgb(64, 42, 19);
 
-            var textTop = img.Height / 2 + img.Height / 2 / 2;
-
             var gfx = Graphics.FromImage(img);
-            gfx.CompositingMode = CompositingMode.SourceOver;
-            gfx.SmoothingMode = SmoothingMode.HighQuality;
-            gfx.InterpolationMode = InterpolationMode.Bicubic;
-            gfx.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            gfx.SmoothingMode = SmoothingMode.None;
+            gfx.InterpolationMode = InterpolationMode.NearestNeighbor;
+            gfx.PixelOffsetMode = PixelOffsetMode.None;
+
+            // Load Files
+            var balloonLokiHard = new Dicer(Path.Combine(BasePath, "BALLOON_LOKI_HARD.HTX.png"));
+
+            // Scene Variables
+            var scene = 1; // Make an enum
+            int x = 0, y = 0, xR = 0;
+            float scaleX = 0.75f, scaleY = 0.75f;
+            var lineHeight = Font.Characters.First().GlyphHeight;
 
             // Begin Drawing
-            gfx.DrawImage(bg, 0, 0);
-            gfx.FillRectangle(new SolidBrush(Color.White), new Rectangle(16, textTop, 256, 48));
+            switch (scene)
+            {
+                case 1:
+                    Font.SetColor(cDefault);
 
-            Font.SetColor(cDefault);
-            var lineHeight = Font.Characters.First().GlyphHeight;
-            int x = 20, y = textTop + 4;
+                    gfx.DrawImage(BG01, 0, 0);
+
+                    var grid = balloonLokiHard.Slice.UserInt1;
+                    var balloonStartX = 16;
+                    balloonLokiHard.DrawSlice("BigCorner", gfx, balloonStartX, balloonStartX);
+                    balloonLokiHard.DrawSlice("BigCorner", gfx, balloonStartX + grid * 14, balloonStartX, true);
+
+                    balloonLokiHard.DrawSlice("Edge", gfx, balloonStartX + grid * 2, balloonStartX, false, true);
+                    balloonLokiHard.DrawSlice("Edge", gfx, balloonStartX + grid * 2, balloonStartX + grid * 5);
+                    balloonLokiHard.DrawSlice("Edge", gfx, balloonStartX + grid * 6, balloonStartX, false, true);
+                    balloonLokiHard.DrawSlice("Edge", gfx, balloonStartX + grid * 6, balloonStartX + grid * 5);
+                    balloonLokiHard.DrawSlice("Edge", gfx, balloonStartX + grid * 10, balloonStartX, false, true);
+                    balloonLokiHard.DrawSlice("Edge", gfx, balloonStartX + grid * 10, balloonStartX + grid * 5);
+
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 2, balloonStartX + grid);
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 2, balloonStartX + grid * 2);
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 2, balloonStartX + grid * 3);
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 2, balloonStartX + grid * 4);
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 6, balloonStartX + grid);
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 6, balloonStartX + grid * 2);
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 6, balloonStartX + grid * 3);
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 6, balloonStartX + grid * 4);
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 10, balloonStartX + grid);
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 10, balloonStartX + grid * 2);
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 10, balloonStartX + grid * 3);
+                    balloonLokiHard.DrawSlice("Fill", gfx, balloonStartX + grid * 10, balloonStartX + grid * 4);
+
+                    // Set text box
+                    x = xR = balloonStartX + grid * 2;
+                    y = balloonStartX + grid + 1;
+                    scaleX = scaleY = 0.875f;
+
+                    break;
+            }
+            
+            gfx.SmoothingMode = SmoothingMode.HighQuality;
+            gfx.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            gfx.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
             foreach (var c in entry.EditedText)
             {
                 // Handle line break
                 if (c == '\n')
                 {
-                    x = 20;
+                    x = xR;
                     y += lineHeight;
                     continue;
                 }
@@ -102,11 +147,124 @@ namespace plugin_valkyria_chronicles.Game
                 else
                     Font.SetColor(cDefault);
 
-                Font.Draw(c, gfx, x, y, 1.0f, 1.0f);
-                x += Font.GetCharWidthInfo(c).GlyphWidth;
+                Font.Draw(c, gfx, x, y, scaleX, scaleY);
+                x += (int)(Font.GetCharWidthInfo(c).GlyphWidth * scaleX);
             }
 
             return img;
+        }
+    }
+
+    internal sealed class Dicer
+    {
+        public Bitmap Bitmap { get; }
+        public SLICE Slice { get; }
+
+        public Dicer(string imagePath)
+        {
+            Bitmap = new Bitmap(imagePath);
+            Slice = new SLICE(File.OpenRead(Path.ChangeExtension(imagePath, ".slice")));
+        }
+
+        public void DrawSlice(string sliceName, Graphics gfx, int x, int y, bool flipX = false, bool flipY = false)
+        {
+            var slice = Slice.Slices.FirstOrDefault(s => s.Name == sliceName);
+            if (slice == null) return;
+            gfx.DrawImage(Bitmap, new[] {
+                    new Point(x + (flipX ? slice.Width : 0), y + (flipY ? slice.Height : 0)),
+                    new Point(x + (flipX ? 0 : slice.Width), y + (flipY ? slice.Height : 0)),
+                    new Point(x + (flipX ? slice.Width : 0), y + (flipY ? 0 : slice.Height))
+                },
+                new Rectangle(slice.X - (flipX ? 1 : 0), slice.Y, slice.Width, slice.Height),
+                GraphicsUnit.Pixel
+            );
+        }
+    }
+
+    /// <summary>
+    /// The SLICE format class handles image slicing data in a simple format.
+    /// </summary>
+    internal sealed class SLICE
+    {
+        private FileHeader Header { get; }
+
+        public List<Slice> Slices { get; set; }
+
+        public int UserInt1
+        {
+            get => Header.UserInt1;
+            set => Header.UserInt1 = value;
+        }
+
+        public int UserInt2
+        {
+            get => Header.UserInt2;
+            set => Header.UserInt2 = value;
+        }
+
+        public SLICE()
+        {
+            Header = new FileHeader();
+            Slices = new List<Slice>();
+        }
+
+        public SLICE(Stream input)
+        {
+            using (var br = new BinaryReaderX(input))
+            {
+                Header = br.ReadStruct<FileHeader>();
+                if (Header.FileSize != br.BaseStream.Length)
+                    return;
+
+                Slices = new List<Slice>();
+                for (var i = 0; i < Header.SliceCount; i++)
+                {
+                    Slices.Add(new Slice
+                    {
+                        Name = br.ReadString(Header.NameSize).Trim('\0'),
+                        X = br.ReadInt32(),
+                        Y = br.ReadInt32(),
+                        DX = br.ReadInt32(),
+                        DY = br.ReadInt32()
+                    });
+                }
+            }
+        }
+
+        public void Save(Stream output)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal class FileHeader
+        {
+            [FixedLength(8)]
+            public string Magic = "SLICEv1";
+            [FixedLength(4)]
+            public string FTI = "FTI";
+            public int FileSize = 0;
+            public int NameSize = 0x10; // Aligned to 8 bytes including null byte; Applies to all slices.
+            public int SliceCount = 0;
+            public int UserInt1 = 0;
+            public int UserInt2 = 0;
+        }
+
+        internal class Slice
+        {
+            public string Name; // Slice name, no spaces.
+            public int X;
+            public int Y;
+            public int DX;
+            public int DY;
+
+            public int Width => DX - X;
+            public int Height => DY - Y;
+
+            public Point XY => new Point(X, Y);
+            public Point DXDY => new Point(DX, DY);
+            public Rectangle Rect => new Rectangle(X, Y, Width, Height);
+
+            public override string ToString() => Name.Trim('\0');
         }
     }
 }
