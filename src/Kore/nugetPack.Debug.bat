d9300c66cc772c167c8582214de46bf58d7bef04
IcySon55 <icyson55@gmail.com>
2018-12-16 03:18:39 -0500

    Removed nuget from the Kore directory.

--- src/Kore/nugetPack.Debug.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-nuget.exe pack Kore.csproj -IncludeReferencedProjects -Properties Configuration=Debug
-copy /y *.nupkg nuget\*
-del *.nupkg
-
-pause
--- src/Kore/nugetPack.Release.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-nuget.exe pack Kore.csproj -IncludeReferencedProjects -Properties Configuration=Release
-copy /y *.nupkg nuget\*
-del *.nupkg
-
-pause
