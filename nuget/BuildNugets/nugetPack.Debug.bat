c9dd57daad0a955ae41e991581656ab93f3837da
IcySon55 <icyson55@gmail.com>
2018-12-16 03:29:45 -0500

    Removed the directory changing as it is no longer needed to build the nugets successfully.

--- nuget/BuildNugets/nugetPack.Debug.bat
+++ nuget/BuildNugets/nugetPack.Debug.bat
@@ -1,8 +1,6 @@
 @echo off
 
-cd ..\..\
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" Kuriimu2.sln /p:Configuration=Debug /p:WarningLevel=0
-cd nuget\Build
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\..\Kuriimu2.sln /p:Configuration=Debug /p:WarningLevel=0
 nuget.exe pack ..\..\src\Kontract\Kontract.csproj -Properties Configuration=Debug
 nuget.exe pack ..\..\src\Komponent\Komponent.csproj -Properties Configuration=Debug
 nuget.exe pack ..\..\src\Kanvas\Kanvas.csproj -Properties Configuration=Debug
