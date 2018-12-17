845c6e591e4779220c2ee9a56ccc65a1a9fd555e
Kirito <alberteinstein1980@gmx.de>
2018-12-17 14:25:30 +0100

    Added 3 more tests;

--- src/KoreUnitTests/KoreTests.cs
+++ src/KoreUnitTests/KoreTests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading;
+using Kontract.Interfaces.Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace KoreUnitTests
 {
@@ -24,6 +27,35 @@ namespace KoreUnitTests
 
             Assert.IsTrue(closeResult);
             Assert.IsFalse(kore.OpenFiles.Contains(kfi));
+        }
+
+        [TestMethod]
+        public void GetAdapters()
+        {
+            var kore = new Kore.Kore(".");
+
+            var list = kore.GetAdapters<ILoadFiles>().Where(x => x is ITest);
+            Assert.IsTrue(list.Any());
+        }
+
+        [TestMethod]
+        public void FileExtensionsByType()
+        {
+            var kore = new Kore.Kore(".");
+
+            var exts = kore.FileExtensionsByType<ILoadFiles>();
+
+            Assert.IsTrue(exts.Contains(".test"));
+        }
+
+        [TestMethod]
+        public void FileFiltersByType()
+        {
+            var kore = new Kore.Kore(".");
+
+            var filters = kore.FileFiltersByType<ILoadFiles>();
+
+            Assert.IsTrue(filters.Contains("TestPlugin (*.test)|*.test"));
         }
     }
 }
