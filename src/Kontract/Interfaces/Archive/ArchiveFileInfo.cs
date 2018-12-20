8c86adfc4e36e2d0b631e2a95a8772fbdc0c76df
onepiecefreak3 <alberteinstein1980@gmx.de>
2018-12-20 21:34:57 +0100

    Added ArchiveAdapter interface;

---
+++ src/Kontract/Interfaces/Archive/ArchiveFileInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontract.Interfaces.Archive
+{
+    public class ArchiveFileInfo
+    {
+        protected Stream _fileData = null;
+
+        public string FileName { get; set; } = string.Empty; // Complete filename including path and extension.
+        public virtual Stream FileData // Provides a stream to read the file data from.
+        {
+            get
+            {
+                _fileData.Position = 0;
+                return _fileData;
+            }
+            set => _fileData = value;
+        }
+        public virtual long? FileSize => FileData?.Length; // The length of the (uncompressed) stream, override this in derived classes when FileData is also overridden.
+        public ArchiveFileState State { get; set; } = ArchiveFileState.Empty; // Dictates the state of the ArchiveFileInfo to the UI. Plugins should not rely on this field for code logic.
+    }
+
+    [Flags]
+    public enum ArchiveFileState
+    {
+        Empty = 0,
+        Archived = 1,
+        Added = 2,
+        Replaced = 4,
+        Renamed = 8,
+        Deleted = 16
+    }
+}
