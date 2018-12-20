4b5c3f0e9116728a4a2caa5b7ac307431b1db47f
onepiecefreak3 <alberteinstein1980@gmx.de>
2018-12-20 23:28:31 +0100

    Added 2 new forms for WinForms UI;

---
+++ src/Kuriimu2_WinForms/FormatForms/ArchiveForm.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace Kuriimu2_WinForms.FormatForms
+{
+    partial class ArchiveForm
+    {
+        /// <summary> 
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary> 
+        /// Erforderliche Methode für die Designerunterstützung. 
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // ArchiveForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "ArchiveForm";
+            this.Size = new System.Drawing.Size(641, 394);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ src/Kuriimu2_WinForms/FormatForms/ArchiveForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Kuriimu2_WinForms.Interfaces;
+using Kore;
+
+namespace Kuriimu2_WinForms.FormatForms
+{
+    public partial class ArchiveForm : UserControl, IKuriimuForm
+    {
+        public ArchiveForm(KoreFileInfo kfi)
+        {
+            InitializeComponent();
+
+            Kfi = kfi;
+        }
+
+        public KoreFileInfo Kfi { get; set; }
+    }
+}
