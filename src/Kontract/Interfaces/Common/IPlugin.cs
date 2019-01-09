4ace7bba50d48c37e85ee68bb5da60aa944bd13f
IcySon55 <icyson55@gmail.com>
2019-01-09 16:19:21 -0500

    Added IPlugin and move IPluginMetadata to Common. PluginLoader was not refactored.

---
+++ src/Kontract/Interfaces/Common/IPlugin.cs
@@ -0,0 +1,10 @@
+﻿namespace Kontract.Interfaces.Common
+{
+    /// <summary>
+    /// This interface tags your class as a plugin loadable by <see cref="PluginLoader"/>.
+    /// </summary>
+    public interface IPlugin
+    {
+
+    }
+}
---
+++ src/Kontract/Interfaces/Common/IPluginMetadata.cs
@@ -0,0 +1,10 @@
+﻿namespace Kontract.Interfaces.Common
+{
+    /// <summary>
+    /// This interface tags plugin metadata classes to facilitate easier access.
+    /// </summary>
+    public interface IPluginMetadata
+    {
+
+    }
+}
