693c60b67b3330b7c0716a044eee074e41cca9f5
onepiecefreak3 <alberteinstein1980@gmx.de>
2019-01-16 23:30:39 +0100

    Added KuriimuPictureBox; Gridded background and first version of zooming;

--- src/Kuriimu2_WinForms/FormatForms/ImageForm.Designer.cs
+++ src/Kuriimu2_WinForms/FormatForms/ImageForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Kuriimu2_WinForms.FormatForms
+﻿using System.Windows.Forms;
+
+namespace Kuriimu2_WinForms.FormatForms
 {
     partial class ImageForm
     {
@@ -28,33 +30,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tlsMain = new System.Windows.Forms.ToolStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new Kuriimu2_WinForms.KuriimuPictureBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tlsMain
+            // 
+            this.tlsMain.BackColor = System.Drawing.Color.Transparent;
+            this.tlsMain.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.tlsMain.Location = new System.Drawing.Point(0, 0);
+            this.tlsMain.Name = "tlsMain";
+            this.tlsMain.Size = new System.Drawing.Size(789, 25);
+            this.tlsMain.TabIndex = 1;
+            this.tlsMain.Text = "toolStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(789, 446);
+            this.panel1.TabIndex = 2;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(53, 36);
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(268, 220);
+            this.pictureBox1.Size = new System.Drawing.Size(529, 440);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(789, 446);
+            this.splitContainer1.SplitterDistance = 535;
+            this.splitContainer1.TabIndex = 1;
             // 
             // ImageForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tlsMain);
             this.Name = "ImageForm";
-            this.Size = new System.Drawing.Size(645, 395);
+            this.Size = new System.Drawing.Size(789, 471);
             this.Load += new System.EventHandler(this.ImageForm_Load);
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStrip tlsMain;
+        private System.Windows.Forms.Panel panel1;
+        private Kuriimu2_WinForms.KuriimuPictureBox pictureBox1;
+        private SplitContainer splitContainer1;
     }
 }
---
+++ src/Kuriimu2_WinForms/KuriimuPictureBox.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kuriimu2_WinForms
+{
+    public class ZoomChangedEventArgs : EventArgs
+    {
+        public double NewZoomLevel { get; set; }
+    }
+
+    public class KuriimuPictureBox : PictureBox
+    {
+        public virtual Color GridColor1 { get; set; } = Color.White;
+
+        public virtual Color GridColor2 { get; set; } = Color.LightGray;
+
+        public virtual int GridSize { get; set; } = 15;
+
+        public virtual double ZoomLevel { get; set; } = 1.0;
+
+        public event EventHandler<EventArgs> ZoomChanged;
+
+        protected virtual Image ZoomedImage { get; set; }
+
+        protected override bool DoubleBuffered { get; set; } = true;
+
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
+            if (GridSize <= 0)
+            {
+                base.OnPaintBackground(pevent);
+                return;
+            }
+
+            pevent.Graphics.FillRectangles(new SolidBrush(GridColor1), GetGridRectangles(x => x % 2).ToArray());
+            pevent.Graphics.FillRectangles(new SolidBrush(GridColor2), GetGridRectangles(x => x % 2 == 0 ? 1 : 0).ToArray());
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            OnPaintBackground(new PaintEventArgs(CreateGraphics(), DisplayRectangle));
+            pe.Graphics.DrawImage(ZoomedImage ?? Image, new PointF(0, 0));
+            //base.OnPaint(pe);
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            if (e.Delta == 0)
+                return;
+
+            var newZoomLevel = Math.Min(30.0, Math.Max(0.125, (e.Delta < 0) ? ZoomLevel / 2 : ZoomLevel * 2));
+            if (newZoomLevel == ZoomLevel)
+                return;
+
+            ZoomChanged?.Invoke(this, new ZoomChangedEventArgs { NewZoomLevel = newZoomLevel });
+            ZoomLevel = newZoomLevel;
+
+            ZoomedImage?.Dispose();
+            ZoomedImage = ResizeImage(Image, (int)(Image.Width * ZoomLevel), (int)(Image.Height * ZoomLevel));
+            OnPaint(new PaintEventArgs(CreateGraphics(), DisplayRectangle));
+
+            base.OnMouseWheel(e);
+        }
+
+        private IEnumerable<RectangleF> GetGridRectangles(Func<int, int> getWidthGridStart)
+        {
+            var gridsPerWidth = Math.Ceiling(Width / (double)GridSize);
+            var gridsPerHeight = Math.Ceiling(Height / (double)GridSize);
+
+            for (int h = 0; h < gridsPerHeight; h++)
+                for (int w = getWidthGridStart(h); w < gridsPerWidth; w += 2)
+                    yield return new RectangleF(w * GridSize, h * GridSize, GridSize, GridSize);
+        }
+
+        /// <summary>
+        /// Resize the image to the specified width and height.
+        /// </summary>
+        /// <param name="image">The image to resize.</param>
+        /// <param name="width">The width to resize to.</param>
+        /// <param name="height">The height to resize to.</param>
+        /// <returns>The resized image.</returns>
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
