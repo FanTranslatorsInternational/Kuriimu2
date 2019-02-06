1a3ba7597e6722b6b4344150a3e2b54692d4e709
IcySon55 <icyson55@gmail.com>
2019-02-06 15:10:35 -0500

    Potentially added support for names with spaces in QUI. (Omit the single quote to use.)

--- plugins/IdeaFactory/plugin_idea_factory/QUI/QUI.cs
+++ plugins/IdeaFactory/plugin_idea_factory/QUI/QUI.cs
@@ -11,8 +11,8 @@ namespace plugin_idea_factory.QUI
     /// </summary>
     public class QUI
     {
-        private const string MatchMessage = "^\\s*\\(message (.+?) (.+?)(?: |$)";
-        private const string MatchSingleMessage = "^\\s*\\(message (.+?) (.+?) (.+?)\\)$";
+        private const string MatchMessage = "^\\s*\\(message (.+) (.+?)(?: |$)";
+        private const string MatchSingleMessage = "^\\s*\\(message (.+) (.+?) (.+?)\\)$";
 
         /// <summary>
         /// Stores all of the entries.
@@ -212,7 +212,7 @@ namespace plugin_idea_factory.QUI
                                 if (lines.Length == 1)
                                 {
                                     var inText = lines[0];
-                                    var text = (inText.StartsWith("(") && inText.EndsWith(")") && entry.Type == QuiEntryType.Name) || inText.StartsWith("(str-append") ? inText : $"\"{inText}\"";
+                                    var text = inText.StartsWith("(") && inText.EndsWith(")") && entry.Type == QuiEntryType.Name || inText.StartsWith("(str-append") ? inText : $"\"{inText}\"";
 
                                     if (Regex.IsMatch(entry.Content, MatchSingleMessage))
                                         sw.WriteLine(" " + text.Replace("\n", string.Empty) + ")" + entry.Comment);
