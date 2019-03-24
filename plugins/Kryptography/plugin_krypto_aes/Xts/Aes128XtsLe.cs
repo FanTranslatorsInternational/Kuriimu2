5fc07792a253ed783904e44fcd13f0c9464006da
onepiecefreak3 <alberteinstein1980@gmx.de>
2019-03-25 00:24:39 +0100

    Added plugin for kryptography;

---
+++ plugins/Kryptography/plugin_krypto_aes/Xts/Aes128XtsLe.cs
@@ -0,0 +1,95 @@
+﻿using Kontract;
+using Kontract.Interfaces.Common;
+using Kontract.Interfaces.Intermediate;
+using Kryptography.AES;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace plugin_krypto_aes.Ecb
+{
+    [Export(typeof(ICipherAdapter))]
+    public class Aes128XtsLe : ICipherAdapter
+    {
+        public EventHandler<RequestKeyEventArgs> RequestKey { get; set; }
+
+        private byte[] OnRequestKey(string message, int keyLength, out string error)
+        {
+            error = string.Empty;
+
+            var eventArgs = new RequestKeyEventArgs(message, keyLength);
+            RequestKey?.Invoke(this, eventArgs);
+
+            if (eventArgs.Data == null)
+            {
+                error = "Data not given.";
+                return null;
+            }
+
+            if (eventArgs.Data.Length != keyLength)
+            {
+                error = "Data has no valid length.";
+                return null;
+            }
+
+            return eventArgs.Data;
+        }
+
+        public Task<bool> Decrypt(Stream toDecrypt, Stream decryptInto, IProgress<ProgressReport> progress)
+        {
+            return DoCipher(toDecrypt, decryptInto, progress, true);
+        }
+
+        public Task<bool> Encrypt(Stream toEncrypt, Stream encryptInto, IProgress<ProgressReport> progress)
+        {
+            return DoCipher(toEncrypt, encryptInto, progress, false);
+        }
+
+        private Task<bool> DoCipher(Stream input, Stream output, IProgress<ProgressReport> progress, bool decrypt)
+        {
+            var key = OnRequestKey("AES128 XTS Key", 32, out var error);
+            if (key == null)
+                return Task.Factory.StartNew(() =>
+                {
+                    progress.Report(new ProgressReport { Percentage = 0, Message = error });
+                    return false;
+                });
+
+            return Task.Factory.StartNew(() =>
+            {
+                progress.Report(new ProgressReport { Percentage = 0, Message = decrypt ? "Decryption..." : "Encryption..." });
+
+                using (var ecb = new XtsStream(decrypt ? input : output, key, new byte[16], true))
+                {
+                    var buffer = new byte[0x10000];
+                    while (ecb.Position < ecb.Length)
+                    {
+                        var length = (int)Math.Min(0x10000, ecb.Length - ecb.Position);
+
+                        if (decrypt)
+                        {
+                            ecb.Read(buffer, 0, length);
+                            output.Write(buffer, 0, length);
+                        }
+                        else
+                        {
+                            input.Read(buffer, 0, length);
+                            ecb.Write(buffer, 0, length);
+                        }
+
+                        progress.Report(new ProgressReport { Percentage = (double)ecb.Length / ecb.Position * 100, Message = decrypt ? "Decryption..." : "Encryption...", });
+                    }
+                }
+
+                progress.Report(new ProgressReport { Percentage = 100, Message = decrypt ? "Decryption finished." : "Encryption finished." });
+
+                return true;
+            });
+        }
+    }
+}
---
+++ src/Kontract/Interfaces/Intermediate/ICipherAdapter.cs
@@ -0,0 +1,64 @@
+﻿using Kontract.Interfaces.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontract.Interfaces.Intermediate
+{
+    /// <summary>
+    /// Provides methods to decrypt or encrypt files
+    /// </summary>
+    public interface ICipherAdapter : IPlugin
+    {
+        /// <summary>
+        /// Encrypts a file
+        /// </summary>
+        /// <param name="toEncrypt"></param>
+        /// <param name="encryptInto"></param>
+        /// <param name="progress"></param>
+        Task<bool> Encrypt(Stream toEncrypt, Stream encryptInto, IProgress<ProgressReport> progress);
+
+        /// <summary>
+        /// Decrypts a file
+        /// </summary>
+        /// <param name="toDecrypt"></param>
+        /// <param name="decryptInto"></param>
+        /// <param name="progress"></param>
+        Task<bool> Decrypt(Stream toDecrypt, Stream decryptInto, IProgress<ProgressReport> progress);
+
+        /// <summary>
+        /// Eventhandler for requesting key material
+        /// </summary>
+        EventHandler<RequestKeyEventArgs> RequestKey { get; set; }
+    }
+
+    /// <summary>
+    /// The event arguments for requesting key material
+    /// </summary>
+    public class RequestKeyEventArgs : EventArgs
+    {
+        public RequestKeyEventArgs(string requestMessage, int dataLength)
+        {
+            RequestMessage = requestMessage;
+            DataSize = dataLength;
+        }
+
+        /// <summary>
+        /// The message displayed in an input field
+        /// </summary>
+        public string RequestMessage { get; }
+
+        /// <summary>
+        /// The length of the key
+        /// </summary>
+        public int DataSize { get; }
+
+        /// <summary>
+        /// The key returned by the event
+        /// </summary>
+        public byte[] Data { get; set; }
+    }
+}
