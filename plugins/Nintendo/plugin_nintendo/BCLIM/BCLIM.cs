aadf1416a0a40d2dd855ab2f089b0e31ee50d378
IcySon55 <icyson55@gmail.com>
2019-04-09 02:47:21 -0400

    Migrated the BCLIM plugin out of SamplePlugins.

---
+++ plugins/Nintendo/plugin_nintendo/BCLIM/BCLIM.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using System.IO;
+using Kanvas;
+using Kanvas.Swizzle;
+using Komponent.IO;
+using plugin_nintendo.NW4C;
+
+namespace plugin_nintendo.BCLIM
+{
+    public class BCLIM
+    {
+        public NW4CHeader FileHeader { get; private set; }
+        public BclimHeader TextureHeader { get; private set; }
+        public ImageSettings Settings { get; set; }
+
+        public Bitmap Texture { get; set; }
+
+        public BCLIM(Stream input)
+        {
+            using (var br = new BinaryReaderX(input))
+            {
+                var texture = br.ReadBytes((int)br.BaseStream.Length - 0x28);
+
+                FileHeader = br.ReadType<NW4CHeader>();
+                br.ByteOrder = FileHeader.ByteOrder;
+                TextureHeader = br.ReadType<BclimHeader>();
+
+                Settings = new ImageSettings
+                {
+                    Width = TextureHeader.Width,
+                    Height = TextureHeader.Height,
+                    Format = ImageFormats.CTRFormats[TextureHeader.Format],
+                    Swizzle = new CTRSwizzle(TextureHeader.Width, TextureHeader.Height, TextureHeader.SwizzleTileMode)
+                };
+
+                Texture = Common.Load(texture, Settings);
+            }
+        }
+
+        public void Save(Stream output)
+        {
+
+        }
+    }
+}
---
+++ plugins/Nintendo/plugin_nintendo/NW4C/NW4CHeader.cs
@@ -0,0 +1,20 @@
+﻿using Komponent.IO;
+using Komponent.IO.Attributes;
+
+namespace plugin_nintendo.NW4C
+{
+    /// <summary>
+    /// The general file header for NW4C formats.
+    /// </summary>
+    public class NW4CHeader
+    {
+        [FixedLength(4)]
+        public string Magic;
+        public ByteOrder ByteOrder;
+        public short HeaderSize;
+        public int Version;
+        public int FileSize;
+        public short SectionCount;
+        public short Padding;
+    }
+}
