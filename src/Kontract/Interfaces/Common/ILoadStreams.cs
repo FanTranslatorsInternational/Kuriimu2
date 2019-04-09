bcb4e1744c93869341f2451577f0e4c50f36f36d
IcySon55 <icyson55@gmail.com>
2019-04-09 03:33:47 -0400

    Temporarily restored the Stream interfaces because plugin loading crashes without them.

---
+++ src/Kontract/Interfaces/Common/ILoadStreams.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Kontract.Interfaces.Common
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// This interface allows a plugin to load streams.
+    /// </summary>
+    public interface ILoadStreams : IDisposable
+    {
+        ///// <summary>
+        ///// Returns a list of filenames to be loaded.
+        ///// </summary>
+        ///// <param name="input">The initial stream being loaded.</param>
+        ///// <param name="filename">The initial filename being loaded.</param>
+        ///// <returns></returns>
+        //IList<string> GetFilenames(Stream input, string filename);
+
+        /// <summary>
+        /// Loads the given stream(s).
+        /// </summary>
+        /// <param name="inputs">The stream(s) to be loaded.</param>
+        void Load(params Stream[] inputs);
+    }
+}
---
+++ src/Kontract/Interfaces/Common/ISaveStreams.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Kontract.Interfaces.Common
+{
+    /// <summary>
+    /// This interface allows a plugin to save streams.
+    /// </summary>
+    public interface ISaveStreams
+    {
+        /// <summary>
+        /// Allows a plugin to save streams.
+        /// </summary>
+        /// <param name="output">The stream to be saved.</param>
+        /// <param name="versionIndex">The version index that the user selected.</param>
+        void Save(Stream output, int versionIndex = 0);
+    }
+}
