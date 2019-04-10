14db9ba13440714164918cc84a7a59caf3c227b2
Megaflan <smtivesp@gmail.com>
2019-04-10 07:18:35 +0200

    KSLT Load plugin (without color correction)

--- plugins/Gust/plugin_blue_reflection/KSLT/KsltAdapter.cs
+++ plugins/Gust/plugin_blue_reflection/KSLT/KsltAdapter.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Kontract.Interfaces.Archive;
 using Komponent.IO;
+using Kontract.Interfaces.Image;
+using Kontract;
 
 namespace plugin_blue_reflection.KSLT
 {
@@ -15,9 +17,24 @@ namespace plugin_blue_reflection.KSLT
     [Export(typeof(IPlugin))]
     [PluginInfo("69D27048-0EA2-4C48-A9A3-19521C9115C3", "KSLT (Texture container format)", "KSLT", "Megaflan", "", "This is the KSLT image adapter for Kuriimu2.")]
     [PluginExtensionInfo("*.kslt")]
-    public sealed class KsltAdapter : IImageAdapter, IIdentifyFiles, ILoadFiles, ILoadStreams, ISaveFiles, ISaveStreams
+    public sealed class KsltAdapter : IImageAdapter, IIdentifyFiles, ILoadFiles, ISaveFiles
     {
         private KSLT _format;
+        private List<BitmapInfo> infos;
+
+        #region Properties
+
+        [FormFieldIgnore]
+        public IList<BitmapInfo> BitmapInfos => infos;
+
+        [FormFieldIgnore]
+        public IList<FormatInfo> FormatInfos => null;//ImageFormats.CTRFormats.Select(x => new FormatInfo(x.Key, x.Value.FormatName)).ToList();
+
+        [FormFieldIgnore]
+        public bool LeaveOpen { get; set; }
+
+        #endregion
+
 
         public bool Identify(StreamInfo input)
         {
@@ -35,6 +52,19 @@ namespace plugin_blue_reflection.KSLT
         public void Load(StreamInfo input)
         {
             _format = new KSLT(input.FileData);
+            infos = _format.bitmaps.Select(b => new BitmapInfo(b, new FormatInfo(0x0, ImageFormats.Formats[0x0].FormatName))).ToList();
+            //infos = new List<BitmapInfo>() { new BitmapInfo(_format.Texture, new FormatInfo(_format.TextureHeader.Format, ImageFormats.CTRFormats[_format.TextureHeader.Format].FormatName)) { Name = "0" } };
+        }
+
+        public async Task<bool> Encode(BitmapInfo bitmapInfo, FormatInfo formatInfo, IProgress<ProgressReport> progress)
+        {
+            // TODO: Get Kanvas to encode the image and update the UI with it.
+            return false;
+        }
+
+        public void Save(StreamInfo output, int versionIndex = 0)
+        {
+            //_format.Save(output.FileData);
         }
 
         public void Dispose() { }
--- plugins/Gust/plugin_blue_reflection/KSLT/KsltSupport.cs
+++ plugins/Gust/plugin_blue_reflection/KSLT/KsltSupport.cs
@@ -1,4 +1,7 @@
-﻿using Komponent.IO.Attributes;
+﻿using Kanvas.Format;
+using Kanvas.Interface;
+using Komponent.IO.Attributes;
+using System.Collections.Generic;
 
 namespace plugin_blue_reflection.KSLT
 {
@@ -12,7 +15,7 @@ namespace plugin_blue_reflection.KSLT
         public int FNameTableSize;
         public int FileSize2;
     }
-    public class Padding
+    public class unkPadding
     {
         [FixedLength(0x38)]
         public byte[] Padding = {0x0B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x80,
@@ -25,5 +28,27 @@ namespace plugin_blue_reflection.KSLT
         public int Offset;
         [FixedLength(0x10)]
         public byte[] Padding;
+    }
+    public class ImageHeader
+    {
+        public int unk0;
+        public short Width;
+        public short Height;
+        public int unk3;
+        public int unk4;
+        public int unk5;
+        public int unk6;
+        public int unk7;
+        public int DataSize;
+        public int unk8;
+        [FixedLength(0x24)]
+        public byte[] Padding;
+    }
+    public static class ImageFormats
+    {
+        public static Dictionary<int, IImageFormat> Formats = new Dictionary<int, IImageFormat>
+        {
+            [0x0] = new RGBA(8, 8, 8, 8, false, true),
+        };
     }
 }
