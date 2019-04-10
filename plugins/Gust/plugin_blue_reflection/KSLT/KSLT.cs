771f72a39f5bac4f2d5dfc2bc6f23fa3b7790350
IcySon55 <icyson55@gmail.com>
2019-04-10 11:14:56 -0400

    Fixed the build. MegaFlan...

--- plugins/Gust/plugin_blue_reflection/KSLT/KSLT.cs
+++ plugins/Gust/plugin_blue_reflection/KSLT/KSLT.cs
@@ -54,8 +54,8 @@ namespace plugin_blue_reflection.KSLT
             /*using (var bw = new BinaryWriterX(output, true))
             {
                 
-            }
-            return true;*/
+            }*/
+            return false;
         }
     }
 }
