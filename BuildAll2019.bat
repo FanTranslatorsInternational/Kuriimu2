9463c4424c74b531991bd852c1e71348a4821eac
IcySon55 <icyson55@gmail.com>
2019-04-10 13:32:23 -0400

    Updated the BuildAll script to restore nugets and also made it pretty.

--- BuildAll2019.bat
+++ BuildAll2019.bat
@@ -1,10 +1,18 @@
 @echo off
-rem ##################################
-rem  Build All Kuriimu2 Solutions
-rem ##################################
-
-rem Kuriimu2
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MsBuild.exe" Kuriimu2.sln /p:Configuration=Debug /p:WarningLevel=0
-
-rem Plugins
-for /r %%i in (*.sln) do (echo %%i & call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MsBuild.exe" %%i /p:Configuration=Debug /p:WarningLevel=0)
+echo ##################################
+echo    Build All Kuriimu2 Solutions
+echo ##################################
+echo.
+echo ### Restore Nugets ###
+echo.
+for /r %%i in (*.sln) do (echo Restoring nugets for %%i & call "nuget\BuildNugets\nuget.exe" %%i > nul 2>&1)
+echo.
+echo ### Build Solutions ###
+echo.
+for /r %%i in (*.sln) do (echo Building %%i... & call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MsBuild.exe" %%i /p:Configuration=Debug /p:WarningLevel=0 > nul 2>&1)
+echo.
+echo ##################################
+echo         Build All Complete
+echo ##################################
+echo.
+pause
