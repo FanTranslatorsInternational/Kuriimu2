79d7f51e8068e44718dedb4e2ef93b95f347a6c9
IcySon55 <icyson55@gmail.com>
2019-04-11 00:03:04 -0400

    Updated the BuildAll script so that it works on machines that only have VS2017 Community as well as the ability to fully reinstall nugets from freshly built copies.

--- BuildAll2019.bat
+++ BuildAll2019.bat
@@ -5,11 +5,20 @@ echo ##################################
 echo.
 echo ### Restore Nugets ###
 echo.
-for /r %%i in (*.sln) do (echo Restoring nugets for %%i & call "nuget\BuildNugets\nuget.exe" %%i > nul 2>&1)
+choice /m "Reinstall nugets from scratch (3..2..1..N)" /t 3 /d N
+if errorlevel 2 goto restore
+if errorlevel 1 goto clean
+:clean
+for /r /d %%i in (packages) do @if exist "%%i" (echo Cleaning "%%i"... & rd /s /q "%%i")
+for /r %%i in (*.sln) do (echo Restoring nugets for %%i & call "nuget\BuildNugets\nuget.exe" restore "%%i" -NoCache > nul 2>&1)
+goto build
+:restore
+for /r %%i in (*.sln) do (echo Restoring nugets for %%i & call "nuget\BuildNugets\nuget.exe" restore "%%i" > nul 2>&1)
+:build
 echo.
 echo ### Build Solutions ###
 echo.
-for /r %%i in (*.sln) do (echo Building %%i... & call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MsBuild.exe" %%i /p:Configuration=Debug /p:WarningLevel=0 > nul 2>&1)
+for /r %%i in (*.sln) do (echo Building %%i... & @if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MsBuild.exe" (call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MsBuild.exe" %%i /p:Configuration=Debug /p:WarningLevel=0 > nul 2>&1) else (call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" %%i /p:Configuration=Debug /p:WarningLevel=0 > nul 2>&1))
 echo.
 echo ##################################
 echo         Build All Complete
