ff71296b34165facc7ff3ccada7afd94b0f703b5
IcySon55 <icyson55@gmail.com>
2019-04-13 00:27:10 -0400

    Implemented basic loading for BTX files.

---
+++ plugins/NipponIchiSoftware/plugin_yuusha_shisu/BTX/BTX.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Kanvas;
+using Kanvas.Format;
+using Kanvas.Interface;
+using Kanvas.Palette;
+using Komponent.IO;
+
+namespace plugin_yuusha_shisu.BTX
+{
+    public class BTX
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public FileHeader Header;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public Bitmap Texture;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        public BTX(Stream input)
+        {
+            using (var br = new BinaryReaderX(input, true))
+            {
+                // Read
+                Header = br.ReadType<FileHeader>();
+                br.SeekAlignment();
+                var fileName = br.ReadCStringASCII();
+
+                // Setup
+                var dataLength = Header.Width * Header.Height;
+                var paletteDataLength = Header.ColorCount * 4;
+
+                // Read Image Data
+                br.BaseStream.Position = Header.ImageOffset;
+                var texture = br.ReadBytes(dataLength);
+                br.BaseStream.Position = Header.PaletteOffset;
+                var palette = br.ReadBytes(paletteDataLength);
+
+                // Image
+                var settings = new ImageSettings
+                {
+                    Width = Header.Width,
+                    Height = Header.Height,
+                    Format = Formats[ImageFormat.Palette_8]
+                };
+
+                // Palette
+                (settings.Format as IPaletteFormat)?.SetPalette(PaletteFormats[ImageFormat.RGBA8888].Load(palette));
+
+                // Load!
+                Texture = Common.Load(texture, settings);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="output"></param>
+        public void Save(Stream output)
+        {
+            using (var bw = new BinaryWriterX(output, true))
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static Dictionary<ImageFormat, IImageFormat> Formats = new Dictionary<ImageFormat, IImageFormat>
+        {
+            [ImageFormat.Palette_8] = new Palette(8)
+        };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static Dictionary<ImageFormat, IImageFormat> PaletteFormats = new Dictionary<ImageFormat, IImageFormat>
+        {
+            [ImageFormat.RGBA8888] = new RGBA(8, 8, 8, 8) { ByteOrder = ByteOrder.BigEndian }
+        };
+    }
+}
---
+++ plugins/NipponIchiSoftware/plugin_yuusha_shisu/BTX/BtxAdapter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+using Komponent.IO;
+using Kontract;
+using Kontract.Attributes;
+using Kontract.Interfaces.Common;
+using Kontract.Interfaces.Image;
+
+namespace plugin_yuusha_shisu.BTX
+{
+
+    [Export(typeof(BtxAdapter))]
+    [Export(typeof(IPlugin))]
+    [PluginInfo("plugin_yuusha_shisu_btx", "Death of a Hero", "BTX", "IcySon55")]
+    [PluginExtensionInfo("*.btx")]
+    public class BtxAdapter : IImageAdapter, IIdentifyFiles, ILoadFiles
+    {
+        private BTX _format;
+        private List<BitmapInfo> _bitmapInfos;
+
+        #region Properties
+
+        [FormFieldIgnore]
+        public IList<BitmapInfo> BitmapInfos => _bitmapInfos;
+
+        public IList<FormatInfo> FormatInfos => BTX.Formats.Select(x => new FormatInfo((int)x.Key, x.Value.FormatName)).ToList();
+
+        public bool LeaveOpen { get; set; }
+
+        #endregion
+
+        public bool Identify(StreamInfo input)
+        {
+            try
+            {
+                using (var br = new BinaryReaderX(input.FileData, true))
+                    return br.PeekString(4) == "btx\0";
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void Load(StreamInfo input)
+        {
+            _format = new BTX(input.FileData);
+            _bitmapInfos = new List<BitmapInfo> { new BitmapInfo(_format.Texture, new FormatInfo(0, BTX.Formats[ImageFormat.Palette_8].FormatName)) };
+        }
+
+        public async Task<bool> Encode(BitmapInfo bitmapInfo, FormatInfo formatInfo, IProgress<ProgressReport> progress)
+        {
+            return false;
+        }
+
+        public void Dispose() { }
+    }
+}
