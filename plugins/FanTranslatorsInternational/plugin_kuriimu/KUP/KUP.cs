282ff4da9d0e0a5d6c716ee14959ee83502be19a
IcySon55 <icyson55@gmail.com>
2019-04-15 10:55:31 -0400

    Ported KUP into its own plugin.

---
+++ plugins/FanTranslatorsInternational/plugin_kuriimu/KUP/KUP.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using Kontract.Interfaces.Text;
+
+namespace plugin_kuriimu.KUP
+{
+    /// <summary>
+    /// KUP is Kuriimu's basic transport format for text.
+    /// </summary>
+    [XmlRoot("kup")]
+    public sealed class KUP
+    {
+        /// <summary>
+        /// The text entries stored in the file.
+        /// </summary>
+        [XmlArray("entries")]
+        [XmlArrayItem("entry")]
+        public List<TextEntry> Entries { get; set; }
+
+        /// <summary>
+        /// Creates a new KUP file.
+        /// </summary>
+        public KUP()
+        {
+            Entries = new List<TextEntry>();
+        }
+
+        /// <summary>
+        /// Load a KUP files into memory.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static KUP Load(Stream input)
+        {
+            var xmlSettings = new XmlReaderSettings { CheckCharacters = false };
+            return (KUP)new XmlSerializer(typeof(KUP)).Deserialize(XmlReader.Create(input, xmlSettings));
+        }
+
+        /// <summary>
+        /// Save the text to a KUP file.
+        /// </summary>
+        /// <param name="output"></param>
+        public void Save(Stream output)
+        {
+            try
+            {
+                var xmlSettings = new XmlWriterSettings
+                {
+                    Encoding = Encoding.UTF8,
+                    Indent = true,
+                    NewLineOnAttributes = false,
+                    NewLineHandling = NewLineHandling.Entitize,
+                    IndentChars = "	",
+                    CheckCharacters = false
+                };
+
+                using (var xmlIO = new StreamWriter(output, xmlSettings.Encoding))
+                {
+                    var serializer = new XmlSerializer(typeof(KUP));
+                    var namespaces = new XmlSerializerNamespaces();
+                    namespaces.Add(string.Empty, string.Empty);
+                    serializer.Serialize(XmlWriter.Create(xmlIO, xmlSettings), this, namespaces);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ plugins/FanTranslatorsInternational/plugin_kuriimu/KUP/KupAdapter.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
+using System.Linq;
+using Kontract.Attributes;
+using Kontract.Interfaces.Common;
+using Kontract.Interfaces.Text;
+
+namespace plugin_kuriimu.KUP
+{
+    [Export(typeof(KupAdapter))]
+    [Export(typeof(IPlugin))]
+    [PluginInfo("963E7A09-61C4-4A80-94BA-427134F1A5B8", "Kuriimu Text Archive", "KUP", "IcySon55", "", "This is the KUP text adapter for Kuriimu.")]
+    [PluginExtensionInfo("*.kup")]
+    public sealed class KupAdapter : ITextAdapter, IIdentifyFiles, ICreateFiles, ILoadFiles, ISaveFiles, IAddEntries
+    {
+        private KUP _format;
+
+        #region Properties
+
+        public IEnumerable<TextEntry> Entries
+        {
+            get => _format?.Entries;
+            set => _format.Entries = value.ToList();
+        }
+
+        public string NameFilter => @".*";
+
+        public int NameMaxLength => 0;
+
+        public string LineEndings { get; set; } = "\n";
+
+        public bool LeaveOpen { get; set; }
+
+        #endregion
+
+        public bool Identify(StreamInfo input)
+        {
+            var result = true;
+
+            try
+            {
+                using (var sr = new StreamReader(input.FileData))
+                {
+                    sr.ReadLine(); // Skip the XML declaration
+                    if (!sr.ReadLine().StartsWith("<kup"))
+                        result = false;
+                }
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        public void Create()
+        {
+            _format = new KUP();
+        }
+
+        public void Load(StreamInfo input)
+        {
+            _format = KUP.Load(input.FileData);
+        }
+
+        public void Save(StreamInfo output, int versionIndex = 0)
+        {
+            _format.Save(output.FileData);
+        }
+
+        public TextEntry NewEntry()
+        {
+            return new TextEntry();
+        }
+
+        public bool AddEntry(TextEntry entry)
+        {
+            _format.Entries.Add(entry);
+            return true;
+        }
+
+        public void Dispose() { }
+    }
+}
