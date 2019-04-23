d72f0f31a041c7818ee4de00fe08c9077551f83d
onepiecefreak3 <alberteinstein1980@gmx.de>
2019-04-23 20:59:27 +0200

    Added OctreeColorCache;

---
+++ src/Kanvas/Quantization/ColorCaches/OctreeColorCache.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kanvas.Quantization.Helper;
+using Kanvas.Quantization.Models.ColorCache;
+
+namespace Kanvas.Quantization.ColorCaches
+{
+    public class OctreeColorCache : BaseColorCache
+    {
+        private OctreeCacheNode _root;
+
+        protected override void OnPrepare()
+        {
+        }
+
+        protected override void OnCachePalette()
+        {
+            _root = new OctreeCacheNode();
+
+            Int32 index = 0;
+            foreach (Color color in Palette)
+            {
+                _root.AddColor(color, index++, 0);
+            }
+        }
+
+        protected override int CalculatePaletteIndex(Color color)
+        {
+            var candidates = _root.GetPaletteIndex(color, 0);
+
+            var result = 0;
+            int index = 0;
+            int colorIndex =
+                ColorModelHelper.GetSmallestEuclideanDistanceIndex(_colorModel, color, candidates.Values.ToList());
+
+            foreach (var colorPaletteIndex in candidates.Keys)
+            {
+                if (index == colorIndex)
+                {
+                    result = colorPaletteIndex;
+                    break;
+                }
+
+                index++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/Kanvas/Quantization/Models/ColorCache/OctreeCacheNode.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanvas.Quantization.Models.ColorCache
+{
+    class OctreeCacheNode
+    {
+        private static readonly byte[] Mask = { 0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01 };
+
+        private readonly OctreeCacheNode[] nodes;
+        private readonly Dictionary<int, Color> entries;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OctreeCacheNode"/> class.
+        /// </summary>
+        public OctreeCacheNode()
+        {
+            nodes = new OctreeCacheNode[8];
+            entries = new Dictionary<int, Color>();
+        }
+
+        /// <summary>
+        /// Adds the color.
+        /// </summary>
+        /// <param name="color">The color.</param>
+        /// <param name="paletteIndex">Index of the palette.</param>
+        /// <param name="level">The level.</param>
+        public void AddColor(Color color, int paletteIndex, int level)
+        {
+            // if this node is a leaf, then increase a color amount, and pixel presence
+            entries.Add(paletteIndex, color);
+
+            if (level < 8) // otherwise goes one level deeper
+            {
+                // calculates an index for the next sub-branch
+                Int32 index = GetColorIndexAtLevel(color, level);
+
+                // if that branch doesn't exist, grows it
+                if (nodes[index] == null)
+                {
+                    nodes[index] = new OctreeCacheNode();
+                }
+
+                // adds a color to that branch
+                nodes[index].AddColor(color, paletteIndex, level + 1);
+            }
+        }
+
+        /// <summary>
+        /// Gets the index of the palette.
+        /// </summary>
+        public Dictionary<int, Color> GetPaletteIndex(Color color, int level)
+        {
+            Dictionary<int, Color> result = entries;
+
+            if (level < 8)
+            {
+                int index = GetColorIndexAtLevel(color, level);
+
+                if (nodes[index] != null)
+                {
+                    result = nodes[index].GetPaletteIndex(color, level + 1);
+                }
+            }
+
+            return result;
+        }
+
+        private static int GetColorIndexAtLevel(Color color, int level)
+        {
+            return ((color.R & Mask[level]) == Mask[level] ? 4 : 0) |
+                   ((color.G & Mask[level]) == Mask[level] ? 2 : 0) |
+                   ((color.B & Mask[level]) == Mask[level] ? 1 : 0);
+        }
+    }
+}
