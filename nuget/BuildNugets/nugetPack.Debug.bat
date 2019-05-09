4f395856c2879b9c8df500cd8a797a160f438c33
Kirito <alberteinstein1980@gmx.de>
2019-05-09 11:45:53 +0200

    Updated nuget.Debug.bat;

--- nuget/BuildNugets/nugetPack.Debug.bat
+++ nuget/BuildNugets/nugetPack.Debug.bat
@@ -1,4 +1,7 @@
 @echo off
+SET com2017="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+SET com2019="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MsBuild.exe"
+SET pro2019="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\amd64\MSBuild.exe"
 echo ##################################
 echo       Build Kuriimu2 Nugets
 echo ##################################
@@ -6,7 +9,9 @@ echo.
 echo ### Build Kuriimu2 Libraries ###
 echo.
 echo Building Kuriimu2.sln... 
-if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MsBuild.exe" (call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\amd64\MsBuild.exe" ..\..\Kuriimu2.sln /p:Configuration=Debug /p:WarningLevel=0 > nul 2>&1) else (call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ..\..\Kuriimu2.sln /p:Configuration=Debug /p:WarningLevel=0 > nul 2>&1)
+if exist %com2017% (call %com2017% ..\..\Kuriimu2.sln /p:Configuration=Debug /p:WarningLevel=0 > nul 2>&1) ^
+else if exist %com2019% (call %com2019% ..\..\Kuriimu2.sln /p:Configuration=Debug /p:WarningLevel=0 > nul 2>&1) ^
+else (call %pro2019% ..\..\Kuriimu2.sln /p:Configuration=Debug /p:WarningLevel=0 > nul 2>&1)
 echo.
 echo ### Build Nugets ###
 echo.
@@ -22,3 +27,4 @@ echo ##################################
 echo       Build Nugets Complete
 echo ##################################
 echo.
+pause
