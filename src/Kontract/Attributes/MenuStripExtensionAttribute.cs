085ddf704b2fbb2c097502e4c0bb93f4dfc9c0a2
IcySon55 <icyson55@gmail.com>
2019-05-20 01:51:54 -0400

    Removed reference to System.Windows.Forms in MenuStripExtensionAttribute. (Portability)

--- src/Kontract/Attributes/MenuStripExtensionAttribute.cs
+++ src/Kontract/Attributes/MenuStripExtensionAttribute.cs
@@ -1,23 +1,22 @@
 ﻿using System;
-using System.Windows.Forms;
 
 namespace Kontract.Attributes
 {
     /// <summary>
-    /// An attribute used to describe the path in a <see cref="MenuStrip"/>.
+    /// An attribute used to describe the path in a MenuStrip.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class MenuStripExtensionAttribute : Attribute
     {
         /// <summary>
-        /// An array containing the elements of the path in a <see cref="MenuStrip"/>.
+        /// An array containing the elements of the path in a MenuStrip.
         /// </summary>
         public string[] Strips { get; }
 
         /// <summary>
         /// Creates a new instance of <see cref="MenuStripExtensionAttribute"/>.
         /// </summary>
-        /// <param name="strips">An array containing the elements of the path in a <see cref="MenuStrip"/>.</param>
+        /// <param name="strips">An array containing the elements of the path in a MenuStrip.</param>
         public MenuStripExtensionAttribute(params string[] strips)
         {
             Strips = strips;
