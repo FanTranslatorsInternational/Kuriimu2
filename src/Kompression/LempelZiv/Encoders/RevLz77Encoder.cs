45eb3aa113e0ecbc86ac6511438549eae9181e55
onepiecefreak3 <alberteinstein1980@gmx.de>
2019-08-06 16:51:21 +0200

    Start RevLz77Encoder and adapter;

---
+++ src/Kompression/LempelZiv/Encoders/RevLz77Encoder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kompression.LempelZiv.Encoders
+{
+    public class RevLz77Encoder : ILzEncoder
+    {
+        public void Encode(Stream input, Stream output, LzMatch[] matches)
+        {
+            // Matches are backward relative to the end of input
+            // That means matches[0] relates to the first match found from the end of input
+            foreach (var match in matches.Reverse())    // We reverse the matches to work from the beginning of the file as usual
+            {
+
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/Kompression/LempelZiv/RevLZ77.cs
+++ src/Kompression/LempelZiv/RevLZ77.cs
@@ -1,25 +1,50 @@
 ﻿using System;
+using System.IO;
 using Kompression.LempelZiv.Decoders;
 using Kompression.LempelZiv.Encoders;
+using Kompression.LempelZiv.MatchFinder;
 using Kompression.LempelZiv.Parser;
 
 namespace Kompression.LempelZiv
 {
-    public class RevLz77 : BaseLz
+    public class RevLz77 : ICompression
     {
-        protected override ILzEncoder CreateEncoder()
+        public void Decompress(Stream input, Stream output)
         {
-            throw new NotImplementedException();
+            var decoder = new RevLz77Decoder(ByteOrder.LittleEndian);
+
+            decoder.Decode(input, output);
+
+            decoder.Dispose();
         }
 
-        protected override ILzParser CreateParser(int inputLength)
+        public void Compress(Stream input, Stream output)
         {
-            throw new NotImplementedException();
+            var encoder = new RevLz77Encoder();
+            var parser = new PlusOneGreedyParser(new NeedleHaystackMatchFinder(3, 0x12, 0x1002));
+
+            // Reverse input
+            var inputArray = ToArray(input);
+            Array.Reverse(inputArray);
+
+            // Get matches; All information in them are backwards relative to the end
+            // For example position of 0 relates to input.Length
+            var matches = parser.Parse(new Span<byte>(inputArray));
+
+            encoder.Encode(input, output, matches);
+
+            encoder.Dispose();
+            parser.Dispose();
         }
 
-        protected override ILzDecoder CreateDecoder()
+        private byte[] ToArray(Stream input)
         {
-            return new RevLz77Decoder();
+            var bkPos = input.Position;
+            var inputArray = new byte[input.Length];
+            input.Read(inputArray, 0, inputArray.Length);
+            input.Position = bkPos;
+
+            return inputArray;
         }
     }
 }
