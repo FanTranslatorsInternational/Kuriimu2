84125a741f6124ce45d726a6810e8a1c1a6aca27
onepiecefreak3 <alberteinstein1980@gmx.de>
2019-09-22 21:17:42 +0200

    Added mode 3 for Tales of decompression; Added Enc decompression;

---
+++ src/Kompression/Implementations/Decoders/LzEncDecoder.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kompression.PatternMatch;
+
+namespace Kompression.Implementations.Decoders
+{
+    public class LzEncDecoder : IPatternMatchDecoder
+    {
+        private byte[] _windowBuffer;
+        private int _windowBufferOffset;
+
+        private byte _codeByte;
+
+        public void Decode(Stream input, Stream output)
+        {
+            _windowBuffer = new byte[0xBFFF];
+            _windowBufferOffset = 0;
+
+            // Handle initial byte
+            _codeByte = (byte)input.ReadByte();
+            if (_codeByte < 0x12)
+            {
+                input.Position--;
+                if (!ReadSubLoopRawData(input, output))
+                    if (!ReadSubLoopMatch(input, output))
+                        SubLoop(input, output);
+            }
+            else
+            {
+                var count = _codeByte - 0x11;
+                if (count > 3)
+                {
+                    ReadRawData(input, output, count);
+                    if (!ReadSubLoopMatch(input, output))
+                        SubLoop(input, output);
+                }
+                else
+                {
+                    ReadRawData(input, output, count);
+                }
+            }
+
+            MainLoop(input, output);
+        }
+
+        private void MainLoop(Stream input, Stream output)
+        {
+            while (true)
+            {
+                _codeByte = (byte)input.ReadByte();
+
+                if (_codeByte >= 0x40)
+                {
+                    var byte2 = (byte)input.ReadByte();
+
+                    // Min disp: 0x1; Max disp: 0x800
+                    // Min length: 1; Max length: 6
+
+                    var displacement = ((_codeByte >> 2) & 0x7) + (byte2 << 3) + 1;
+                    var length = (_codeByte >> 5) +1;
+
+                    var bufferIndex = _windowBufferOffset + _windowBuffer.Length - displacement;
+                    for (int i = 0; i < length; i++)
+                    {
+                        var value = _windowBuffer[bufferIndex++ % _windowBuffer.Length];
+                        output.WriteByte(value);
+                        _windowBuffer[_windowBufferOffset] = value;
+                        _windowBufferOffset = (_windowBufferOffset + 1) % _windowBuffer.Length;
+                    }
+                }
+                else if (_codeByte >= 0x20)
+                {
+                    // 14 bit displacement
+                    // Min disp: 1; Max disp: 0x4000
+                    // Min length: 2; Max length: virtually infinite
+
+                    var length = _codeByte & 0x1F;
+                    if (length == 0)
+                        length += ReadVariableLength(input, 5);
+                    length += 2;
+
+                    _codeByte = (byte)input.ReadByte();
+                    var byte2 = (byte)input.ReadByte();
+                    var displacement = (_codeByte >> 2) + (byte2 << 6) + 1;
+
+                    var bufferIndex = _windowBufferOffset + _windowBuffer.Length - displacement;
+                    for (int i = 0; i < length; i++)
+                    {
+                        var value = _windowBuffer[bufferIndex++ % _windowBuffer.Length];
+                        output.WriteByte(value);
+                        _windowBuffer[_windowBufferOffset] = value;
+                        _windowBufferOffset = (_windowBufferOffset + 1) % _windowBuffer.Length;
+                    }
+                }
+                else if (_codeByte >= 0x10)
+                {
+                    var length = _codeByte & 0x7;
+                    if (length == 0)
+                        length += ReadVariableLength(input, 3);
+                    length += 2;
+
+                    var sign = _codeByte & 0x8;
+                    _codeByte = (byte)input.ReadByte();
+                    var byte2 = (byte)input.ReadByte();
+
+                    if (sign == 0 && (_codeByte >> 2) == 0 && byte2 == 0)
+                        // End of decompression
+                        break;
+
+                    var displacement = (_codeByte >> 2) + (byte2 << 6) + (sign > 0 ? 0x4000 : 0) + 0x4000;
+
+                    var bufferIndex = _windowBufferOffset + _windowBuffer.Length - displacement;
+                    for (int i = 0; i < length; i++)
+                    {
+                        var value = _windowBuffer[bufferIndex++ % _windowBuffer.Length];
+                        output.WriteByte(value);
+                        _windowBuffer[_windowBufferOffset] = value;
+                        _windowBufferOffset = (_windowBufferOffset + 1) % _windowBuffer.Length;
+                    }
+                }
+                else
+                {
+                    var byte2 = (byte)input.ReadByte();
+
+                    var displacement = ((byte2 << 2) | (_codeByte >> 2)) + 1;
+
+                    var bufferIndex = _windowBufferOffset + _windowBuffer.Length - displacement;
+                    for (int i = 0; i < 2; i++)
+                    {
+                        var value = _windowBuffer[bufferIndex++ % _windowBuffer.Length];
+                        output.WriteByte(value);
+                        _windowBuffer[_windowBufferOffset] = value;
+                        _windowBufferOffset = (_windowBufferOffset + 1) % _windowBuffer.Length;
+                    }
+                }
+
+                SubLoop(input, output);
+            }
+        }
+
+        private void SubLoop(Stream input, Stream output)
+        {
+            while (true)
+            {
+                if ((_codeByte & 0x3) != 0)
+                {
+                    ReadRawData(input, output, _codeByte & 0x3);
+                    break;
+                }
+
+                if (ReadSubLoopRawData(input, output))
+                    break;
+
+                if (ReadSubLoopMatch(input, output))
+                    break;
+            }
+        }
+
+        private bool ReadSubLoopRawData(Stream input, Stream output)
+        {
+            // Raw Data read
+            _codeByte = (byte)input.ReadByte();
+            if (_codeByte > 0xF)
+            {
+                input.Position--;
+                return true;
+            }
+
+            var length = _codeByte + 3;
+            if (_codeByte == 0)
+                length += ReadVariableLength(input, 4);
+            ReadRawData(input, output, length);
+
+            return false;
+        }
+
+        private bool ReadSubLoopMatch(Stream input, Stream output)
+        {
+            // Read LZ match
+            _codeByte = (byte)input.ReadByte();
+            if (_codeByte > 0xF)
+            {
+                input.Position--;
+                return true;
+            }
+
+            var codeByte2 = (byte)input.ReadByte();
+            var displacement = ((codeByte2 << 2) | (_codeByte >> 2)) + 0x801;
+
+            var bufferIndex = _windowBufferOffset + _windowBuffer.Length - displacement;
+            for (int i = 0; i < 3; i++)
+            {
+                var value = _windowBuffer[bufferIndex++ % _windowBuffer.Length];
+                output.WriteByte(value);
+                _windowBuffer[_windowBufferOffset] = value;
+                _windowBufferOffset = (_windowBufferOffset + 1) % _windowBuffer.Length;
+            }
+
+            return false;
+        }
+
+        private void ReadRawData(Stream input, Stream output, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                var value = (byte)input.ReadByte();
+                output.WriteByte(value);
+                _windowBuffer[_windowBufferOffset] = value;
+                _windowBufferOffset = (_windowBufferOffset + 1) % _windowBuffer.Length;
+            }
+        }
+
+        private int ReadVariableLength(Stream input, int bitCount)
+        {
+            var length = 0;
+            var flag = input.ReadByte();
+            while (flag == 0)
+            {
+                length += 0xFF;
+                flag = input.ReadByte();
+            }
+
+            return length + flag + ((1 << bitCount) - 1);
+        }
+
+        public void Dispose()
+        {
+            // Nothing to dispose
+        }
+    }
+}
---
+++ src/Kompression/Implementations/TalesOf03.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kompression.Implementations.Decoders;
+using Kompression.Implementations.MatchFinders;
+using Kompression.PatternMatch;
+using Kompression.PatternMatch.LempelZiv;
+
+namespace Kompression.Implementations
+{
+    public class TalesOf03 : BaseLz
+    {
+        public TalesOf03()
+        {
+        }
+
+        protected override IPatternMatchEncoder CreateEncoder()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IMatchParser CreateParser(int inputLength)
+        {
+            return new NewOptimalParser(null, 0, new BackwardLz77MatchFinder(0, 0, 0, 0));
+        }
+
+        protected override IPatternMatchDecoder CreateDecoder()
+        {
+            return new TalesOf03Decoder();
+        }
+
+        public override string[] Names => new[] { "Tales Of Mode 0x03" };
+    }
+}
