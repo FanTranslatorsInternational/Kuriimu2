1ecddc16154705907f3d77e56f8c0fb95bc30cb7
IcySon55 <icyson55@gmail.com>
2019-10-07 11:50:59 -0400

    Fixed a startup crash when no plugins are available and a file is provided via the command line.

--- src/Kuriimu2/Dialogs/ViewModels/SelectAdapterViewModel.cs
+++ src/Kuriimu2/Dialogs/ViewModels/SelectAdapterViewModel.cs
@@ -66,7 +66,7 @@ namespace Kuriimu2.Dialogs.ViewModels
 
         public void OKButton()
         {
-            Adapter = SelectedAdapter.Adapter;
+            Adapter = SelectedAdapter?.Adapter;
 
             if (ValidationCallback != null)
             {
