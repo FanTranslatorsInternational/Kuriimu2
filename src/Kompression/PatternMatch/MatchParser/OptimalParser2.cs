f3f9dda1cb1f2c5a689b8230e0d3794a2e88bd42
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-02-05 22:57:55 +0100

    Use only one list for OptimalParser2;

--- src/Kompression/PatternMatch/MatchParser/OptimalParser2.cs
+++ src/Kompression/PatternMatch/MatchParser/OptimalParser2.cs
@@ -55,108 +55,126 @@ namespace Kompression.PatternMatch.MatchParser
             return Array.Empty<Match>();
         }
 
-        struct Position
+        //struct Position
+        //{
+        //    public int position;
+        //    public int runLength;
+        //    public bool matchRun;
+
+        //    public int historyIndex;
+
+        //    public Position(int position, int runLength, bool matchRun, int historyIndex)
+        //    {
+        //        this.position = position;
+        //        this.runLength = runLength;
+        //        this.matchRun = matchRun;
+        //        this.historyIndex = historyIndex;
+        //    }
+
+        //    public override bool Equals(object obj)
+        //    {
+        //        if (obj is Position struct1)
+        //        {
+        //            return position == struct1.position && runLength == struct1.runLength && matchRun == struct1.matchRun;
+        //        }
+
+        //        return base.Equals(obj);
+        //    }
+        //}
+
+        private IList<PositionElement>[] _history2;
+
+        class PositionElement
         {
-            public int position;
             public int runLength;
             public bool matchRun;
 
-            public int historyIndex;
-
-            public Position(int position, int runLength, bool matchRun, int historyIndex)
-            {
-                this.position = position;
-                this.runLength = runLength;
-                this.matchRun = matchRun;
-                this.historyIndex = historyIndex;
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (obj is Position struct1)
-                {
-                    return position == struct1.position && runLength == struct1.runLength && matchRun == struct1.matchRun;
-                }
-
-                return base.Equals(obj);
-            }
-        }
-
-        struct PositionElement
-        {
-            public Position? parent;
+            public PositionElement parent;
             public int price;
 
-            public PositionElement(Position? parent, int price)
+            public PositionElement(int runLength, bool matchRun)
+            {
+                this.runLength = runLength;
+                this.matchRun = matchRun;
+            }
+
+            public PositionElement(int runLength, bool matchRun, PositionElement parent, int price) :
+                this(runLength, matchRun)
             {
                 this.parent = parent;
                 this.price = price;
             }
         }
 
-        private List<Position>[] _historyPositions;
-        private List<PositionElement> _historyElements;
+        //private List<Position>[] _historyPositions;
+        //private List<PositionElement> _historyElements;
 
         private void ForwardPass(byte[] input, int startPosition)
         {
-            _historyPositions = new List<Position>[input.Length - startPosition + (int)FindOptions.UnitSize];
-            for (var i = 0; i < _historyPositions.Length; i++)
-                _historyPositions[i] = new List<Position>();
-            _historyPositions[0].Add(new Position(0, 0, false, 0));
-
-            _historyElements = new List<PositionElement>();
-            _historyElements.Add(new PositionElement(null, 0));
+            _history2 = new IList<PositionElement>[input.Length - startPosition + (int)FindOptions.UnitSize];
+            for (var i = 0; i < _history2.Length; i++)
+                _history2[i] = new List<PositionElement>();
+            _history2[0].Add(new PositionElement(0, false));
 
             var matches = GetAllMatches(input, startPosition);
 
             var unitSize = (int)FindOptions.UnitSize;
             for (var dataPosition = 0; dataPosition < input.Length - startPosition; dataPosition += unitSize)
             {
-                foreach (var literalPosition in _historyPositions[dataPosition])
+                foreach (var literalPosition in _history2[dataPosition])
                 {
                     var price = _priceCalculator.CalculateLiteralPrice(dataPosition, input[dataPosition],
                         literalPosition.runLength, literalPosition.matchRun);
 
                     var newRunLength = literalPosition.matchRun ? unitSize : literalPosition.runLength + unitSize;
-                    if (!_historyPositions[dataPosition + unitSize].Any(x => x.matchRun == false && x.runLength == newRunLength))
+                    var existingEntry = _history2[dataPosition + unitSize]
+                               .FirstOrDefault(x => x.matchRun == false && x.runLength == newRunLength);
+                    if (existingEntry == null)
                     {
-                        _historyPositions[dataPosition + unitSize].Add(new Position(dataPosition + unitSize, newRunLength, false, _historyElements.Count));
-                        _historyElements.Add(new PositionElement(literalPosition, _historyElements[literalPosition.historyIndex].price + price));
+                        var newEntry = new PositionElement(newRunLength, false,
+                            literalPosition, literalPosition.price + price);
+                        _history2[dataPosition + unitSize].Add(newEntry);
                     }
                     else
                     {
-                        var historyPosition = _historyPositions[dataPosition + unitSize].First(x => x.matchRun == false && x.runLength == newRunLength);
-                        if (_historyElements[historyPosition.historyIndex].price > _historyElements[literalPosition.historyIndex].price + price)
-                            _historyElements.Add(new PositionElement(literalPosition, _historyElements[literalPosition.historyIndex].price + price));
+                        if (existingEntry.price > literalPosition.price + price)
+                        {
+                            existingEntry.parent = literalPosition;
+                            existingEntry.price = literalPosition.price + price;
+                        }
                     }
                 }
 
                 // Then go through all matches at current position
                 for (var finderIndex = 0; finderIndex < _finders.Length; finderIndex++)
                 {
-                    var finderMatches = matches[finderIndex][dataPosition];
-                    if (finderMatches == null || !finderMatches.Any())
+                    var finderMatch = matches[finderIndex][dataPosition];
+                    if (finderMatch == null || !finderMatch.HasMatches)
                         continue;
 
-                    var matchIndex = 0;
-                    for (var j = _finders[finderIndex].FindLimitations.MinLength; j <= finderMatches[finderMatches.Count - 1].Length; j += unitSize)
+                    for (var j = _finders[finderIndex].FindLimitations.MinLength; j <= finderMatch.MaxLength; j += unitSize)
                     {
-                        foreach (var matchPosition in _historyPositions[dataPosition])
+                        foreach (var matchPosition in _history2[dataPosition])
                         {
-                            var price = _priceCalculator.CalculateMatchPrice(dataPosition, finderMatches[matchIndex].Displacement, j,
+                            var price = _priceCalculator.CalculateMatchPrice(dataPosition, finderMatch.GetDisplacement(j), j,
                                 matchPosition.runLength, matchPosition.matchRun);
 
                             var newRunLength = matchPosition.matchRun ? matchPosition.runLength + 1 : 1;
-                            if (!_historyPositions[dataPosition + j].Any(x => x.matchRun && x.runLength == newRunLength))
+                            var existingEntry = _history2[dataPosition + j]
+                                .FirstOrDefault(x => x.matchRun == true && x.runLength == newRunLength);
+                            if (existingEntry == null)
                             {
-                                _historyPositions[dataPosition + j].Add(new Position(dataPosition + j, newRunLength, true, _historyElements.Count));
-                                _historyElements.Add(new PositionElement(matchPosition, _historyElements[matchPosition.historyIndex].price + price));
+                                var newEntry = new PositionElement(newRunLength, true,
+                                    matchPosition, matchPosition.price + price);
+                                _history2[dataPosition + j].Add(newEntry);
                             }
                             else
                             {
-                                var historyPosition = _historyPositions[dataPosition + j].First(x => x.matchRun && x.runLength == newRunLength);
-                                if (_historyElements[historyPosition.historyIndex].price > _historyElements[matchPosition.historyIndex].price + price)
-                                    _historyElements.Add(new PositionElement(matchPosition, _historyElements[matchPosition.historyIndex].price + price));
+                                if (existingEntry.price > matchPosition.price + price)
+                                {
+                                    existingEntry.parent = matchPosition;
+                                    existingEntry.price = matchPosition.price + price;
+                                }
                             }
                         }
 
@@ -171,9 +189,6 @@ namespace Kompression.PatternMatch.MatchParser
                         //    _history[dataPosition + j].Length = j;
                         //    _history[dataPosition + j].Price = matchPrice;
                         //}
-
-                        if (j + unitSize > finderMatches[matchIndex].Length)
-                            matchIndex++;
                     }
                 }
             }
@@ -242,14 +257,46 @@ namespace Kompression.PatternMatch.MatchParser
         //    }
         //}
 
-        private IList<IList<IList<Match>>> GetAllMatches(byte[] input, int startPosition)
+        class AggregateMatch
         {
-            var result = new IList<IList<Match>>[_finders.Length];
+            private readonly IList<Match> _matches;
+            private readonly (int, int, int)[] _lengthRange;
+
+            public int MaxLength { get; }
+
+            public bool HasMatches { get; }
+
+            public AggregateMatch(IList<Match> matches)
+            {
+                _matches = matches;
+                _lengthRange = new (int, int, int)[matches.Count];
+
+                var prevLength = 1;
+                for (var i = 0; i < matches.Count; i++)
+                {
+                    _lengthRange[i] = (prevLength, matches[i].Length, i);
+                    prevLength += matches[i].Length;
+                }
+
+                HasMatches = matches.Any();
+                if (HasMatches)
+                    MaxLength = matches.Last().Length;
+            }
+
+            public int GetDisplacement(int length)
+            {
+                return _matches[_lengthRange.First(x => length >= x.Item1 && length <= x.Item2).Item3].Displacement;
+            }
+        }
+
+        private IList<IList<AggregateMatch>> GetAllMatches(byte[] input, int startPosition)
+        {
+            var result = new IList<AggregateMatch>[_finders.Length];
 
             for (var i = 0; i < _finders.Length; i++)
             {
                 result[i] = Enumerable.Range(startPosition, input.Length).AsParallel().AsOrdered()
-                    .Select(x => _finders[i].FindMatchesAtPosition(input, x)).ToArray();
+                    .Select(x => new AggregateMatch(_finders[i].FindMatchesAtPosition(input, x))).ToArray();
             }
 
             return result;
