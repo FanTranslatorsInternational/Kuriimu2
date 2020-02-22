90e508d7b99d6f6f57c88176c7aa84d491686169
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-02-22 16:56:46 +0100

    Add more net core code; Remove unnecessary files;

--- src/Kompression/Extensions/Int32Extensions.cs
+++ src/Kompression/Extensions/Int32Extensions.cs
@@ -1,15 +1,32 @@
-﻿namespace Kompression.Extensions
+﻿#if NET_CORE_31
+using System.Buffers.Binary;
+#endif
+
+namespace Kompression.Extensions
 {
+    // TODO: Remove after switching to net core 3.1
     static class Int32Extensions
     {
         public static byte[] GetArrayLittleEndian(this int value)
         {
+#if NET_CORE_31
+            var buffer = new byte[4];
+            BinaryPrimitives.WriteInt32LittleEndian(buffer, value);
+            return buffer;
+#else
             return new[] { (byte)value, (byte)(value >> 8), (byte)(value >> 16), (byte)(value >> 24) };
+#endif
         }
 
         public static byte[] GetArrayBigEndian(this int value)
         {
+#if NET_CORE_31
+            var buffer = new byte[4];
+            BinaryPrimitives.WriteInt32BigEndian(buffer, value);
+            return buffer;
+#else
             return new[] { (byte)(value >> 24), (byte)(value >> 16), (byte)(value >> 8), (byte)value };
+#endif
         }
     }
 }
