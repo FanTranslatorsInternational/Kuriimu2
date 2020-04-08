0db59aa29811e40944cd843d748735bf8020ba1b
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-04-08 15:47:36 +0200

    Add Save method for QP in Skip Ltd plugin;

--- plugins/Nintendo/plugin_skip_ltd/Archives/QP.cs
+++ plugins/Nintendo/plugin_skip_ltd/Archives/QP.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using Komponent.IO;
 using Komponent.IO.Streams;
 using Kontract.Models.Archive;
 using Kontract.Models.IO;
 using System.Linq;
+using System.Text;
 using Kontract.Extensions;
 
 namespace plugin_skip_ltd.Archives
 {
     public class QP
     {
+        private static int _headerSize = 0x20;
         private static int _entrySize = Tools.MeasureType(typeof(QpEntry));
 
         public IReadOnlyList<ArchiveFileInfo> Load(Stream input)
@@ -72,20 +73,117 @@ namespace plugin_skip_ltd.Archives
 
         public void Save(Stream output, IReadOnlyList<ArchiveFileInfo> files)
         {
+            using var bw = new BinaryWriterX(output);
+
             // Build directory tree
             var directoryTree = BuildDirectoryTree(files);
 
+            // Build entries
+            var nameStream = new MemoryStream();
+            using var nameBw = new BinaryWriterX(nameStream, true);
 
+            var entries = new List<QpEntry>();
+            var fileInfos = new List<ArchiveFileInfo>();
+            var fileOffset = 0;
+
+            void PopulateEntryList(IList<(UPath, int)> directories, int parentIndex)
+            {
+                for (var i = 0; i < directories.Count; i++)
+                {
+                    var currentDirectory = directories[i];
+
+                    // Write directory name
+                    var directoryNameOffset = (int)nameBw.BaseStream.Position;
+                    var splittedDirectoryName = currentDirectory.Item1.Split();
+                    nameBw.WriteString(splittedDirectoryName.Any() ? splittedDirectoryName.Last() : string.Empty, Encoding.ASCII, false);
+
+                    // Add directory
+                    var currentDirectoryIndex = entries.Count;
+                    var currentDirectoryEntry = new QpEntry
+                    {
+                        IsDirectory = true,
+                        NameOffset = directoryNameOffset,
+                        fileOffset = parentIndex
+                    };
+                    entries.Add(currentDirectoryEntry);
+
+                    // Add files
+                    foreach (var file in files.Where(x => x.FilePath.GetDirectory() == currentDirectory.Item1))
+                    {
+                        fileInfos.Add(file);
+
+                        // Write file name
+                        var nameOffset = (int)nameBw.BaseStream.Position;
+                        nameBw.WriteString(file.FilePath.GetName(), Encoding.ASCII, false);
+
+                        // Add file entry
+                        var fileEntry = new QpEntry
+                        {
+                            IsDirectory = false,
+                            NameOffset = nameOffset,
+                            fileOffset = fileOffset,
+                            fileSize = (int)file.FileSize
+                        };
+                        entries.Add(fileEntry);
+
+                        fileOffset = (int)((fileOffset + file.FileSize + 0x1F) & ~0x1F);
+                    }
+
+                    // Add sub directories
+                    var subDirectories = directories.Where(x => x.Item1.IsInDirectory(currentDirectory.Item1, true)).ToArray();
+                    PopulateEntryList(subDirectories, currentDirectoryIndex);
+
+                    // Edit fileSize field
+                    currentDirectoryEntry.fileSize = entries.Count;
+
+                    i += subDirectories.Length - 1;
+                }
+            }
+
+            PopulateEntryList(directoryTree, 0);
+
+            // Adjust file offsets to absolute values
+            var dataOffset = (_headerSize + entries.Count * _entrySize + (int)nameStream.Length + 0x1F) & ~0x1F;
+            foreach (var entry in entries)
+                if (!entry.IsDirectory)
+                    entry.fileOffset += dataOffset;
+
+            // Write entries
+            bw.BaseStream.Position = _headerSize;
+            bw.WriteMultiple(entries);
+
+            // Write names
+            nameStream.Position = 0;
+            nameStream.CopyTo(bw.BaseStream);
+            bw.WriteAlignment(0x20);
+
+            // Write files
+            foreach (var fileInfo in fileInfos)
+            {
+                fileInfo.SaveFileData(bw.BaseStream, null);
+                bw.WriteAlignment(0x20);
+            }
+
+            // Write header
+            bw.BaseStream.Position = 0;
+            bw.WriteType(new QpHeader
+            {
+                // TODO: Set hash for header
+                hash = 0xFFFFFFFF,
+                entryDataOffset = _headerSize,
+                entryDataSize = entries.Count * _entrySize + (int)nameStream.Length,
+                dataOffset = dataOffset
+            });
         }
 
-        private IList<UPath> BuildDirectoryTree(IReadOnlyList<ArchiveFileInfo> files)
+        private IList<(UPath, int)> BuildDirectoryTree(IReadOnlyList<ArchiveFileInfo> files)
         {
             var distinctDirectories = files
                 .OrderBy(x => x.FilePath.GetDirectory())
                 .Select(x => x.FilePath.GetDirectory())
                 .Distinct();
 
-            var directories = new List<UPath> { UPath.Root };
+            var directories = new List<(UPath, int)> { (UPath.Empty, -1) };
             foreach (var directory in distinctDirectories)
             {
                 var splittedDirectory = directory.Split();
@@ -94,8 +192,8 @@ namespace plugin_skip_ltd.Archives
                     var takenParts = splittedDirectory.Take(i + 1).Select(x => (UPath)x).ToArray();
                     var combinedPath = UPath.Combine(takenParts);
 
-                    if (!directories.Contains(combinedPath))
-                        directories.Add(combinedPath);
+                    if (directories.All(x => x.Item1 != combinedPath))
+                        directories.Add((combinedPath, directories.FindIndex(x => x.Item1 == combinedPath.GetDirectory())));
                 }
             }
 
