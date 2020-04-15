092539b50160214db700ed567eed89372f67cbb1
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-04-15 22:40:48 +0200

    Add TypeWriter for use in BinaryWriterX;

---
+++ src/Komponent/IO/BinarySupport/TypeWriter.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Komponent.IO.Attributes;
+using Kontract.Models.IO;
+
+namespace Komponent.IO.BinarySupport
+{
+    class TypeWriter
+    {
+        public void WriteType(BinaryWriterX bw, object writeValue)
+        {
+            var storage = new ValueStorage();
+            WriteTypeInternal(bw, writeValue, storage);
+        }
+
+        private void WriteTypeInternal(BinaryWriterX bw, object writeValue, ValueStorage storage, FieldInfo fieldInfo = null, bool isTypeChose = false)
+        {
+            var writeType = writeValue.GetType();
+            var typeAttributes = new MemberAttributeInfo(writeType);
+            var fieldAttributes = fieldInfo == null ? null : new MemberAttributeInfo(fieldInfo);
+
+            var bkByteOrder = bw.ByteOrder;
+            var bkBitOrder = bw.BitOrder;
+            var bkBlockSize = bw.BlockSize;
+
+            bw.ByteOrder = fieldAttributes?.EndiannessAttribute?.ByteOrder ??
+                           typeAttributes.EndiannessAttribute?.ByteOrder ??
+                           bw.ByteOrder;
+
+            if (writeType.IsPrimitive)
+            {
+                WriteTypePrimitive(bw, writeValue, writeType);
+            }
+            else if (writeType == typeof(string))
+            {
+                WriteTypeString(bw, (string)writeValue, fieldAttributes, storage);
+            }
+            else if (writeType == typeof(decimal))
+            {
+                bw.Write((decimal)writeValue);
+            }
+            else if (Tools.IsList(writeType))
+            {
+                WriteTypeList(bw, (IList)writeValue, fieldAttributes, storage);
+            }
+            else if (writeType.IsClass || Tools.IsStruct(writeType))
+            {
+                WriteTypeClass(bw, writeValue, writeType, storage.CreateScope(fieldInfo?.Name));
+            }
+            else if (writeType.IsEnum)
+            {
+                var underlyingType = (writeType as TypeInfo)?.DeclaredFields.ToList()[0];
+                WriteTypeInternal(bw, underlyingType?.GetValue(writeValue), storage);
+            }
+            else throw new UnsupportedTypeException(writeType);
+
+            bw.ByteOrder = bkByteOrder;
+            bw.BitOrder = bkBitOrder;
+            bw.BlockSize = bkBlockSize;
+        }
+
+        private void WriteTypePrimitive(BinaryWriterX bw, object writeValue, Type writeType)
+        {
+            switch (Type.GetTypeCode(writeType))
+            {
+                case TypeCode.Boolean: bw.Write((bool)writeValue); break;
+                case TypeCode.Byte: bw.Write((byte)writeValue); break;
+                case TypeCode.SByte: bw.Write((sbyte)writeValue); break;
+                case TypeCode.Int16: bw.Write((short)writeValue); break;
+                case TypeCode.UInt16: bw.Write((ushort)writeValue); break;
+                case TypeCode.Char: bw.Write((char)writeValue); break;
+                case TypeCode.Int32: bw.Write((int)writeValue); break;
+                case TypeCode.UInt32: bw.Write((uint)writeValue); break;
+                case TypeCode.Int64: bw.Write((long)writeValue); break;
+                case TypeCode.UInt64: bw.Write((ulong)writeValue); break;
+                case TypeCode.Single: bw.Write((float)writeValue); break;
+                case TypeCode.Double: bw.Write((double)writeValue); break;
+                default: throw new NotSupportedException($"Unsupported Primitive {writeType.FullName}.");
+            }
+        }
+
+        private void WriteTypeString(BinaryWriterX bw, string writeValue, MemberAttributeInfo fieldAttributes, ValueStorage storage)
+        {
+            var attributeValues = GetLengthAttributeValues(fieldAttributes, storage);
+            if (!attributeValues.HasValue)
+                return;
+
+            var (length, encoding) = attributeValues.Value;
+            if (encoding.GetByteCount(writeValue) != length)
+                throw new FieldLengthMismatchException(encoding.GetByteCount(writeValue), length);
+
+            bw.WriteString(writeValue, encoding, false, false);
+        }
+
+        private void WriteTypeList(BinaryWriterX bw, IList writeValue, MemberAttributeInfo fieldAttributes, ValueStorage storage)
+        {
+            var attributeValues = GetLengthAttributeValues(fieldAttributes, storage);
+            if (!attributeValues.HasValue)
+                return;
+
+            var (length, _) = attributeValues.Value;
+
+            if (writeValue.Count != length)
+                throw new FieldLengthMismatchException(writeValue.Count, length);
+
+            var listCounter = 0;
+            foreach (var element in writeValue)
+                WriteTypeInternal(bw, element, storage.CreateScope($"[{listCounter++}]"));
+        }
+
+        private void WriteTypeClass(BinaryWriterX bw, object writeValue, Type writeType, ValueStorage storage)
+        {
+            var typeAttributes = new MemberAttributeInfo(writeType);
+
+            var bitFieldInfoAttribute = typeAttributes.BitFieldInfoAttribute;
+            var alignmentAttribute = typeAttributes.AlignmentAttribute;
+
+            bw.BitOrder = (bitFieldInfoAttribute?.BitOrder != BitOrder.Default ? bitFieldInfoAttribute?.BitOrder : bw.BitOrder) ?? bw.BitOrder;
+            bw.BlockSize = bitFieldInfoAttribute?.BlockSize ?? bw.BlockSize;
+            if (bw.BlockSize != 8 && bw.BlockSize != 4 && bw.BlockSize != 2 && bw.BlockSize != 1)
+                throw new InvalidBitFieldInfoException(bw.BlockSize);
+
+            var fields = writeType.GetFields().OrderBy(fi => fi.MetadataToken);
+            foreach (var field in fields)
+            {
+                var fieldValue = field.GetValue(writeValue);
+                storage.Add(field.Name, fieldValue);
+
+                var bitInfo = field.GetCustomAttribute<BitFieldAttribute>();
+                if (bitInfo != null)
+                    bw.WriteBits(Convert.ToInt64(fieldValue), bitInfo.BitLength);
+                else
+                    WriteTypeInternal(bw, fieldValue, storage, field);
+            }
+
+            bw.Flush();
+
+            // Apply alignment
+            if (alignmentAttribute != null)
+                bw.WriteAlignment(alignmentAttribute.Alignment);
+        }
+
+        private (int, Encoding)? GetLengthAttributeValues(MemberAttributeInfo fieldAttributes, ValueStorage storage)
+        {
+            var fixedLengthAttribute = fieldAttributes?.FixedLengthAttribute;
+            var variableLengthAttribute = fieldAttributes?.VariableLengthAttribute;
+            var calculatedLengthAttribute = fieldAttributes?.CalculatedLengthAttribute;
+
+            Encoding stringEncoding;
+            int length;
+            if (fixedLengthAttribute != null)
+            {
+                stringEncoding = Tools.RetrieveEncoding(fixedLengthAttribute.StringEncoding);
+                length = fixedLengthAttribute.Length;
+            }
+            else if (variableLengthAttribute != null)
+            {
+                stringEncoding = Tools.RetrieveEncoding(variableLengthAttribute.StringEncoding);
+                length = Convert.ToInt32(storage.Get(variableLengthAttribute.FieldName)) + variableLengthAttribute.Offset;
+            }
+            else if (calculatedLengthAttribute != null)
+            {
+                stringEncoding = Tools.RetrieveEncoding(calculatedLengthAttribute.StringEncoding);
+                length = calculatedLengthAttribute.CalculationAction(storage);
+            }
+            else
+            {
+                return null;
+            }
+
+            return (length, stringEncoding);
+        }
+    }
+}
--- src/Komponent/IO/BinaryWriterX.cs
+++ src/Komponent/IO/BinaryWriterX.cs
@@ -2,10 +2,7 @@
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Reflection;
 using System.Collections.Generic;
-using System.Collections;
-using Komponent.IO.Attributes;
 using Komponent.IO.BinarySupport;
 using Kontract.Models.IO;
 
@@ -360,156 +357,16 @@ namespace Komponent.IO
 
         #region Generic type writing
 
-        public void WriteType<T>(T obj) => WriteType(obj, null, null);
+        public void WriteType<T>(T obj)
+        {
+            var typeWriter = new TypeWriter();
+            typeWriter.WriteType(this, obj);
+        }
 
         public void WriteMultiple<T>(IEnumerable<T> list)
         {
             foreach (var element in list)
                 WriteType(element);
-        }
-
-        private void WriteType(object data, MemberInfo fieldInfo, List<(string name, object value)> wroteVals, string currentNest = "", bool isTypeChosen = false)
-        {
-            var type = data.GetType();
-
-            if (wroteVals == null)
-                wroteVals = new List<(string, object)>();
-
-            var typeAttributes = new MemberAttributeInfo(type);
-            MemberAttributeInfo fieldAttributes = null;
-            if (fieldInfo != null) fieldAttributes = new MemberAttributeInfo(fieldInfo);
-
-            var bkByteOrder = ByteOrder;
-            var bkBitOrder = BitOrder;
-            var bkBlockSize = _blockSize;
-
-            ByteOrder = fieldAttributes?.EndiannessAttribute?.ByteOrder ?? typeAttributes.EndiannessAttribute?.ByteOrder ?? ByteOrder;
-
-            if (type.IsPrimitive)
-            {
-                WritePrimitive(data);
-            }
-            else if (type == typeof(string))
-            {
-                WriteTypeString(data, fieldAttributes, wroteVals);
-            }
-            else if (type == typeof(decimal))
-            {
-                Write((decimal)data);
-            }
-            else if (Tools.IsList(type))
-            {
-                WriteList(data, fieldAttributes, wroteVals);
-            }
-            else if (type.IsClass || Tools.IsStruct(type))
-            {
-                WriteObject(data, fieldInfo, wroteVals, currentNest);
-            }
-            else if (type.IsEnum)
-            {
-                WriteType((type as TypeInfo)?.DeclaredFields.ToList()[0].GetValue(data));
-            }
-            else throw new UnsupportedTypeException(type);
-
-            ByteOrder = bkByteOrder;
-            BitOrder = bkBitOrder;
-            _blockSize = bkBlockSize;
-        }
-
-        private void WritePrimitive(object data)
-        {
-            switch (Type.GetTypeCode(data.GetType()))
-            {
-                case TypeCode.Boolean: Write((bool)data); break;
-                case TypeCode.Byte: Write((byte)data); break;
-                case TypeCode.SByte: Write((sbyte)data); break;
-                case TypeCode.Int16: Write((short)data); break;
-                case TypeCode.UInt16: Write((ushort)data); break;
-                case TypeCode.Char: Write((char)data); break;
-                case TypeCode.Int32: Write((int)data); break;
-                case TypeCode.UInt32: Write((uint)data); break;
-                case TypeCode.Int64: Write((long)data); break;
-                case TypeCode.UInt64: Write((ulong)data); break;
-                case TypeCode.Single: Write((float)data); break;
-                case TypeCode.Double: Write((double)data); break;
-                default: throw new NotSupportedException("Unsupported Primitive");
-            }
-        }
-
-        private void WriteTypeString(object data, MemberAttributeInfo fieldAttributes, List<(string name, object value)> wroteVals)
-        {
-            var fixedSizeAttribute = fieldAttributes?.FixedLengthAttribute;
-            var variableSizeAttribute = fieldAttributes?.VariableLengthAttribute;
-
-            if (fixedSizeAttribute == null && variableSizeAttribute == null)
-                return;
-
-            var enc = Tools.RetrieveEncoding(fixedSizeAttribute?.StringEncoding ?? variableSizeAttribute.StringEncoding);
-
-            var matchingVals = wroteVals?.Where(v => v.Item1 == variableSizeAttribute?.FieldName);
-            var length = fixedSizeAttribute?.Length ?? ((matchingVals?.Any() ?? false) ? Convert.ToInt32(matchingVals.First().Item2) + variableSizeAttribute.Offset : -1);
-
-            if (enc.GetByteCount((string)data) != length)
-                throw new FieldLengthMismatchException(enc.GetByteCount((string)data), length);
-
-            WriteString((string)data, enc, false, false);
-        }
-
-        private void WriteList(object data, MemberAttributeInfo fieldAttributes, List<(string name, object value)> wroteVals)
-        {
-            var fixedSizeAttribute = fieldAttributes?.FixedLengthAttribute;
-            var variableSizeAttribute = fieldAttributes?.VariableLengthAttribute;
-
-            if (fixedSizeAttribute == null && variableSizeAttribute == null)
-                return;
-
-            var matchingVal = wroteVals?.FirstOrDefault(v => v.Item1 == variableSizeAttribute?.FieldName).Item2;
-            var length = fixedSizeAttribute?.Length ?? ((matchingVal != null) ? Convert.ToInt32(matchingVal) + variableSizeAttribute.Offset : -1);
-
-            var list = (IList)data;
-
-            if (list.Count != length)
-                throw new FieldLengthMismatchException(list.Count, length);
-
-            foreach (var element in list)
-                WriteType(element);
-        }
-
-        private void WriteObject(object data, MemberInfo fieldInfo, List<(string name, object value)> wroteVals, string currentNest)
-        {
-            var typeAttributes = new MemberAttributeInfo(data.GetType());
-
-            var bitFieldInfoAttribute = typeAttributes.BitFieldInfoAttribute;
-            var alignmentAttribute = typeAttributes.AlignmentAttribute;
-
-            BitOrder = (bitFieldInfoAttribute?.BitOrder != BitOrder.Default ? bitFieldInfoAttribute?.BitOrder : BitOrder) ?? BitOrder;
-            _blockSize = bitFieldInfoAttribute?.BlockSize ?? _blockSize;
-            if (_blockSize != 8 && _blockSize != 4 && _blockSize != 2 && _blockSize != 1)
-                throw new InvalidBitFieldInfoException(_blockSize);
-
-            foreach (var field in data.GetType().GetFields().OrderBy(fi => fi.MetadataToken))
-            {
-                var fieldName = string.IsNullOrEmpty(currentNest) ? field.Name : string.Join(".", currentNest, field.Name);
-                wroteVals.Add((fieldName, field.GetValue(data)));
-
-                var bitInfo = field.GetCustomAttribute<BitFieldAttribute>();
-                if (bitInfo != null)
-                {
-                    WriteBits(Convert.ToInt64(field.GetValue(data)), bitInfo.BitLength);
-                }
-                else
-                    WriteType(field.GetValue(data), field.CustomAttributes.Any() ? field : null, wroteVals, fieldName);
-            }
-
-            Flush();
-
-            // Apply alignment
-            if (alignmentAttribute != null)
-            {
-                var remainder = BaseStream.Position % alignmentAttribute.Alignment;
-                if (remainder > 0)
-                    Write(new byte[alignmentAttribute.Alignment - remainder]);
-            }
         }
 
         #endregion
