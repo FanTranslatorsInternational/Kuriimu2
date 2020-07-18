a0b2592470958c4fe961b8b13dd4ae9fc00f1352
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-07-19 00:51:54 +0200

    Add WiiDisc plugin load;

---
+++ plugins/Nintendo/plugin_nintendo/Archives/WiiDiscPlugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+
+namespace plugin_nintendo.Archives
+{
+    public class WiiDiscPlugin : IFilePlugin
+    {
+        public Guid PluginId => Guid.Parse("e5a2f369-2daa-4575-ae4f-f980aac8f2c3");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] { "*.cgrp" };
+        public PluginMetadata Metadata { get; }
+
+        public WiiDiscPlugin()
+        {
+            Metadata = new PluginMetadata("WiiDisc", "onepiecefreak", "The disc format for the Nintendo Wii");
+        }
+
+        public IPluginState CreatePluginState(IPluginManager pluginManager)
+        {
+            return new WiiDiscState();
+        }
+    }
+}
--- plugins/Nintendo/plugin_skip_ltd/Archives/QP.cs
+++ plugins/Nintendo/plugin_skip_ltd/Archives/QP.cs
@@ -12,67 +12,23 @@ namespace plugin_skip_ltd.Archives
     public class QP
     {
         private static int _headerSize = 0x20;
-        private static int _entrySize = Tools.MeasureType(typeof(QpEntry));
+        private static int _entrySize = Tools.MeasureType(typeof(U8Entry));
 
         public IReadOnlyList<ArchiveFileInfo> Load(Stream input)
         {
             using var br = new BinaryReaderX(input, true, ByteOrder.BigEndian);
 
             // Read header
-            var header = br.ReadType<QpHeader>();
+            var header = br.ReadType<U8Header>();
 
-            // Read entries
-            br.BaseStream.Position = header.entryDataOffset;
-            var rootEntry = br.ReadType<QpEntry>();
-
-            br.BaseStream.Position = header.entryDataOffset;
-            var entries = br.ReadMultiple<QpEntry>(rootEntry.size);
-
-            // Read names
-            var nameStream = new SubStream(input, br.BaseStream.Position, header.entryDataSize + header.entryDataOffset - br.BaseStream.Position);
-
-            // Add files
-            using var nameBr = new BinaryReaderX(nameStream);
-
-            var result = new List<ArchiveFileInfo>();
-            var lastDirectoryEntry = entries[0];
-            foreach (var entry in entries.Skip(1))
-            {
-                // A file does not know of its parent directory
-                // The tree is structured so that the last directory entry read must hold the current file
-
-                // Remember the last directory entry
-                if (entry.IsDirectory)
-                {
-                    lastDirectoryEntry = entry;
-                    continue;
-                }
-
-                // Find whole path recursively from lastDirectoryEntry
-                var currentDirectoryEntry = lastDirectoryEntry;
-                var currentPath = UPath.Empty;
-                while (currentDirectoryEntry != entries[0])
-                {
-                    nameBr.BaseStream.Position = currentDirectoryEntry.NameOffset;
-                    currentPath = nameBr.ReadCStringASCII() / currentPath;
-
-                    currentDirectoryEntry = entries[currentDirectoryEntry.offset];
-                }
-
-                // Get file name
-                nameBr.BaseStream.Position = entry.NameOffset;
-                var fileName = currentPath / nameBr.ReadCStringASCII();
-
-                var fileStream = new SubStream(input, entry.offset, entry.size);
-                result.Add(new ArchiveFileInfo(fileStream, fileName.FullName));
-            }
-
-            return result;
+            // Parse file system
+            var fileSystemParser = new U8FileSystem();
+            return fileSystemParser.Parse(input, header.entryDataOffset, header.entryDataSize, 0).ToArray();
         }
 
         public void Save(Stream output, IReadOnlyList<ArchiveFileInfo> files)
         {
-            var darcTreeBuilder = new QpTreeBuilder(Encoding.ASCII);
+            var darcTreeBuilder = new U8TreeBuilder(Encoding.ASCII);
             darcTreeBuilder.Build(files.Select(x => ("/." + x.FilePath.FullName, x)).ToArray());
 
             var entries = darcTreeBuilder.Entries;
@@ -103,11 +59,11 @@ namespace plugin_skip_ltd.Archives
 
             // Write entries
             bw.BaseStream.Position = _headerSize;
-            bw.WriteMultiple(entries.Select(x=>x.Item1));
+            bw.WriteMultiple(entries.Select(x => x.Item1));
 
             // Write header
             bw.BaseStream.Position = 0;
-            bw.WriteType(new QpHeader
+            bw.WriteType(new U8Header
             {
                 entryDataOffset = _headerSize,
                 entryDataSize = entries.Count * _entrySize + (int)nameStream.Length,
