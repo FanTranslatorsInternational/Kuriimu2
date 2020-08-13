6e7ab5bb776c183242a0cb2e3e4ad3b98a25bdf3
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-08-13 17:24:07 +0200

    Fix ToolStripProgressBarOutput;

--- src/Kuriimu2.WinForms/Progress/ToolStripProgressBarOutput.cs
+++ src/Kuriimu2.WinForms/Progress/ToolStripProgressBarOutput.cs
@@ -5,46 +5,20 @@ using Kuriimu2.WinForms.Controls;
 
 namespace Kuriimu2.WinForms.Progress
 {
-    class ToolStripProgressBarOutput : IProgressOutput
+    class ToolStripProgressBarOutput : BaseConcurrentProgressOutput
     {
         private readonly Kuriimu2ProgressBarToolStrip _progressBar;
-        private readonly Timer _timer;
 
-        private ProgressState _progressState;
-
-        public ToolStripProgressBarOutput(Kuriimu2ProgressBarToolStrip progressBar, double updateInterval)
+		public ToolStripProgressBarOutput(Kuriimu2ProgressBarToolStrip progressBar, int updateInterval) : 
+			base(updateInterval)
         {
-            _progressBar = progressBar;
-
-            _timer = new Timer(updateInterval);
-            _timer.Elapsed += Timer_Elapsed;
-            _timer.Enabled = true;
+			_progressBar = progressBar;
         }
 
-        public void SetProgress(ProgressState state)
-        {
-            _progressState = state;
-        }
-
-        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            if (_progressState == null)
-                return;
-
-            var localProgress = _progressState;
-
-            var percentageValue = localProgress.PartialValue / (double)localProgress.MaxValue;
-            var percentageInRange = (localProgress.MaxPercentage - localProgress.MinPercentage) * percentageValue;
-
-            var message = string.IsNullOrWhiteSpace(localProgress.PreText) ?
-                localProgress.Message :
-                localProgress.PreText + localProgress.Message;
-            message = string.IsNullOrWhiteSpace(message) ? string.Empty : message + " - ";
-
-            var completion = localProgress.MinPercentage + percentageInRange;
-
-            _progressBar.Value = Convert.ToInt32(completion);
-            _progressBar.Text = message + $@"{completion:0.00}%";
-        }
+		protected override void OutputProgressInternal(double completion, string message)
+		{
+			_progressBar.Value = Convert.ToInt32(completion);
+            _progressBar.Text = message + $@" - {completion:0.00}%";
+		}
     }
 }
