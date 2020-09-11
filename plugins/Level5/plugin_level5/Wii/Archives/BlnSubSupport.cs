93a0499d882d0edaecbf8838e890d83e2531c126
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-09-11 21:09:27 +0200

    Add archive indexing comments to Bln;

--- plugins/Level5/plugin_level5/Wii/Archives/BlnSubSupport.cs
+++ plugins/Level5/plugin_level5/Wii/Archives/BlnSubSupport.cs
@@ -7,6 +7,14 @@ using Kontract.Models.Archive;
 
 namespace plugin_level5.Wii.Archives
 {
+	// Archive index maps to the following bins for Inazuma Eleven Strikers 2013
+	// 0x00 => strap.bin
+	// 0x01 => scn.bin
+	// 0x02 => scn_sh.bin
+	// 0x03 => ui.bin
+	// 0x04 => dat.bin
+	// 0x05 => grp.bin?
+	
     class BlnSubEntry
     {
         public int archiveIndex;    // index to an external bin
