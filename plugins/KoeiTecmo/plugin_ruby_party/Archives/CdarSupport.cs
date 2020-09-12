d8bbfa4f18746b2c56ea00d2b48e6606fd1c2a7d
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-09-12 23:39:41 +0200

    Add CDAR;

---
+++ plugins/KoeiTecmo/plugin_ruby_party/Archives/CdarSupport.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Komponent.IO.Attributes;
+using Kontract.Interfaces.Progress;
+using Kontract.Kompression.Configuration;
+using Kontract.Models.Archive;
+
+namespace plugin_ruby_party.Archives
+{
+    class CdarHeader
+    {
+        [FixedLength(4)]
+        public string magic = "CDAR";
+        public int unk1;
+        public int entryCount;
+        public int unk2;
+    }
+
+    class CdarFileEntry
+    {
+        public int offset;
+        public int decompSize;
+        public int size;
+    }
+
+    class CdarArchiveFileInfo : ArchiveFileInfo
+    {
+        public CdarArchiveFileInfo(Stream fileData, string filePath) :
+            base(fileData, filePath)
+        {
+        }
+
+        public CdarArchiveFileInfo(Stream fileData, string filePath, IKompressionConfiguration configuration, long decompressedSize) :
+            base(fileData, filePath, configuration, decompressedSize)
+        {
+        }
+
+        public override long SaveFileData(Stream output, bool compress, IProgressContext progress = null)
+        {
+            var writtenSize = base.SaveFileData(output, compress, progress);
+
+            var random = new Random();
+            while (output.Position % 0x10 > 0)
+                output.WriteByte((byte)random.Next());
+
+            return writtenSize;
+        }
+    }
+}
