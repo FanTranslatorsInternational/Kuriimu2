a23475e1a4f1fa34afc473c31360e7abe1b06b11
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-09-12 12:58:43 +0200

    Add ENC;

---
+++ plugins/Kadokawa/plugin_kadokawa/Archives/EncSupport.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Komponent.IO;
+using Kontract.Kompression.Configuration;
+using Kontract.Models.Archive;
+
+namespace plugin_kadokawa.Archives
+{
+    class EncFileEntry
+    {
+        public uint size;
+        public int decompSize;
+        public int offset;
+
+        public bool IsCompressed => (size & 0x80000000) > 0;
+    }
+
+    class EncSupport
+    {
+        public static string DetermineExtension(Stream input)
+        {
+            var magicSamples = CollectMagicSamples(input);
+
+            if (magicSamples.Any(x => x.Contains("CTX")))
+                return ".ctx";
+
+            return ".bin";
+        }
+
+        private static IList<string> CollectMagicSamples(Stream input)
+        {
+            var bkPos = input.Position;
+
+            using var br = new BinaryReaderX(input, true);
+
+            // Get 3 samples to check magic with compression
+            input.Position = bkPos;
+            var magic1 = br.ReadString(4);
+            input.Position = bkPos + 1;
+            var magic2 = br.ReadString(4);
+            input.Position = bkPos + 2;
+            var magic3 = br.ReadString(4);
+
+            return new[] { magic1, magic2, magic3 };
+        }
+    }
+}
--- src/Kontract/Models/Archive/ArchiveFileInfo.cs
+++ src/Kontract/Models/Archive/ArchiveFileInfo.cs
@@ -25,7 +25,7 @@ namespace Kontract.Models.Archive
         /// <summary>
         /// Determines if the FileData is compressed, and has to be handled as such
         /// </summary>
-        protected bool UsesCompression => _configuration != null;
+        public bool UsesCompression => _configuration != null;
 
         /// <summary>
         /// Determines if the content of this file info was modified.
