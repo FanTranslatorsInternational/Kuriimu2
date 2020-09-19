d5f392dca00a039d5f7eeb1f671ab4dbb356cb57
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-09-19 21:38:25 +0200

    REfactor Ps2Disc Load with own code;

--- plugins/Sony/plugin_sony/Archives/Ps2Disc.cs
+++ plugins/Sony/plugin_sony/Archives/Ps2Disc.cs
@@ -1,28 +1,75 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using DiscUtils;
-using DiscUtils.Iso9660;
+using Komponent.IO;
+using Komponent.IO.Streams;
 using Kontract.Models.Archive;
+using Kontract.Models.IO;
 
 namespace plugin_sony.Archives
 {
     class Ps2Disc
     {
+        private const int SectorSize_ = 0x800;
+        private const int DescriptorStart_ = 0x10 * SectorSize_;
+
         public IList<IArchiveFileInfo> Load(Stream input)
         {
-            using var reader = new CDReader(input, false);
-            return EnumerateFiles(reader.Root).ToArray();
+            //using var reader = new CDReader(input, false);
+            //return EnumerateFiles(reader.Root).ToArray();
+
+            using var br = new BinaryReaderX(input, true);
+
+            // Read volume descriptor
+            input.Position = DescriptorStart_;
+            var descriptor = br.ReadType<IsoVolumeDescriptor>();
+
+            // Read directory entries
+            return ReadDirectories(br, descriptor.descriptorPrimary.rootDir).ToArray();
         }
 
-        private IEnumerable<IArchiveFileInfo> EnumerateFiles(DiscDirectoryInfo directoryInfo)
+        private IEnumerable<IArchiveFileInfo> ReadDirectories(BinaryReaderX br, IsoDirEntry directory, string currentPath = "")
         {
-            foreach (var file in directoryInfo.GetFiles())
-                yield return new Ps2DiscArchiveFileInfo(file);
+            var directoryOffset = directory.body.lbaExtent.Value * SectorSize_;
+            var internalOffset = 0x60;  // Skip self entry and parent entry directly
 
-            foreach (var directory in directoryInfo.GetDirectories())
-                foreach (var file in EnumerateFiles(directory))
-                    yield return file;
+            while (PeekByte(br, directoryOffset + internalOffset) != 0)
+            {
+                br.BaseStream.Position = directoryOffset + internalOffset;
+
+                var entry = br.ReadType<IsoDirEntry>();
+                br.BaseStream.Position += 0xE;
+                internalOffset = (int)(br.BaseStream.Position - directoryOffset);
+
+                // Continue reading if the entry points to the current one
+                if (entry.body.lbaExtent.Value == directory.body.lbaExtent.Value)
+                    continue;
+
+                // Read all sub directories and files of this directory
+                if (entry.IsDirectory)
+                {
+                    foreach (var afi in ReadDirectories(br, entry, Path.Combine(currentPath, entry.body.fileName)))
+                        yield return afi;
+                    continue;
+                }
+
+                // Otherwise return file
+                var subStream = new SubStream(br.BaseStream, entry.body.lbaExtent.Value * SectorSize_, entry.body.sizeExtent.Value);
+                yield return new Ps2DiscArchiveFileInfo(subStream, Path.Combine(currentPath, entry.body.fileName), entry);
+            }
+        }
+
+        private byte PeekByte(BinaryReaderX br, long offset)
+        {
+            var bkPos = br.BaseStream.Position;
+
+            br.BaseStream.Position = offset;
+            var value = br.ReadByte();
+
+            br.BaseStream.Position = bkPos;
+
+            return value;
         }
     }
 }
--- plugins/Sony/plugin_sony/Archives/Ps2DiscPlugin.cs
+++ plugins/Sony/plugin_sony/Archives/Ps2DiscPlugin.cs
@@ -3,11 +3,10 @@ using Kontract.Interfaces.Managers;
 using Kontract.Interfaces.Plugins.Identifier;
 using Kontract.Interfaces.Plugins.State;
 using Kontract.Models;
-using Kontract.Models.Context;
 
 namespace plugin_sony.Archives
 {
-    public class Ps2DiscPlugin : IFilePlugin,IRegisterAssembly
+    public class Ps2DiscPlugin : IFilePlugin
     {
         public Guid PluginId => Guid.Parse("c774f77b-4fe4-4550-9ca0-c8967b99eb78");
         public PluginType PluginType => PluginType.Archive;
@@ -22,13 +21,6 @@ namespace plugin_sony.Archives
         public IPluginState CreatePluginState(IPluginManager pluginManager)
         {
             return new Ps2DiscState();
-        }
-
-        public void RegisterAssemblies(DomainContext context)
-        {
-            context.FromResource("plugin_sony.Libs.DiscUtils.Iso9660.dll");
-            context.FromResource("plugin_sony.Libs.DiscUtils.Core.dll");
-            context.FromResource("plugin_sony.Libs.DiscUtils.Streams.dll");
         }
     }
 }
