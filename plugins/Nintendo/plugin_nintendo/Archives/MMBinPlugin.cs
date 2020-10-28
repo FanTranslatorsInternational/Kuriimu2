835e5f6daa127176e1a35f53fc35747ab6868d2e
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-10-28 22:45:48 +0100

    Add MMBin Load/Save;

---
+++ plugins/Nintendo/plugin_nintendo/Archives/MMBinPlugin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+using Kontract.Models.Context;
+using Kontract.Models.IO;
+
+namespace plugin_nintendo.Archives
+{
+    public class MMBinPlugin : IFilePlugin
+    {
+        public Guid PluginId => Guid.Parse("3f6edc1c-215f-4c25-9e06-1bea714e72fe");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] { "*.bin" };
+        public PluginMetadata Metadata { get; }
+
+        public MMBinPlugin()
+        {
+            Metadata = new PluginMetadata("MMBin", "IcySon55", "2D resource from Mario Maker.");
+        }
+
+        public IPluginState CreatePluginState(IPluginManager pluginManager)
+        {
+            return new MMBinState();
+        }
+    }
+}
---
+++ plugins/Nintendo/plugin_nintendo/Archives/MMBinSupport.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Komponent.IO.Attributes;
+
+namespace plugin_nintendo.Archives
+{
+    class MMBinHeader
+    {
+        public int tableSize;
+        public short resourceCount;
+        public short unk1;
+        public int unk2;
+    }
+
+    class MMBinResourceEntry
+    {
+        [FixedLength(0x24)]
+        public string resourceName;
+        public int offset;
+        public int metaSize;
+        public int ctpkSize;
+        [FixedLength(0xC)]
+        public byte[] padding = new byte[0xC];
+    }
+}
