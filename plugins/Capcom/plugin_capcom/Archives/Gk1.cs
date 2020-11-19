78eb76b01cdb0f8b757f87ed3c3b7e734b046b7f
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-11-19 22:50:09 +0100

    Add main archive GK1 Load/Save;

---
+++ plugins/Capcom/plugin_capcom/Archives/Gk1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Komponent.IO;
+using Komponent.IO.Streams;
+using Kontract.Models.Archive;
+
+namespace plugin_capcom.Archives
+{
+    class Gk1
+    {
+        public IList<IArchiveFileInfo> Load(Stream input)
+        {
+            using var br = new BinaryReaderX(input, true);
+
+            br.BaseStream.Position += 8;
+
+            // Read first offset
+            var firstOffset = br.ReadInt32();
+            br.BaseStream.Position -= 4;
+
+            // Read all offsets
+            var offsets = br.ReadMultiple<int>((firstOffset - 8) / 4);
+
+            // Add files
+            var result = new List<IArchiveFileInfo>();
+            for (var i = 0; i < offsets.Count; i++)
+            {
+                br.BaseStream.Position = offsets[i];
+                var fileSize = br.ReadInt32();
+
+                var subStream = new SubStream(input, offsets[i] + 4, fileSize);
+                var fileName = $"{i:00000000}.bin";
+
+                result.Add(new ArchiveFileInfo(subStream, fileName));
+            }
+
+            return result;
+        }
+
+        public void Save(Stream output, IList<IArchiveFileInfo> files)
+        {
+            using var bw = new BinaryWriterX(output);
+
+            // Calculate offsets
+            var entryOffset = 8;
+            var fileOffset = entryOffset + files.Count * 4;
+
+            // Write files
+            var offsets = new List<int>();
+
+            var filePosition = fileOffset;
+            foreach (var file in files.Cast<ArchiveFileInfo>())
+            {
+                offsets.Add(filePosition);
+                output.Position = filePosition;
+
+                // Write size
+                bw.Write((int)file.FileSize);
+
+                // Write file data
+                file.SaveFileData(output);
+
+                filePosition += (int)(4 + file.FileSize);
+            }
+
+            // Write offsets
+            output.Position = entryOffset;
+            bw.WriteMultiple(offsets);
+
+            // Write header data
+            output.Position = 0;
+            bw.Write(fileOffset - 4);
+        }
+    }
+}
---
+++ plugins/Capcom/plugin_capcom/Archives/Gk1Plugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+
+namespace plugin_capcom.Archives
+{
+    public class Gk1Plugin : IFilePlugin
+    {
+        public Guid PluginId => Guid.Parse("5e7d1d34-4106-4d72-8d69-773b2713ae46");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] { "*.bin" };
+        public PluginMetadata Metadata { get; }
+
+        public Gk1Plugin()
+        {
+            Metadata = new PluginMetadata("GK1", "onepiecefreak", "The main resource archive in Gyakuten Kenji 1.");
+        }
+
+        public IPluginState CreatePluginState(IPluginManager pluginManager)
+        {
+            return new Gk1State();
+        }
+    }
+}
