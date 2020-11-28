c06e3268692a5483e3205cc9a56123bdd8766d48
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-11-28 13:19:21 +0100

    Add NCGR Load;

---
+++ plugins/Nintendo/plugin_nintendo/Images/NcgrSupport.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Komponent.IO.Attributes;
+
+namespace plugin_nintendo.Images
+{
+    class NitroCharHeader
+    {
+        [FixedLength(4)]
+        public string magic;
+
+        public int sectionSize;
+        public short tileCountX;
+        public short tileCountY;
+        public int imageFormat;
+        public short unk1;
+        public short unk2;
+        public int tiledFlag;
+        public int tileDataSize;
+        public int unk3;
+    }
+
+    class NitroTtlpHeader
+    {
+        [FixedLength(4)] public string magic;
+        public int sectionSize;
+        public int colorDepth;  // Not depth of the palette colors; Colors are BGR555 always
+        public int unk1;
+        public int paletteSize;
+        public int colorsPerPalette;
+    }
+
+    class NcgrSupport
+    {
+    }
+}
