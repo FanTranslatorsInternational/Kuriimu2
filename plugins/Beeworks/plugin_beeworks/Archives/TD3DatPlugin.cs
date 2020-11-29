52808b36752adb006ec5de9700f2dc9c782d8285
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-11-29 13:47:37 +0100

    Add Touch Detective 3 Load/Save;

---
+++ plugins/Beeworks/plugin_beeworks/Archives/TD3DatPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+
+namespace plugin_beeworks.Archives
+{
+    public class TD3DatPlugin : IFilePlugin
+    {
+        public Guid PluginId => Guid.Parse("adc5ff0e-9857-4a3e-8ccb-3b79c4b6f5e8");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] { "*.dat" };
+        public PluginMetadata Metadata { get; }
+
+        public TD3DatPlugin()
+        {
+            Metadata = new PluginMetadata("Touch Detective 3", "onepiecefreak", "The main resource archive in Touch Detective 3.");
+        }
+
+        public IPluginState CreatePluginState(IPluginManager pluginManager)
+        {
+            return new TD3State();
+        }
+    }
+}
---
+++ plugins/Beeworks/plugin_beeworks/Archives/TD3Support.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Komponent.IO.Attributes;
+
+namespace plugin_beeworks.Archives
+{
+    [Alignment(0x10)]
+    class TD3Header
+    {
+        public int fileCount;
+        public int nameBufSize = 0x40;
+    }
+
+    class TD3Entry
+    {
+        public int offset;
+        public int size;
+        [FixedLength(0x40)]
+        public string fileName;
+    }
+}
