962e4e6e6b1a7b2332685047ebd2ebb70066ac6f
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-11-29 22:49:37 +0100

    Add HED and MRG Load; Add MRG Save;

---
+++ plugins/HuneX/plugin_hunex/Archives/HED.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Komponent.IO;
+using Komponent.IO.Streams;
+using Kontract.Extensions;
+using Kontract.Models.Archive;
+using Kontract.Models.IO;
+
+namespace plugin_hunex.Archives
+{
+    class HED
+    {
+        public IList<IArchiveFileInfo> Load(Stream hedStream, Stream mrgStream, Stream namStream = null)
+        {
+            using var hedBr = new BinaryReaderX(hedStream);
+            using var mrgBr = new BinaryReaderX(mrgStream, true);
+
+            // Determine entry type
+            // HedEntry1 stores the offset as an int and since the first offset is 0, the high 16 bits can only be 0
+            // Otherwise we may deal with smaller entry HedEntry2
+            var firstOffset = hedBr.ReadInt32() & 0xFFFF0000;
+            var entryType = firstOffset > 0 ? typeof(HedEntry2) : typeof(HedEntry1);
+            var nameEntry = firstOffset > 0 ? typeof(NamEntry2) : typeof(NamEntry1);
+
+            // Determine entry count
+            var entrySize = Tools.MeasureType(entryType);
+            var entryCount = (int)((hedStream.Length - 0x10) / entrySize);
+
+            // Read entries
+            hedStream.Position = 0;
+            var entries = hedBr.ReadMultiple(entryCount, entryType);
+
+            // Read names
+            var names = (IList<INamEntry>)Array.Empty<INamEntry>();
+            if (namStream != null)
+            {
+                using var namBr = new BinaryReaderX(namStream);
+                names = namBr.ReadMultiple(entryCount, nameEntry).Cast<INamEntry>().ToArray();
+            }
+
+            // Add files
+            var usedNames = new Dictionary<string, int>();
+
+            var result = new List<IArchiveFileInfo>();
+            for (var i = 0; i < entryCount; i++)
+            {
+                var entry = (IHedEntry)entries[i];
+
+                var subStream = new SubStream(mrgStream, entry.Offset, entry.Size);
+                var fileName = $"{i:00000000}.bin";
+
+                if (names.Count > 0)
+                {
+                    var listName = (UPath)names[i].Name;
+                    if (!usedNames.ContainsKey(listName.FullName))
+                        usedNames[listName.FullName] = 0;
+                    fileName = listName.GetNameWithoutExtension() + $"_{usedNames[listName.FullName]++:00}" + listName.GetExtensionWithDot();
+                }
+
+                result.Add(new ArchiveFileInfo(subStream, fileName));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ plugins/HuneX/plugin_hunex/Archives/MRGSupport.cs
@@ -0,0 +1,41 @@
+﻿using Komponent.IO.Attributes;
+
+namespace plugin_hunex.Archives
+{
+    class MRGHeader
+    {
+        [FixedLength(6)]
+        public string magic = "mrgd00";
+        public short fileCount;
+    }
+
+    class MRGEntry
+    {
+        public ushort sectorOffset;
+        public ushort lowOffset;
+        public ushort sectorCount;
+        public ushort lowSize;
+
+        public int Offset
+        {
+            get => sectorOffset * 0x800 + lowOffset;
+            set
+            {
+                sectorOffset = (ushort)(value / 0x800);
+                lowOffset = (ushort)(value % 0x800);
+            }
+        }
+
+        // HINT: Some entries seem to not correlate with the sectorCount
+        // Sometimes the sectorCount is less than a block above the lowSize, sometimes less than a block below lowSize
+        public int Size
+        {
+            get => (sectorCount - 1) / 0x20 * 0x800 * 0x20 + lowSize;
+            set
+            {
+                lowSize = (ushort)value;
+                sectorCount = (ushort)(((value + 0x7FF) & ~0x7FF) / 0x800);
+            }
+        }
+    }
+}
