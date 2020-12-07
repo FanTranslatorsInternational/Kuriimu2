1e696c33c6a8ed9c178cb5997e3031e2a14134e2
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-12-07 22:47:49 +0100

    Add Mario Party PAC Load;

---
+++ plugins/Nintendo/plugin_nintendo/Archives/Pac.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Komponent.IO;
+using Komponent.IO.Streams;
+using Kontract.Models.Archive;
+using Kontract.Models.IO;
+
+namespace plugin_nintendo.Archives
+{
+    class Pac
+    {
+        private static readonly int EntrySize = Tools.MeasureType(typeof(PacEntry));
+
+        private PacHeader _header;
+
+        public IList<IArchiveFileInfo> Load(Stream input)
+        {
+            using var br = new BinaryReaderX(input, true, ByteOrder.BigEndian);
+
+            // Read header
+            _header = br.ReadType<PacHeader>();
+
+            // Read table info
+            var tableInfo = br.ReadType<PacTableInfo>();
+
+            // Read assets
+            input.Position = tableInfo.assetOffset;
+            var assets = br.ReadMultiple<PacAsset>(tableInfo.assetCount);
+
+            // Read entries
+            input.Position = tableInfo.entryOffset;
+            var entries = br.ReadMultiple<PacEntry>(tableInfo.entryCount);
+
+            // Add files
+            var result = new List<IArchiveFileInfo>();
+            foreach (var asset in assets)
+            {
+                input.Position = asset.stringOffset;
+                var assetName = br.ReadCStringASCII();
+
+                var entryStartCount = (asset.entryOffset - tableInfo.assetOffset) / EntrySize;
+                foreach (var entry in entries.Skip(entryStartCount).Take(asset.count))
+                {
+                    input.Position = entry.stringOffset;
+                    var entryName = br.ReadCStringASCII();
+
+                    var subStream = new SubStream(input, entry.offset, entry.compSize);
+                    var fileName = assetName + "/" + entryName;
+
+                    result.Add(new PacArchiveFileInfo(subStream, fileName, entry, Kompression.Implementations.Compressions.ZLib, entry.decompSize));
+                }
+            }
+
+            return result;
+        }
+
+        public void Save(Stream output, IList<IArchiveFileInfo> files)
+        {
+
+        }
+    }
+}
---
+++ plugins/Nintendo/plugin_nintendo/Archives/PacPlugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Komponent.IO;
+using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+using Kontract.Models.Context;
+using Kontract.Models.IO;
+
+namespace plugin_nintendo.Archives
+{
+    public class PacPlugin:IFilePlugin,IIdentifyFiles
+    {
+        public Guid PluginId { get; }
+        public PluginType PluginType { get; }
+        public string[] FileExtensions { get; }
+        public PluginMetadata Metadata { get; }
+
+        public PacPlugin()
+        {
+            Metadata=new PluginMetadata("PAC","onepiecefreak","The main resource in Mario Party 10.");
+        }
+
+        public async Task<bool> IdentifyAsync(IFileSystem fileSystem, UPath filePath, IdentifyContext identifyContext)
+        {
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+
+            using var br=new BinaryReaderX(fileStream);
+            return br.ReadString(3) == "PAC";
+        }
+
+        public IPluginState CreatePluginState(IPluginManager pluginManager)
+        {
+            return new PacState();
+        }
+    }
+}
