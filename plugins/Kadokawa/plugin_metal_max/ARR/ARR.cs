c1ff07ceff2488e0df2c43867e399a08e64101cf
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-12-07 20:55:53 +0100

    Upgrade metal max plugin to new API;

--- plugins/Kadokawa/plugin_metal_max/ARR/ARR.cs
+++ plugins/Kadokawa/plugin_metal_max/ARR/ARR.cs
@@ -1,116 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using Komponent.IO;
-using Kontract.Interfaces.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using Komponent.IO;
+//using Kontract.Interfaces.Text;
 
-namespace plugin_metal_max.ARR
-{
-    public sealed class ARR
-    {
-        /// <summary>
-        /// The list of text entries in the file.
-        /// </summary>
-        public List<TextEntry> Entries { get; set; } = new List<TextEntry>();
+//namespace plugin_metal_max.ARR
+//{
+//    public sealed class ARR
+//    {
+//        /// <summary>
+//        /// The list of text entries in the file.
+//        /// </summary>
+//        public List<TextEntry> Entries { get; set; } = new List<TextEntry>();
 
-        /// <summary>
-        /// Read an ARR file into memory.
-        /// </summary>
-        /// <param name="input">A readable stream of an ARR file.</param>
-        public ARR(Stream input)
-        {
-            using (var br = new BinaryReaderX(input))
-            {
-                var count = br.ReadInt32();
+//        /// <summary>
+//        /// Read an ARR file into memory.
+//        /// </summary>
+//        /// <param name="input">A readable stream of an ARR file.</param>
+//        public ARR(Stream input)
+//        {
+//            using (var br = new BinaryReaderX(input))
+//            {
+//                var count = br.ReadInt32();
 
-                var pointers = new List<short>();
+//                var pointers = new List<short>();
 
-                for (var i = 0; i < count; i++)
-                {
-                    for (var j = 0; j < 7; j++)
-                    {
-                        var p = br.ReadInt16();
-                        if (p == 0) continue;
-                        pointers.Add(p);
-                    }
-                }
+//                for (var i = 0; i < count; i++)
+//                {
+//                    for (var j = 0; j < 7; j++)
+//                    {
+//                        var p = br.ReadInt16();
+//                        if (p == 0) continue;
+//                        pointers.Add(p);
+//                    }
+//                }
 
-                for (var i = 0; i < pointers.Count; i++)
-                {
-                    br.BaseStream.Position = pointers[i];
+//                for (var i = 0; i < pointers.Count; i++)
+//                {
+//                    br.BaseStream.Position = pointers[i];
 
-                    short b = 0;
-                    var chars = new List<byte>();
-                    do
-                    {
-                        b = br.ReadInt16();
-                        chars.AddRange(BitConverter.GetBytes(b));
-                    }
-                    while (b != 0);
+//                    short b = 0;
+//                    var chars = new List<byte>();
+//                    do
+//                    {
+//                        b = br.ReadInt16();
+//                        chars.AddRange(BitConverter.GetBytes(b));
+//                    }
+//                    while (b != 0);
 
-                    var str = Encoding.Unicode.GetString(chars.ToArray());
+//                    var str = Encoding.Unicode.GetString(chars.ToArray());
 
-                    Entries.Add(new TextEntry { Name = (i + 1).ToString(), EditedText = str.TrimEnd('\0') });
-                }
-            }
-        }
+//                    Entries.Add(new TextEntry { Name = (i + 1).ToString(), EditedText = str.TrimEnd('\0') });
+//                }
+//            }
+//        }
 
-        /// <summary>
-        /// Write an ARR file to disk.
-        /// </summary>
-        /// <param name="output">A writable stream of an ARR file.</param>
-        public void Save(Stream output)
-        {
-            using (var bw = new BinaryWriterX(output))
-            {
-                const int pointerGroupSize = 6;
-                var missingPointers = pointerGroupSize - (Entries.Count % pointerGroupSize == 0 ? pointerGroupSize : Entries.Count % pointerGroupSize);
+//        /// <summary>
+//        /// Write an ARR file to disk.
+//        /// </summary>
+//        /// <param name="output">A writable stream of an ARR file.</param>
+//        public void Save(Stream output)
+//        {
+//            using (var bw = new BinaryWriterX(output))
+//            {
+//                const int pointerGroupSize = 6;
+//                var missingPointers = pointerGroupSize - (Entries.Count % pointerGroupSize == 0 ? pointerGroupSize : Entries.Count % pointerGroupSize);
 
-                for (var i = 0; i < missingPointers; i++)
-                    Entries.Add(new TextEntry { Name = (Entries.Count + 1).ToString() });
+//                for (var i = 0; i < missingPointers; i++)
+//                    Entries.Add(new TextEntry { Name = (Entries.Count + 1).ToString() });
 
-                var pointerGroupCount = Entries.Count / pointerGroupSize;
-                var textStartOffset = pointerGroupCount * 14 + 4;
+//                var pointerGroupCount = Entries.Count / pointerGroupSize;
+//                var textStartOffset = pointerGroupCount * 14 + 4;
 
-                var pointerList = new List<short>();
-                var strings = new List<(string Text, short Pointer)>();
+//                var pointerList = new List<short>();
+//                var strings = new List<(string Text, short Pointer)>();
 
-                bw.BaseStream.Position = textStartOffset;
+//                bw.BaseStream.Position = textStartOffset;
 
-                foreach (var entry in Entries)
-                {
-                    if (entry.EditedText != string.Empty)
-                    {
-                        var offset = (short)bw.BaseStream.Position;
+//                foreach (var entry in Entries)
+//                {
+//                    if (entry.EditedText != string.Empty)
+//                    {
+//                        var offset = (short)bw.BaseStream.Position;
 
-                        if (strings.All(s => s.Text != entry.EditedText))
-                        {
-                            strings.Add((entry.EditedText, offset));
-                            pointerList.Add(offset);
-                            bw.Write(Encoding.Unicode.GetBytes(entry.EditedText));
-                            bw.Write((byte)0x0);
-                            bw.Write((byte)0x0);
-                        }
-                        else
-                            pointerList.Add(strings.First(s => s.Text == entry.EditedText).Pointer);
-                    }
-                    else
-                        pointerList.Add((short)(textStartOffset - 2));
-                }
+//                        if (strings.All(s => s.Text != entry.EditedText))
+//                        {
+//                            strings.Add((entry.EditedText, offset));
+//                            pointerList.Add(offset);
+//                            bw.Write(Encoding.Unicode.GetBytes(entry.EditedText));
+//                            bw.Write((byte)0x0);
+//                            bw.Write((byte)0x0);
+//                        }
+//                        else
+//                            pointerList.Add(strings.First(s => s.Text == entry.EditedText).Pointer);
+//                    }
+//                    else
+//                        pointerList.Add((short)(textStartOffset - 2));
+//                }
 
-                bw.BaseStream.Position = 0;
+//                bw.BaseStream.Position = 0;
 
-                bw.Write(pointerGroupCount);
+//                bw.Write(pointerGroupCount);
 
-                for (var i = 0; i < pointerList.Count; i++)
-                {
-                    if (i != 0 && i % pointerGroupSize == 0)
-                        bw.Write((short)0);
-                    bw.Write(pointerList[i]);
-                }
-            }
-        }
-    }
-}
+//                for (var i = 0; i < pointerList.Count; i++)
+//                {
+//                    if (i != 0 && i % pointerGroupSize == 0)
+//                        bw.Write((short)0);
+//                    bw.Write(pointerList[i]);
+//                }
+//            }
+//        }
+//    }
+//}
--- plugins/Kadokawa/plugin_metal_max/NAM/NamAdapter.cs
+++ plugins/Kadokawa/plugin_metal_max/NAM/NamAdapter.cs
@@ -1,52 +1,52 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.IO;
-using Kontract.Attributes;
-using Kontract.Interfaces;
-using Kontract.Interfaces.Common;
-using Kontract.Interfaces.Text;
+﻿//using System.Collections.Generic;
+//using System.ComponentModel.Composition;
+//using System.IO;
+//using Kontract.Attributes;
+//using Kontract.Interfaces;
+//using Kontract.Interfaces.Common;
+//using Kontract.Interfaces.Text;
 
-namespace plugin_metal_max.NAM
-{
-    [Export(typeof(NamAdapter))]
-    [Export(typeof(IPlugin))]
-    [PluginInfo("18F83F20-3FB7-46DC-9028-EE00D658F029", "Metal Max 3: NAM Text", "NAM", "IcySon55", "", "This is the Metal Max 3 NAM text adapter for Kuriimu2.")]
-    [PluginExtensionInfo("*.nam")]
-    public sealed class NamAdapter : ITextAdapter, ILoadFiles, ISaveFiles
-    {
-        private NAM _format;
+//namespace plugin_metal_max.NAM
+//{
+//    [Export(typeof(NamAdapter))]
+//    [Export(typeof(IPlugin))]
+//    [PluginInfo("18F83F20-3FB7-46DC-9028-EE00D658F029", "Metal Max 3: NAM Text", "NAM", "IcySon55", "", "This is the Metal Max 3 NAM text adapter for Kuriimu2.")]
+//    [PluginExtensionInfo("*.nam")]
+//    public sealed class NamAdapter : ITextAdapter, ILoadFiles, ISaveFiles
+//    {
+//        private NAM _format;
 
-        #region Properties
+//        #region Properties
 
-        public IEnumerable<TextEntry> Entries => _format?.Entries;
+//        public IEnumerable<TextEntry> Entries => _format?.Entries;
 
-        public string NameFilter => @".*";
+//        public string NameFilter => @".*";
 
-        public int NameMaxLength => 0;
+//        public int NameMaxLength => 0;
 
-        public string LineEndings { get; set; } = "\n";
+//        public string LineEndings { get; set; } = "\n";
 
-        public bool LeaveOpen { get; set; }
+//        public bool LeaveOpen { get; set; }
 
-        #endregion
+//        #endregion
 
-        public void Load(StreamInfo input)
-        {
-            var arrFile = Path.ChangeExtension(input.FileName, ".ARR");
+//        public void Load(StreamInfo input)
+//        {
+//            var arrFile = Path.ChangeExtension(input.FileName, ".ARR");
 
-            if (File.Exists(input.FileName) && File.Exists(arrFile))
-                _format = new NAM(input.FileData, File.OpenRead(arrFile), input.FileName);
-            else if (File.Exists(input.FileName))
-                _format = new NAM(input.FileData, null, input.FileName);
-        }
+//            if (File.Exists(input.FileName) && File.Exists(arrFile))
+//                _format = new NAM(input.FileData, File.OpenRead(arrFile), input.FileName);
+//            else if (File.Exists(input.FileName))
+//                _format = new NAM(input.FileData, null, input.FileName);
+//        }
 
-        public void Save(StreamInfo output, int versionIndex = 0)
-        {
-            var arrFile = Path.ChangeExtension(output.FileName, ".ARR");
+//        public void Save(StreamInfo output, int versionIndex = 0)
+//        {
+//            var arrFile = Path.ChangeExtension(output.FileName, ".ARR");
 
-            _format.Save(output.FileData);
-        }
+//            _format.Save(output.FileData);
+//        }
 
-        public void Dispose() { }
-    }
-}
+//        public void Dispose() { }
+//    }
+//}
