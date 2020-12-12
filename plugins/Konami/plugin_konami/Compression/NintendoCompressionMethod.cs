59f57b37405cd0bb37b8044d716c8495cca6a3b9
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-12-12 19:00:05 +0100

    Add TARC Load/Save;

---
+++ plugins/Konami/plugin_konami/Compression/NintendoCompressionMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plugin_konami.Compression
+{
+    enum NintendoCompressionMethod : byte
+    {
+        Lz10 = 0x10,
+        Lz11 = 0x11,
+        Lz40 = 0x40,
+        Lz60 = 0x60,
+        Huffman4 = 0x24,
+        Huffman8 = 0x28,
+        Rle = 0x30
+    }
+}
