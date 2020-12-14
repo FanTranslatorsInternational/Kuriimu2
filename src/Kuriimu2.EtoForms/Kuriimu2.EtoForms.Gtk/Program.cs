dd1370ecabc3ad87fa592c55f02aa200c845f3cb
onepiecefreak3 <alberteinstein1980@gmx.de>
2020-12-14 13:12:26 +0100

    Add Eto.Forms solution;

---
+++ src/Kuriimu2.EtoForms/Kuriimu2.EtoForms.Gtk/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Eto.Forms;
+
+namespace Kuriimu2.EtoForms.Gtk
+{
+    class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platforms.Gtk).Run(new MainForm());
+        }
+    }
+}
---
+++ src/Kuriimu2.EtoForms/Kuriimu2.EtoForms/MainForm.cs
@@ -0,0 +1,14 @@
+using System;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace Kuriimu2.EtoForms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
