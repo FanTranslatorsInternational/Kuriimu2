40ee56672f6542863b8a6cbefa8574f29b6fba34
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-03-05 22:22:09 +0100

    Add JARC Load/Save;

---
+++ plugins/BandaiNamco/plugin_ganbarion/Archives/JarcSupport.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using Komponent.IO.Attributes;
+using Kontract.Kompression.Configuration;
+using Kontract.Models.Archive;
+
+namespace plugin_ganbarion.Archives
+{
+    class JarcHeader
+    {
+        [FixedLength(4)]
+        public string magic = "jARC";
+        public int fileSize;
+        public int unk1;
+        public int fileCount;
+    }
+
+    class JarcEntry
+    {
+        public int fileOffset;
+        public int fileSize;
+        public int nameOffset;
+        public uint hash;
+        public int unk1;
+    }
+
+    class JarcArchiveFileInfo : ArchiveFileInfo
+    {
+        public JarcEntry Entry { get; }
+
+        public JarcArchiveFileInfo(Stream fileData, string filePath,JarcEntry entry) : base(fileData, filePath)
+        {
+            Entry = entry;
+        }
+
+        public JarcArchiveFileInfo(Stream fileData, string filePath, IKompressionConfiguration configuration, long decompressedSize, JarcEntry entry) : base(fileData, filePath, configuration, decompressedSize)
+        {
+            Entry = entry;
+        }
+    }
+}
