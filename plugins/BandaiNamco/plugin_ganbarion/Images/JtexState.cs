ced0a7765a55beda4f377e42418c6d1bdc09a5b4
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-03-05 20:36:15 +0100

    Add jIMG Load/Save;

---
+++ plugins/BandaiNamco/plugin_ganbarion/Images/JtexState.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Kanvas;
+using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Kanvas;
+using Kontract.Models.Context;
+using Kontract.Models.Image;
+using Kontract.Models.IO;
+
+namespace plugin_ganbarion.Images
+{
+    class JtexState : IImageState, ILoadFiles, ISaveFiles
+    {
+        private Jtex _jtex;
+
+        public EncodingDefinition EncodingDefinition { get; }
+        public IList<IKanvasImage> Images { get; private set; }
+
+        public bool ContentChanged => IsContentChanged();
+
+        public JtexState()
+        {
+            _jtex = new Jtex();
+
+            EncodingDefinition = JtexSupport.GetEncodingDefinition();
+        }
+
+        public async Task Load(IFileSystem fileSystem, UPath filePath, LoadContext loadContext)
+        {
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+            Images = new IKanvasImage[] { new KanvasImage(EncodingDefinition, _jtex.Load(fileStream)) };
+        }
+
+        public Task Save(IFileSystem fileSystem, UPath savePath, SaveContext saveContext)
+        {
+            var fileStream = fileSystem.OpenFile(savePath, FileMode.Create, FileAccess.Write);
+            _jtex.Save(fileStream, Images[0].ImageInfo);
+
+            return Task.CompletedTask;
+        }
+
+        private bool IsContentChanged()
+        {
+            return Images.Any(x => x.ImageInfo.ContentChanged);
+        }
+    }
+}
---
+++ plugins/BandaiNamco/plugin_ganbarion/Images/JtexSupport.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Kanvas.Encoding;
+using Komponent.IO.Attributes;
+using Kontract.Kanvas;
+using Kontract.Models.Image;
+
+namespace plugin_ganbarion.Images
+{
+    class JtexHeader
+    {
+        [FixedLength(4)] 
+        public string magic = "jIMG";
+        public int fileSize;
+        public short paddedWidth;
+        public short paddedHeight;
+        public byte format;
+        public byte orientation;
+        public short unk1;
+        public int unk2;
+        public int unk3;
+        public int dataSize;
+        public int unk4;
+        public int unk5;
+        public int unk6;
+        public int unk7;
+        public short width;
+        public short height;
+    }
+
+    class JtexSupport
+    {
+        private static readonly IDictionary<int, IColorEncoding> Formats = new Dictionary<int, IColorEncoding>
+        {
+            [8] = new Etc1(false, true),
+            [11] = new Etc1(true, true)
+        };
+
+        public static EncodingDefinition GetEncodingDefinition()
+        {
+            var definition = new EncodingDefinition();
+            definition.AddColorEncodings(Formats);
+
+            return definition;
+        }
+    }
+}
