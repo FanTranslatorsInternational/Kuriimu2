13d9e84ffa0d77ddca5ea8b3f9761e41b43a1672
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-03-05 22:50:58 +0100

    Add CVT Load/Save;

---
+++ plugins/ArcSystemWorks/plugin_arc_system_works/Images/CvtSupport.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Kanvas.Encoding;
+using Komponent.IO.Attributes;
+using Kontract.Kanvas;
+using Kontract.Models.Image;
+
+namespace plugin_arc_system_works.Images
+{
+    class CvtHeader
+    {
+        [FixedLength(2)]
+        public string magic="n\0";
+
+        public short width;
+        public short height;
+        public short format;
+        public int unk1;
+        [FixedLength(0x20)]
+        public string name;
+
+        public int unk2;
+        public int unk3;
+    }
+
+    public class CvtSupport
+    {
+        private static readonly IDictionary<int, IColorEncoding> Formats = new Dictionary<int, IColorEncoding>
+        {
+            [0x1006] = new Etc1(true, true)
+        };
+
+        public static EncodingDefinition GetEncodingDefinition()
+        {
+            var definition = new EncodingDefinition();
+            definition.AddColorEncodings(Formats);
+
+            return definition;
+        }
+    }
+}
