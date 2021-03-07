a1ecea0f672b3c27f33e2221f61b8b018e636cc7
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-03-07 22:40:28 +0100

    Add MtTex Load/Save (all previously known platforms);

---
+++ plugins/Capcom/plugin_mt_framework/Images/MtTex.cs
@@ -0,0 +1,408 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using Kanvas.Swizzle;
+using Komponent.IO;
+using Kontract.Models.Image;
+using Kontract.Models.IO;
+
+namespace plugin_mt_framework.Images
+{
+    // TODO: Platofrm.Mobile may only expose one image; For 0x0C expose DXT5 image and internally use KanvasImage for transcoding to ATC and PVRTC
+    class MtTex
+    {
+        private const int HeaderSize_ = 0x10;
+
+        private MtTexPlatform _platform;
+        private MtTexHeader _header;
+        private MobileMtTexHeader _mobileHeader;
+
+        public IList<ImageInfo> Load(Stream input, MtTexPlatform platform)
+        {
+            _platform = platform;
+            using var br = new BinaryReaderX(input);
+
+            // Determine byte order
+            if (br.PeekString() == "\0XET")
+                br.ByteOrder = ByteOrder.BigEndian;
+
+            // Read header
+            _header = br.ReadType<MtTexHeader>();
+
+            input.Position = 0;
+            _mobileHeader = br.ReadType<MobileMtTexHeader>();
+
+            switch (platform)
+            {
+                case MtTexPlatform.N3DS:
+                    return new[] { Load3ds(br) };
+
+                case MtTexPlatform.PS3:
+                    return new[] { LoadPs3(br) };
+
+                case MtTexPlatform.Switch:
+                    return new[] { LoadSwitch(br) };
+
+                case MtTexPlatform.Mobile:
+                    return LoadMobile(br);
+
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        public void Save(Stream output, IList<ImageInfo> imageInfos)
+        {
+            var byteOrder = _header.magic == "\0XET" ? ByteOrder.BigEndian : ByteOrder.LittleEndian;
+            using var bw = new BinaryWriterX(output, byteOrder);
+
+            switch (_platform)
+            {
+                case MtTexPlatform.N3DS:
+                    Save3ds(bw, imageInfos[0]);
+                    break;
+
+                case MtTexPlatform.PS3:
+                    SavePs3(bw, imageInfos[0]);
+                    break;
+
+                case MtTexPlatform.Switch:
+                    SaveSwitch(bw, imageInfos[0]);
+                    break;
+
+                case MtTexPlatform.Mobile:
+                    SaveMobile(bw, imageInfos);
+                    break;
+            }
+        }
+
+        #region Load
+
+        private ImageInfo Load3ds(BinaryReaderX br)
+        {
+            // Skip mip offsets
+            IList<int> mipOffsets = Array.Empty<int>();
+            if (_header.version != 0xA4)
+                mipOffsets = br.ReadMultiple<int>(_header.mipCount);
+
+            // Read image data
+            var bitDepth = MtTexSupport.CtrFormats[_header.format].BitDepth;
+            var dataSize = _header.width * _header.height * bitDepth / 8;
+
+            if (_header.version != 0xA4) br.BaseStream.Position = HeaderSize_ + _header.mipCount * 4 + mipOffsets[0];
+            var imageData = br.ReadBytes(dataSize);
+
+            // Read mips
+            var mipData = new List<byte[]>();
+            for (var i = 1; i < _header.mipCount; i++)
+            {
+                var mipSize = (_header.width >> i) * (_header.height >> i) * bitDepth / 8;
+
+                if (_header.version != 0xA4) br.BaseStream.Position = HeaderSize_ + _header.mipCount * 4 + mipOffsets[i];
+                mipData.Add(br.ReadBytes(mipSize));
+            }
+
+            // Create image info
+            var imageInfo = new ImageInfo(imageData, _header.format, new Size(_header.width, _header.height));
+
+            if (_header.mipCount > 1)
+                imageInfo.MipMapData = mipData;
+
+            imageInfo.RemapPixels.With(context => new CtrSwizzle(context));
+
+            return imageInfo;
+        }
+
+        private ImageInfo LoadPs3(BinaryReaderX br)
+        {
+            // Skip mip offsets
+            var mipOffsets = br.ReadMultiple<int>(_header.mipCount);
+
+            // Read image data
+            var bitDepth = MtTexSupport.Ps3Formats[_header.format].BitDepth;
+            var colorsPerValue = MtTexSupport.Ps3Formats[_header.format].ColorsPerValue;
+            var dataSize = _header.width * _header.height * bitDepth / 8;
+
+            br.BaseStream.Position = mipOffsets[0];
+            var imageData = br.ReadBytes(dataSize);
+
+            // Read mips
+            var mipData = new List<byte[]>();
+            for (var i = 1; i < _header.mipCount; i++)
+            {
+                var mipSize = (_header.width >> i) * (_header.height >> i) * bitDepth / 8;
+
+                br.BaseStream.Position = mipOffsets[i];
+                mipData.Add(br.ReadBytes(mipSize));
+            }
+
+            // Create image info
+            var imageInfo = new ImageInfo(imageData, _header.format, new Size(_header.width, _header.height));
+
+            if (_header.mipCount > 1)
+                imageInfo.MipMapData = mipData;
+
+            // TODO: Remove block swizzle with pre-swizzle implementation in Kanvas
+            if (colorsPerValue > 1)
+                imageInfo.RemapPixels.With(context => new BcSwizzle(context));
+
+            return imageInfo;
+        }
+
+        private ImageInfo LoadSwitch(BinaryReaderX br)
+        {
+            // TODO: Find samples for this behaviour
+            // Skip unknown data between header and mipOffsets
+            var texSize = br.ReadUInt32();
+            //if (texSize > br.BaseStream.Length)
+            //{
+            //    br.BaseStream.Position -= 4;
+            //    _switchUnkData1 = br.ReadBytes(0x6C);
+
+            //    texSize = br.ReadUInt32();
+            //}
+
+            // Skip mip offsets
+            br.ReadMultiple<int>(_header.mipCount);
+
+            // Read image data
+            // HINT: Calculating dataSize by bitsPerValue and colorsPerValue, since bitDepth can be 0 or some float due to ASTC
+            var bitsPerValue = MtTexSupport.SwitchFormats[_header.format].BitsPerValue;
+            var colorsPerValue = MtTexSupport.SwitchFormats[_header.format].ColorsPerValue;
+            var dataSize = _header.width * _header.height / colorsPerValue * bitsPerValue / 8;
+            var imageData = br.ReadBytes(dataSize);
+
+            // Read mips
+            var mipData = new List<byte[]>();
+            for (var i = 1; i < _header.mipCount; i++)
+            {
+                var mipSize = (_header.width >> i) * (_header.height >> i) / colorsPerValue * bitsPerValue / 8;
+                mipData.Add(br.ReadBytes(mipSize));
+            }
+
+            // Create image info
+            var imageInfo = new ImageInfo(imageData, _header.format, new Size(_header.width, _header.height));
+
+            if (_header.mipCount > 1)
+                imageInfo.MipMapData = mipData;
+
+            imageInfo.RemapPixels.With(context => new NxSwizzle(context));
+
+            return imageInfo;
+        }
+
+        private IList<ImageInfo> LoadMobile(BinaryReaderX br)
+        {
+            var texOffsets = br.ReadMultiple<int>(3);
+            var texSizes = br.ReadMultiple<int>(3);
+
+            var formats = _mobileHeader.format == 0x0C ?
+                new byte[] { 0xFD, 0xFE, 0xFF } :
+                new[] { _mobileHeader.format, _mobileHeader.format, _mobileHeader.format };
+
+            var infos = new List<ImageInfo>();
+            for (var i = 0; i < 3; i++)
+            {
+                if (i > 0 && texOffsets[i] == texOffsets[0])
+                    continue;
+
+                // Read image data
+                var bitDepth = MtTexSupport.MobileFormats[formats[i]].BitDepth;
+                var colorsPerValue = MtTexSupport.MobileFormats[formats[i]].ColorsPerValue;
+                var dataSize = _mobileHeader.width * _mobileHeader.height * bitDepth / 8;
+
+                br.BaseStream.Position = texOffsets[i];
+                var imageData = br.ReadBytes(dataSize);
+
+                // Read mips
+                var mipData = new List<byte[]>();
+                for (var j = 1; j < _mobileHeader.mipCount; j++)
+                {
+                    var mipSize = (_mobileHeader.width >> j) * (_mobileHeader.height >> j) * bitDepth / 8;
+                    mipData.Add(br.ReadBytes(mipSize));
+                }
+
+                // Add image info
+                var imageInfo = new ImageInfo(imageData, formats[i], new Size(_mobileHeader.width, _mobileHeader.height));
+
+                if (_mobileHeader.mipCount > 1)
+                    imageInfo.MipMapData = mipData;
+
+                // TODO: Remove block swizzle with pre-swizzle implementation in Kanvas
+                if (colorsPerValue > 1)
+                    imageInfo.RemapPixels.With(context => new BcSwizzle(context));
+
+                infos.Add(imageInfo);
+            }
+
+            return infos;
+        }
+
+        #endregion
+
+        #region Save
+
+        private void Save3ds(BinaryWriterX bw, ImageInfo imageInfo)
+        {
+            bw.BaseStream.Position = HeaderSize_;
+
+            // Write mip offsets
+            if (_header.version != 0xA4)
+            {
+                var mipPosition = 0;
+                bw.Write(mipPosition);
+                mipPosition += imageInfo.ImageData.Length;
+
+                if (imageInfo.MipMapCount > 1)
+                    foreach (var mipData in imageInfo.MipMapData)
+                    {
+                        bw.Write(mipPosition);
+                        mipPosition += mipData.Length;
+                    }
+            }
+
+            // Write image data
+            bw.Write(imageInfo.ImageData);
+
+            foreach (var mipData in imageInfo.MipMapData)
+                bw.Write(mipData);
+
+            // Update header
+            _header.format = (byte)imageInfo.ImageFormat;
+            _header.width = (short)imageInfo.ImageSize.Width;
+            _header.height = (short)imageInfo.ImageSize.Height;
+            _header.mipCount = (byte)(imageInfo.MipMapCount + 1);
+
+            // Write header
+            bw.BaseStream.Position = 0;
+            bw.WriteType(_header);
+        }
+
+        private void SavePs3(BinaryWriterX bw, ImageInfo imageInfo)
+        {
+            bw.BaseStream.Position = HeaderSize_;
+
+            // Write mip offsets
+            var mipPosition = HeaderSize_ + (imageInfo.MipMapCount + 1) * 4;
+            bw.Write(mipPosition);
+            mipPosition += imageInfo.ImageData.Length;
+
+            if (imageInfo.MipMapCount > 1)
+                foreach (var mipData in imageInfo.MipMapData)
+                {
+                    bw.Write(mipPosition);
+                    mipPosition += mipData.Length;
+                }
+
+            // Write image data
+            bw.Write(imageInfo.ImageData);
+
+            foreach (var mipData in imageInfo.MipMapData)
+                bw.Write(mipData);
+
+            // Update header
+            _header.format = (byte)imageInfo.ImageFormat;
+            _header.width = (short)imageInfo.ImageSize.Width;
+            _header.height = (short)imageInfo.ImageSize.Height;
+            _header.mipCount = (byte)(imageInfo.MipMapCount + 1);
+
+            // Write header
+            bw.BaseStream.Position = 0;
+            bw.WriteType(_header);
+        }
+
+        private void SaveSwitch(BinaryWriterX bw, ImageInfo imageInfo)
+        {
+            bw.BaseStream.Position = HeaderSize_;
+
+            // Write total tex size
+            bw.Write(imageInfo.ImageData.Length + imageInfo.MipMapData.Sum(m => m.Length));
+
+            // Write mip offsets
+            var mipPosition = 0;
+            bw.Write(mipPosition);
+            mipPosition += imageInfo.ImageData.Length;
+
+            if (imageInfo.MipMapCount > 1)
+                foreach (var mipData in imageInfo.MipMapData)
+                {
+                    bw.Write(mipPosition);
+                    mipPosition += mipData.Length;
+                }
+
+            // Write image data
+            bw.Write(imageInfo.ImageData);
+
+            foreach (var mipData in imageInfo.MipMapData)
+                bw.Write(mipData);
+
+            // Update header
+            _header.format = (byte)imageInfo.ImageFormat;
+            _header.width = (short)imageInfo.ImageSize.Width;
+            _header.height = (short)imageInfo.ImageSize.Height;
+            _header.mipCount = (byte)(imageInfo.MipMapCount + 1);
+
+            // Write header
+            bw.BaseStream.Position = 0;
+            bw.WriteType(_header);
+        }
+
+        private void SaveMobile(BinaryWriterX bw, IList<ImageInfo> imageInfos)
+        {
+            var texOffsets = new List<int>();
+            var texSizes = new List<int>();
+
+            // Write image data
+            bw.BaseStream.Position = HeaderSize_ + 0x18;
+
+            for (var i = 0; i < imageInfos.Count; i++)
+            {
+                var imageInfo = imageInfos[i];
+
+                if (i > 0 && imageInfo.ImageFormat < 0xFD)
+                {
+                    texOffsets.Add(texOffsets[0]);
+                    texSizes.Add(0);
+                }
+
+                var texOffset = (int)bw.BaseStream.Position;
+                texOffsets.Add(texOffset);
+
+                bw.Write(imageInfo.ImageData);
+
+                if (imageInfo.MipMapCount > 1)
+                    foreach (var mipData in imageInfo.MipMapData)
+                        bw.Write(mipData);
+
+                if (imageInfo.ImageFormat >= 0xFD)
+                    texSizes.Add((int)bw.BaseStream.Length - texOffset);
+                else texSizes.Add(0);
+            }
+
+            // Pad tex lists
+            while (texOffsets.Count < 3)
+                texOffsets.Add(texOffsets[0]);
+            while (texSizes.Count < 3)
+                texSizes.Add(0);
+
+            // Write tex offsets and sizes
+            bw.BaseStream.Position = HeaderSize_;
+            bw.WriteMultiple(texOffsets);
+            bw.WriteMultiple(texSizes);
+
+            // Update header
+            _mobileHeader.format = imageInfos[0].ImageFormat == 0xFD ? (byte)0x0C : (byte)imageInfos[0].ImageFormat;
+            _mobileHeader.width = (short)imageInfos[0].ImageSize.Width;
+            _mobileHeader.height = (short)imageInfos[0].ImageSize.Height;
+            _mobileHeader.mipCount = (byte)(imageInfos[0].MipMapCount + 1);
+
+            // Write header
+            bw.BaseStream.Position = 0;
+            bw.WriteType(_mobileHeader);
+        }
+
+        #endregion
+    }
+}
---
+++ plugins/Capcom/plugin_mt_framework/Images/MtTexSupport.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using Kanvas;
+using Komponent.IO;
+using Komponent.IO.Attributes;
+using Kontract.Kanvas;
+using Kontract.Models.Image;
+using Kontract.Models.IO;
+
+namespace plugin_mt_framework.Images
+{
+    [BitFieldInfo(BitOrder = BitOrder.LeastSignificantBitFirst, BlockSize = 4)]
+    class MtTexHeader
+    {
+        [FixedLength(4)]
+        public string magic;
+
+        [BitField(12)]
+        public short version;
+        [BitField(12)]
+        public short unk1;
+        [BitField(4)]
+        public byte reserved1;
+        [BitField(4)]
+        public byte alphaFlags;
+
+        [BitField(6)]
+        public byte mipCount;
+        [BitField(13)]
+        public short width;
+        [BitField(13)]
+        public short height;
+
+        [BitField(8)]
+        public byte unk2;
+        [BitField(8)]
+        public byte format;
+        [BitField(16)]
+        public ushort unk3;
+    }
+
+    [BitFieldInfo(BitOrder = BitOrder.LeastSignificantBitFirst, BlockSize = 4)]
+    class MobileMtTexHeader
+    {
+        [FixedLength(4)]
+        public string magic;
+
+        [BitField(16)]
+        public short version;
+        [BitField(8)]
+        public byte format;
+        [BitField(8)]
+        public byte unk1;
+
+        [BitField(4)]
+        public byte unk2;
+        [BitField(28)]
+        public int r1;
+
+        [BitField(13)]
+        public short width;
+        [BitField(13)]
+        public short height;
+        [BitField(4)]
+        public byte mipCount;
+        [BitField(2)]
+        public byte unk3;
+    }
+
+    class MtTexSupport
+    {
+        public static readonly IDictionary<int, IColorEncoding> CtrFormats = new Dictionary<int, IColorEncoding>
+        {
+            [0x01] = ImageFormats.Rgba4444(),
+            [0x02] = ImageFormats.Rgba5551(),
+            [0x03] = ImageFormats.Rgba8888(),
+            [0x04] = ImageFormats.Rgb565(),
+            [0x05] = ImageFormats.A8(),
+            [0x06] = ImageFormats.L8(),
+            [0x07] = ImageFormats.La88(),
+
+            [0x0A] = ImageFormats.Rg88(),
+            [0x0B] = ImageFormats.Etc1(true),
+            [0x0C] = ImageFormats.Etc1A4(true),
+
+            [0x0E] = ImageFormats.A4(BitOrder.LeastSignificantBitFirst),
+            [0x0F] = ImageFormats.L4(BitOrder.LeastSignificantBitFirst),
+            [0x10] = ImageFormats.La44(),
+            [0x11] = ImageFormats.Rgb888()
+        };
+
+        public static readonly IDictionary<int, IColorEncoding> Ps3Formats = new Dictionary<int, IColorEncoding>
+        {
+            [0x01] = ImageFormats.Rgba4444(),
+            [0x02] = ImageFormats.Rgba5551(),
+            [0x03] = ImageFormats.Rgba8888(),
+            [0x04] = ImageFormats.Rgb565(),
+            [0x05] = ImageFormats.A8(),
+            [0x06] = ImageFormats.L8(),
+            [0x07] = ImageFormats.La88(),
+
+            [0x0A] = ImageFormats.Rg88(),
+            [0x0B] = ImageFormats.Etc1(true),
+            [0x0C] = ImageFormats.Etc1A4(true),
+
+            [0x0E] = ImageFormats.A4(BitOrder.LeastSignificantBitFirst),
+            [0x0F] = ImageFormats.L4(BitOrder.LeastSignificantBitFirst),
+            [0x10] = ImageFormats.La44(),
+            [0x11] = ImageFormats.Rgb888(),
+
+            [0x13] = ImageFormats.Dxt1(),
+            [0x14] = ImageFormats.Dxt3(),
+            [0x17] = ImageFormats.Dxt5(),
+
+            [0x19] = ImageFormats.Dxt1(),
+            [0x21] = ImageFormats.Dxt5(),
+            [0x27] = ImageFormats.Dxt5(),
+            [0x2A] = ImageFormats.Dxt5()
+        };
+
+        public static readonly IDictionary<int, IColorEncoding> SwitchFormats = new Dictionary<int, IColorEncoding>
+        {
+            [0x07] = ImageFormats.Rgba8888(ByteOrder.BigEndian),
+            [0x13] = ImageFormats.Dxt1(),
+            [0x17] = ImageFormats.Dxt5(),
+            [0x19] = ImageFormats.Ati1A(),
+            [0x1F] = ImageFormats.Ati2()
+        };
+
+        public static readonly IDictionary<int, IColorEncoding> MobileFormats = new Dictionary<int, IColorEncoding>
+        {
+            [0x1] = ImageFormats.Rgba8888(ByteOrder.BigEndian),
+            [0x7] = ImageFormats.Rgba4444(ByteOrder.BigEndian),
+            [0xa] = ImageFormats.Etc1(false, ByteOrder.BigEndian),
+
+            // Used as placeholders for format 0x0C, which defines 3 images of different encodings for different mobile platforms
+            [0xFD] = ImageFormats.Dxt5(),
+            [0xFE] = ImageFormats.PvrtcA_4bpp(),
+            [0xFF] = ImageFormats.AtcInterpolated()
+        };
+
+        private static readonly IDictionary<int, IColorShader> Shaders = new Dictionary<int, IColorShader>
+        {
+            [0x21] = new MtTex_NoAlphaShader(),
+            [0x2A] = new MtTex_YCbCrColorShader()
+        };
+
+        public static MtTexPlatform DeterminePlatform(Stream file)
+        {
+            using var br = new BinaryReaderX(file, true);
+
+            var magic = br.ReadString(4);
+            if (magic == "\0XET")
+                br.ByteOrder = ByteOrder.BigEndian;
+
+            // Read version
+            file.Position = 4;
+            var block = br.ReadUInt32();
+            file.Position = 0;
+
+            var version = block & 0xFFF;
+            var mobileVersion = block & 0xFFFF;
+
+            // Determine platform
+            if (magic == "TEX " && mobileVersion == 0x09)
+                return MtTexPlatform.Mobile;
+
+            switch (version)
+            {
+                case 0xa4:
+                case 0xa5:
+                case 0xa6:
+                    return MtTexPlatform.N3DS;
+
+                case 0x9a:
+                    return MtTexPlatform.PS3;
+
+                case 0xa0:
+                    return MtTexPlatform.Switch;
+
+                default:
+                    throw new InvalidOperationException($"MtTex version 0x{version:X2} is not supported.");
+            }
+        }
+
+        public static EncodingDefinition GetEncodingDefinition(MtTexPlatform platform)
+        {
+            var definition = new EncodingDefinition();
+
+            switch (platform)
+            {
+                case MtTexPlatform.N3DS:
+                    definition.AddColorEncodings(CtrFormats);
+                    break;
+
+                case MtTexPlatform.Switch:
+                    definition.AddColorEncodings(SwitchFormats);
+                    break;
+
+                case MtTexPlatform.PS3:
+                    definition.AddColorEncodings(Ps3Formats);
+                    definition.AddColorShaders(Shaders);
+                    break;
+
+                case MtTexPlatform.Mobile:
+                    definition.AddColorEncodings(MobileFormats);
+                    break;
+            }
+
+            return definition;
+        }
+    }
+
+    enum MtTexPlatform
+    {
+        N3DS,
+        Switch,
+        PS3,
+        Mobile
+    }
+
+    class MtTex_YCbCrColorShader : IColorShader
+    {
+        // https://en.wikipedia.org/wiki/YCbCr#JPEG_conversion
+        private const int CbCrThreshold_ = 123; // usually 128, but 123 seems to work better here
+
+        public Color Read(Color c)
+        {
+            var (a, y, cb, cr) = (c.G, c.A, c.B - CbCrThreshold_, c.R - CbCrThreshold_);
+            return Color.FromArgb(a,
+                Clamp(y + 1.402 * cr),
+                Clamp(y - 0.344136 * cb - 0.714136 * cr),
+                Clamp(y + 1.772 * cb));
+        }
+
+        public Color Write(Color c)
+        {
+            var (a, y, cb, cr) = (c.A,
+                0.299 * c.R + 0.587 * c.G + 0.114 * c.B,
+                CbCrThreshold_ - 0.168736 * c.R - 0.331264 * c.G + 0.5 * c.B,
+                CbCrThreshold_ + 0.5 * c.R - 0.418688 * c.G - 0.081312 * c.B);
+            return Color.FromArgb(Clamp(y), Clamp(cr), a, Clamp(cb));
+        }
+
+        private int Clamp(double n) => (int)Math.Max(0, Math.Min(n, 255));
+    }
+
+    class MtTex_NoAlphaShader : IColorShader
+    {
+        public Color Read(Color c)
+        {
+            return Color.FromArgb(255, c.R, c.G, c.B);
+        }
+
+        public Color Write(Color c)
+        {
+            return c;
+        }
+    }
+}
