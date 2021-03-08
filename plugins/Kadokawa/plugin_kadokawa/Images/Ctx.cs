5b4f795ba081915952e337877081133fcb4957dd
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-03-08 18:34:41 +0100

    Add CTX Load/Save;

---
+++ plugins/Kadokawa/plugin_kadokawa/Images/Ctx.cs
@@ -0,0 +1,53 @@
+﻿using System.Drawing;
+using System.IO;
+using Kanvas.Swizzle;
+using Komponent.IO;
+using Kontract.Models.Image;
+
+namespace plugin_kadokawa.Images
+{
+    class Ctx
+    {
+        private CtxHeader _header;
+
+        public ImageInfo Load(Stream input)
+        {
+            using var br = new BinaryReaderX(input);
+
+            // Read header
+            _header = br.ReadType<CtxHeader>();
+
+            // Read image data
+            var imageData = br.ReadBytes(_header.dataSize);
+            var imageInfo = new ImageInfo(imageData, _header.format, new Size(_header.width, _header.height));
+
+            imageInfo.RemapPixels.With(context => new CtrSwizzle(context));
+
+            return imageInfo;
+        }
+
+        public void Save(Stream output, ImageInfo imageInfo)
+        {
+            using var bw = new BinaryWriterX(output);
+
+            // Calculate offsets
+            var dataOffset = 0x40;
+
+            // Write image data
+            output.Position = dataOffset;
+            bw.Write(imageInfo.ImageData);
+
+            // Update header
+            _header.dataSize = imageInfo.ImageData.Length;
+            _header.format = imageInfo.ImageFormat;
+            _header.width = imageInfo.ImageSize.Width;
+            _header.height = imageInfo.ImageSize.Height;
+            _header.width2 = imageInfo.ImageSize.Width;
+            _header.height2 = imageInfo.ImageSize.Height;
+
+            // Write header
+            output.Position = 0;
+            bw.WriteType(_header);
+        }
+    }
+}
---
+++ plugins/Kadokawa/plugin_kadokawa/Images/CtxSupport.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Kanvas;
+using Komponent.IO.Attributes;
+using Kontract.Kanvas;
+using Kontract.Models.Image;
+using Kontract.Models.IO;
+
+namespace plugin_kadokawa.Images
+{
+    [Alignment(0x20)]
+    class CtxHeader
+    {
+        [FixedLength(8)]
+        public string magic = "CTX 10 \0";
+        public int width;
+        public int height;
+        public int width2;
+        public int height2;
+        public int unk1;
+        public int format;
+        public int unk2;
+        public int dataSize;
+    }
+
+    class CtxSupport
+    {
+        private static readonly IDictionary<uint, IColorEncoding> Formats = new Dictionary<uint, IColorEncoding>
+        {
+            // Composed of dataType and PixelFormat
+            // Short + short
+            [0x14016752] = ImageFormats.Rgba8888(),
+            [0x00006752] = ImageFormats.Rgba8888(),
+            [0x80336752] = ImageFormats.Rgba4444(),
+            [0x80346752] = ImageFormats.Rgba5551(),
+            [0x14016754] = ImageFormats.Rgb888(),
+            [0x83636754] = ImageFormats.Rgb565(),
+            [0x14016756] = ImageFormats.A8(),
+            [0x67616756] = ImageFormats.A4(BitOrder.LeastSignificantBitFirst),
+            [0x14016757] = ImageFormats.L8(),
+            [0x67616757] = ImageFormats.L4(BitOrder.LeastSignificantBitFirst),
+            [0x14016757] = ImageFormats.La88(),
+            [0x67606758] = ImageFormats.La44(),
+            [0x0000675A] = ImageFormats.Etc1(true),
+            [0x0000675B] = ImageFormats.Etc1A4(true),
+            [0x1401675A] = ImageFormats.Etc1(true),
+            [0x1401675B] = ImageFormats.Etc1A4(true)
+        };
+
+        public static EncodingDefinition GetEncodingDefinition()
+        {
+            var definition = new EncodingDefinition();
+            definition.AddColorEncodings(Formats.ToDictionary(x => (int)x.Key, y => y.Value));
+
+            return definition;
+        }
+    }
+}
