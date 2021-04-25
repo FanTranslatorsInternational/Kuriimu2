19af2ccf07c377afab60c062119582a857782e71
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-04-25 21:02:25 +0200

    Add CHNK Load;

---
+++ plugins/Kadokawa/plugin_cattle_call/Images/ChnkPlugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Komponent.IO;
+using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+using Kontract.Models.Context;
+using Kontract.Models.IO;
+
+namespace plugin_cattle_call.Images
+{
+    public class ChnkPlugin : IFilePlugin, IIdentifyFiles
+    {
+        public Guid PluginId => Guid.Parse("f41f9666-1a3f-4011-9231-4b27302d54f5");
+        public PluginType PluginType => PluginType.Image;
+        public string[] FileExtensions => new[] { "*.tex" };
+        public PluginMetadata Metadata { get; }
+
+        public ChnkPlugin()
+        {
+            Metadata = new PluginMetadata("CHNK", "onepiecefreak", "Image resource in Metal Max 3.");
+        }
+
+        public async Task<bool> IdentifyAsync(IFileSystem fileSystem, UPath filePath, IdentifyContext identifyContext)
+        {
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+
+            using var br = new BinaryReaderX(fileStream);
+            return br.ReadString(4) == "CHNK";
+        }
+
+        public IPluginState CreatePluginState(IFileManager fileManager)
+        {
+            return new ChnkState();
+        }
+    }
+}
