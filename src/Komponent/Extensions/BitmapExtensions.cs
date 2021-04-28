1ecc9a60b1709a5c656ed3afbd0c7ab2f334d269
LIT <littoma@hotmail.com>
2021-04-28 10:53:38 +0800

    Work in place.

--- src/Komponent/Extensions/BitmapExtensions.cs
+++ src/Komponent/Extensions/BitmapExtensions.cs
@@ -15,36 +15,36 @@ namespace Komponent.Extensions
 
             int bytesOfBmp = Math.Abs(bmpData.Stride) * bitmap.Height;
             int bytesOfChn = Math.Abs(chnData.Stride) * channel.Height;
-            byte[] bmpPixels = new byte[bytesOfBmp];
-            byte[] chnPixels = new byte[bytesOfChn];
-
-            System.Runtime.InteropServices.Marshal.Copy(ptrBmp, bmpPixels, 0, bytesOfBmp);
-            System.Runtime.InteropServices.Marshal.Copy(ptrChn, chnPixels, 0, bytesOfChn);
-
-            for (int y = 0; y < channel.Height && y < bitmap.Height; y++)
+            
+            unsafe
             {
-                for (int x = 0; x < channel.Width && x < bitmap.Width; x++)
+                Span<byte> bmpPixels = new Span<byte>(ptrBmp.ToPointer(), bytesOfBmp);
+                Span<byte> chnPixels = new Span<byte>(ptrChn.ToPointer(), bytesOfChn);
+
+                for (int y = 0; y < channel.Height && y < bitmap.Height; y++)
                 {
-                    int bmpPixelIndex = ((y * bitmap.Width) + x) * 4;
-                    byte bA = bmpPixels[bmpPixelIndex + 0];
-                    byte bR = bmpPixels[bmpPixelIndex + 1];
-                    byte bG = bmpPixels[bmpPixelIndex + 2];
-                    byte bB = bmpPixels[bmpPixelIndex + 3];
+                    for (int x = 0; x < channel.Width && x < bitmap.Width; x++)
+                    {
+                        int bmpPixelIndex = ((y * bitmap.Width) + x) * 4;
+                        byte bA = bmpPixels[bmpPixelIndex + 0];
+                        byte bR = bmpPixels[bmpPixelIndex + 1];
+                        byte bG = bmpPixels[bmpPixelIndex + 2];
+                        byte bB = bmpPixels[bmpPixelIndex + 3];
 
-                    int chnPixelIndex = ((y * channel.Width) + x) * 4;
-                    byte cA = chnPixels[chnPixelIndex + 0];
-                    byte cR = chnPixels[chnPixelIndex + 1];
-                    byte cG = chnPixels[chnPixelIndex + 2];
-                    byte cB = chnPixels[chnPixelIndex + 3];
+                        int chnPixelIndex = ((y * channel.Width) + x) * 4;
+                        byte cA = chnPixels[chnPixelIndex + 0];
+                        byte cR = chnPixels[chnPixelIndex + 1];
+                        byte cG = chnPixels[chnPixelIndex + 2];
+                        byte cB = chnPixels[chnPixelIndex + 3];
 
-                    bmpPixels[bmpPixelIndex + 0] = (byte)(bA | cA);
-                    bmpPixels[bmpPixelIndex + 1] = (byte)(bR | cR);
-                    bmpPixels[bmpPixelIndex + 2] = (byte)(bG | cG);
-                    bmpPixels[bmpPixelIndex + 3] = (byte)(bB | cB);
+                        bmpPixels[bmpPixelIndex + 0] = (byte)(bA | cA);
+                        bmpPixels[bmpPixelIndex + 1] = (byte)(bR | cR);
+                        bmpPixels[bmpPixelIndex + 2] = (byte)(bG | cG);
+                        bmpPixels[bmpPixelIndex + 3] = (byte)(bB | cB);
+                    }
                 }
             }
-
-            System.Runtime.InteropServices.Marshal.Copy(bmpPixels, 0, ptrBmp, bytesOfBmp);
+            
             bitmap.UnlockBits(bmpData);
             channel.UnlockBits(chnData);
         }
