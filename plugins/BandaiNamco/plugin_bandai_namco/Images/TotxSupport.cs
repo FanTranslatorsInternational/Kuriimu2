bd1b069594d41a6df4c914e94cfe1b63a7e1f31f
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-04-30 18:45:09 +0200

    Add TOTX Load/Save;

---
+++ plugins/BandaiNamco/plugin_bandai_namco/Images/TotxSupport.cs
@@ -0,0 +1,18 @@
+﻿using Komponent.IO.Attributes;
+
+namespace plugin_bandai_namco.Images
+{
+    class TotxHeader
+    {
+        [FixedLength(4)] 
+        public string magic = "TOTX";
+        public int zero0;
+        public int zero1;
+        public short width;
+        public short height;
+    }
+
+    class TotxSupport
+    {
+    }
+}
