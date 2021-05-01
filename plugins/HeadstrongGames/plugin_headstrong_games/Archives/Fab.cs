7be49d67220597efca4a6e638062cca2e484396b
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-05-01 23:57:08 +0200

    Add FBRC BNDL Load/Save;

---
+++ plugins/HeadstrongGames/plugin_headstrong_games/Archives/Fab.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Komponent.IO;
+using Kontract.Extensions;
+using Kontract.Models.Archive;
+using Kontract.Models.IO;
+
+namespace plugin_headstrong_games.Archives
+{
+    class Fab
+    {
+        private FabNode _root;
+
+        public IList<IArchiveFileInfo> Load(Stream input)
+        {
+            using var br = new BinaryReaderX(input, true, ByteOrder.BigEndian);
+
+            // Read node tree
+            _root = FabNode.Read(br);
+
+            // Read files
+            var result = new List<IArchiveFileInfo>();
+            foreach (var fileBranch in _root.Nodes.Where(x => x.SubType == "FILE"))
+            {
+                var fileName = fileBranch.Nodes.FirstOrDefault(x => x.Type == "NAME")?.AsString();
+                if (result.Any(x => x.FilePath.ToRelative().FullName == fileName))
+                    continue;
+
+                var fileDataNode = fileBranch.Nodes.FirstOrDefault(x => x.SubType == "DATA");
+                var userNode = fileDataNode?.Nodes.FirstOrDefault(x => x.Type == "USER");
+
+                var relevantNode = userNode ?? fileDataNode;
+                var fileStream = relevantNode?.Data;
+
+                // TODO: Detect and wrap compression?
+                result.Add(new FabArchiveFileInfo(fileStream, fileName) { DataNode = relevantNode });
+            }
+
+            return result;
+        }
+
+        public void Save(Stream output, IList<IArchiveFileInfo> files)
+        {
+            // Replace changed file data in nodes
+            foreach (var file in files.Where(x => x.ContentChanged).Cast<FabArchiveFileInfo>())
+            {
+                // This also re-compresses the changed file, if a compression is attached
+                var ms = new MemoryStream();
+                file.SaveFileData(ms);
+
+                ms.Position = 0;
+                file.DataNode.Data = ms;
+            }
+
+            // Write node tree
+            using var bw = new BinaryWriterX(output, ByteOrder.BigEndian);
+            _root.Write(bw);
+        }
+    }
+}
---
+++ plugins/HeadstrongGames/plugin_headstrong_games/Archives/FabPlugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Komponent.IO;
+using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+using Kontract.Models.Context;
+using Kontract.Models.IO;
+
+namespace plugin_headstrong_games.Archives
+{
+    public class FabPlugin : IFilePlugin, IIdentifyFiles
+    {
+        public Guid PluginId => Guid.Parse("c112dde7-b983-4c63-9c06-9e4fbfee04d5");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] {"*.fab"};
+        public PluginMetadata Metadata { get; }
+
+        public FabPlugin()
+        {
+            Metadata = new PluginMetadata("FBRC", "onepiecefreak", "The main image resource in Pokemon Art Academy.");
+        }
+
+        public async Task<bool> IdentifyAsync(IFileSystem fileSystem, UPath filePath, IdentifyContext identifyContext)
+        {
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+
+            using var br = new BinaryReaderX(fileStream);
+            var magic = br.ReadString(4);
+
+            fileStream.Position += 0x4;
+            var magic2 = br.ReadString(4);
+
+            return magic == "FBRC" && magic2 == "BNDL";
+        }
+
+        public IPluginState CreatePluginState(IFileManager fileManager)
+        {
+            return new FabState();
+        }
+    }
+}
