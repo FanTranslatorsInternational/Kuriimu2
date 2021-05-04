5520d60ad9120cbfae1585976c05782bfd2fbd18
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-05-04 23:14:01 +0200

    Add DAT Load/Save for Azure Striker Gunvolt;

---
+++ plugins/IntiCreates/plugin_inti_creates/Archives/DatState.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Interfaces.Plugins.State.Archive;
+using Kontract.Models.Archive;
+using Kontract.Models.Context;
+using Kontract.Models.IO;
+
+namespace plugin_inti_creates.Archives
+{
+    class DatState : IArchiveState, ILoadFiles, ISaveFiles, IReplaceFiles
+    {
+        private Dat _arc;
+
+        public IList<IArchiveFileInfo> Files { get; private set; }
+        public bool ContentChanged => IsContentChanged();
+
+        public DatState()
+        {
+            _arc = new Dat();
+        }
+
+        public async Task Load(IFileSystem fileSystem, UPath filePath, LoadContext loadContext)
+        {
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+            Files = new List<IArchiveFileInfo> { _arc.Load(fileStream) };
+        }
+
+        public Task Save(IFileSystem fileSystem, UPath savePath, SaveContext saveContext)
+        {
+            var fileStream = fileSystem.OpenFile(savePath, FileMode.Create, FileAccess.Write);
+            _arc.Save(fileStream, Files[0]);
+
+            return Task.CompletedTask;
+        }
+
+        public void ReplaceFile(IArchiveFileInfo afi, Stream fileData)
+        {
+            afi.SetFileData(fileData);
+        }
+
+        private bool IsContentChanged()
+        {
+            return Files.Any(x => x.ContentChanged);
+        }
+    }
+}
