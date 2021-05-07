a3d6022f95268b591782d5831a068362e3a3c198
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-05-07 23:17:46 +0200

    Add Gundam 3D Battle APK Load/Save;

---
+++ plugins/BandaiNamco/plugin_bandai_namco/Archives/Apk.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Komponent.IO;
+using Kontract.Models.Archive;
+using Kontract.Models.IO;
+
+namespace plugin_bandai_namco.Archives
+{
+    class Apk
+    {
+        private byte[] _headerIdent;
+        private string _name;
+
+        public IList<IArchiveFileInfo> Load(Stream input)
+        {
+            using var br = new BinaryReaderX(input, true);
+
+            // Read sections
+            var sections = ApkSection.ReadAll(input);
+
+            // Read pack header
+            var packHeader = sections.FirstOrDefault(x => x.Type == ApkSection.PackHeader).As<ApkPackHeader>();
+
+            _headerIdent = packHeader.headerIdent;
+
+            // Read entries
+            var entries = sections.FirstOrDefault(x => x.Type == ApkSection.PackToc).As<ApkToc>().entries;
+
+            // Read strings
+            using var stringBr = new BinaryReaderX(sections.FirstOrDefault(x => x.Type == ApkSection.StringTable).Data, true);
+
+            var stringHeader = stringBr.ReadType<ApkStringHeader>();
+
+            stringBr.BaseStream.Position = stringHeader.tableOffset;
+            var stringOffsets = stringBr.ReadMultiple<int>(stringHeader.stringCount);
+
+            var strings = new List<string>();
+            foreach (var stringOffset in stringOffsets)
+            {
+                stringBr.BaseStream.Position = stringHeader.dataOffset + stringOffset;
+                strings.Add(stringBr.ReadCStringASCII());
+            }
+
+            _name = strings[packHeader.stringIndex];
+
+            return ApkSupport.EnumerateFiles(new List<Stream> { input }, entries[0], UPath.Root, new List<ApkPackHeader> { packHeader }, strings, entries).ToArray();
+        }
+
+        public void Save(Stream output, IList<IArchiveFileInfo> files)
+        {
+            ApkSupport.Save(output, files, _name, _headerIdent);
+        }
+    }
+}
---
+++ plugins/BandaiNamco/plugin_bandai_namco/Archives/ApkPlugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+using Kontract.Models.Context;
+using Kontract.Models.IO;
+
+namespace plugin_bandai_namco.Archives
+{
+    public class ApkPlugin : IFilePlugin, IIdentifyFiles
+    {
+        public Guid PluginId => Guid.Parse("082d58ca-f3c6-4bb7-ae9a-b46b97a6bb44");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] { "*.apk" };
+        public PluginMetadata Metadata { get; }
+
+        public ApkPlugin()
+        {
+            Metadata = new PluginMetadata("APK", "onepiecefreak", "Main package resource in Gundam 3D Battle.");
+        }
+
+        public async Task<bool> IdentifyAsync(IFileSystem fileSystem, UPath filePath, IdentifyContext identifyContext)
+        {
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+
+            var sections = ApkSection.ReadAll(fileStream);
+            return sections.Count(x => x.Type == ApkSection.PackHeader) == 1;
+        }
+
+        public IPluginState CreatePluginState(IFileManager fileManager)
+        {
+            return new ApkState();
+        }
+    }
+}
