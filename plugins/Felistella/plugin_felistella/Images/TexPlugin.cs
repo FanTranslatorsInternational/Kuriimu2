234edd266bab367a087989cf75d2a336916439fb
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-05-08 14:02:24 +0200

    Add tex_vita Load/Save;

---
+++ plugins/Felistella/plugin_felistella/Images/TexPlugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+
+namespace plugin_felistella.Images
+{
+    public class TexPlugin : IFilePlugin
+    {
+        public Guid PluginId => Guid.Parse("4a29ddcc-bf9b-4fba-a5cd-6291fed13f23");
+        public PluginType PluginType => PluginType.Image;
+        public string[] FileExtensions => new[] {"*.tex_vita"};
+        public PluginMetadata Metadata { get; }
+
+        public TexPlugin()
+        {
+            Metadata=new PluginMetadata("TEX_VITA","onepiecefreak","The main image resource in Genka Tikko Seven Pirates.");
+        }
+
+        public IPluginState CreatePluginState(IFileManager fileManager)
+        {
+            return new TexState();
+        }
+    }
+}
