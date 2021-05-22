7b3ff4308337bc187e4536ac80c5c429730d41e8
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-05-22 17:44:25 +0200

    Add Pck Load/Save for Sting Entertainment; Add Sting Entertainment to build action;

---
+++ plugins/StingEntertainment/plugin_sting_entertainment/Archives/PckPlugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Komponent.IO;
+using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+using Kontract.Models.Context;
+using Kontract.Models.IO;
+
+namespace plugin_sting_entertainment.Archives
+{
+    public class PckPlugin : IFilePlugin, IIdentifyFiles
+    {
+        public Guid PluginId => Guid.Parse("dcdd3e5f-ba74-4541-a870-5d705ed6471a");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] { "*.pck" };
+        public PluginMetadata Metadata { get; }
+
+        public PckPlugin()
+        {
+            Metadata = new PluginMetadata("PCK", "onepiecefreak", "The main package resource in Dungeon Travelers 2.");
+        }
+
+        public async Task<bool> IdentifyAsync(IFileSystem fileSystem, UPath filePath, IdentifyContext identifyContext)
+        {
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+
+            using var br = new BinaryReaderX(fileStream);
+            return br.ReadString(8) == "Filename";
+        }
+
+        public IPluginState CreatePluginState(IFileManager fileManager)
+        {
+            return new PckState();
+        }
+    }
+}
