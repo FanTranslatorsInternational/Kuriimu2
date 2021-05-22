8830224cab266523b2eaeb984d7177bba833b81f
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-05-22 14:10:28 +0200

    Add Bimg Load/Save;

---
+++ plugins/Nintendo/plugin_nintendo/Images/Bimg.cs
@@ -0,0 +1,54 @@
+﻿using System.Drawing;
+using System.IO;
+using Kanvas.Swizzle;
+using Komponent.IO;
+using Kontract.Models.Image;
+
+namespace plugin_nintendo.Images
+{
+    class Bimg
+    {
+        private static readonly int HeaderSize = Tools.MeasureType(typeof(BimgHeader));
+
+        private BimgHeader _header;
+
+        public ImageInfo Load(Stream input)
+        {
+            using var br = new BinaryReaderX(input);
+
+            // Read header
+            _header = br.ReadType<BimgHeader>();
+
+            // Read image data
+            var imgData = br.ReadBytes(_header.dataSize);
+
+            // Create image info
+            var imageInfo = new ImageInfo(imgData, _header.format, new Size(_header.width, _header.height));
+            imageInfo.RemapPixels.With(context => new CtrSwizzle(context));
+
+            return imageInfo;
+        }
+
+        public void Save(Stream output, ImageInfo imageInfo)
+        {
+            using var bw = new BinaryWriterX(output);
+
+            // Calculate offsets
+            var dataOffset = HeaderSize;
+
+            // Write image data
+            output.Position = dataOffset;
+            bw.Write(imageInfo.ImageData);
+
+            // Update header
+            _header.format = imageInfo.ImageFormat;
+            _header.dataSize = imageInfo.ImageData.Length;
+            _header.width = (short)imageInfo.ImageSize.Width;
+            _header.height = (short)imageInfo.ImageSize.Height;
+
+            // Write header
+            output.Position = 0;
+            bw.WriteType(_header);
+        }
+    }
+}
---
+++ plugins/Nintendo/plugin_nintendo/Images/BimgPlugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+
+namespace plugin_nintendo.Images
+{
+    public class BimgPlugin : IFilePlugin
+    {
+        public Guid PluginId => Guid.Parse("7e60edb3-9a23-4efa-a10b-f113da20d1bc");
+        public PluginType PluginType => PluginType.Image;
+        public string[] FileExtensions => new[] { "*.bimg" };
+        public PluginMetadata Metadata { get; }
+
+        public BimgPlugin()
+        {
+            Metadata = new PluginMetadata("BIMG", "onepiecefreak", "The thumbnail format for 3DS movies from the eshop.");
+        }
+
+        public IPluginState CreatePluginState(IFileManager fileManager)
+        {
+            return new BimgState();
+        }
+    }
+}
