0d6f06637261d739737dee1633ef9c9e1347cd29
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-05-30 00:43:52 +0200

    Add support for Sangokushi bin archives;

---
+++ plugins/KoeiTecmo/plugin_koei_tecmo/Archives/SangoBinPlugin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+
+namespace plugin_koei_tecmo.Archives
+{
+    public class SangoBinPlugin : IFilePlugin
+    {
+        public Guid PluginId => Guid.Parse("aa6a43ce-ff5f-4bc5-b8b4-fe1b84a5d40e");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] { "*.bin" };
+        public PluginMetadata Metadata { get; }
+
+        public SangoBinPlugin()
+        {
+            Metadata = new PluginMetadata("BIN", "onepiecefreak", "One file archive found in Yo-Kai Watch Sangokushi.");
+        }
+
+        public IPluginState CreatePluginState(IFileManager fileManager)
+        {
+            return new SangoBinState();
+        }
+    }
+}
