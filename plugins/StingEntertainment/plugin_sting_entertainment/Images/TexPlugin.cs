94b8410dd75a8184286f1ad32e8ddf6d6120f179
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-06-18 21:49:20 +0200

    Add Tex Load/Save for Sting Entertainment games;

---
+++ plugins/StingEntertainment/plugin_sting_entertainment/Images/TexPlugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Komponent.IO;
+using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+using Kontract.Models.Context;
+using Kontract.Models.IO;
+
+namespace plugin_sting_entertainment.Images
+{
+    public class TexPlugin : IFilePlugin, IIdentifyFiles
+    {
+        public Guid PluginId => Guid.Parse("dcd694e0-31b5-481c-8cb8-4bec0fc05233");
+        public PluginType PluginType => PluginType.Image;
+        public string[] FileExtensions => new[] { "*.tex" };
+        public PluginMetadata Metadata { get; }
+
+        public TexPlugin()
+        {
+            Metadata = new PluginMetadata("TEX", "onepiecefreak", "The main texture in Dungeon Travelers 2.");
+        }
+
+        public async Task<bool> IdentifyAsync(IFileSystem fileSystem, UPath filePath, IdentifyContext identifyContext)
+        {
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+
+            using var br = new BinaryReaderX(fileStream);
+            return br.ReadString(8) == "Texture ";
+        }
+
+        public IPluginState CreatePluginState(IFileManager fileManager)
+        {
+            return new TexState();
+        }
+    }
+}
---
+++ plugins/StingEntertainment/plugin_sting_entertainment/Images/TexSupport.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Kanvas;
+using Kanvas.Encoding;
+using Kontract.Kanvas;
+using Kontract.Models.Image;
+using Kontract.Models.IO;
+
+namespace plugin_sting_entertainment.Images
+{
+    class TexHeader
+    {
+        public int unk1;
+        public int dataSize;
+        public int width;
+        public int height;
+    }
+
+    class TexSupport
+    {
+        private static readonly IDictionary<int, IIndexEncoding> IndexFormats = new Dictionary<int, IIndexEncoding>
+        {
+            [0] = ImageFormats.I8()
+        };
+
+        private static readonly IDictionary<int, IColorEncoding> PaletteFormats = new Dictionary<int, IColorEncoding>
+        {
+            [0] = ImageFormats.Rgba8888(ByteOrder.BigEndian)
+        };
+
+        public static EncodingDefinition GetEncodingDefinition()
+        {
+            var definition = new EncodingDefinition();
+
+            definition.AddPaletteEncodings(PaletteFormats);
+            definition.AddIndexEncodings(IndexFormats.Select(x => (x.Key, new IndexEncodingDefinition(x.Value, new[] { 0 }))).ToArray());
+
+            return definition;
+        }
+    }
+}
