964ea04b3640d03f816c8e4226a180a676d3337d
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-06-26 22:58:06 +0200

    Add Gz compression container;

---
+++ plugins/KoeiTecmo/plugin_gust/Archives/Gz.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Komponent.IO;
+using Komponent.IO.Streams;
+using Kompression.Implementations;
+using Kontract.Models.Archive;
+
+namespace plugin_gust.Archives
+{
+    class Gz
+    {
+        private static readonly int HeaderSize = Tools.MeasureType(typeof(GzHeader));
+
+        private GzHeader _header;
+        private IList<int> _blockSizes;
+        private Stream _origStream;
+
+        public IArchiveFileInfo Load(Stream input, string fileName = null)
+        {
+            using var br = new BinaryReaderX(input, true);
+
+            // Read header
+            _header = br.ReadType<GzHeader>();
+
+            // Read sizes
+            _blockSizes = br.ReadMultiple<int>(_header.blockCount);
+            var blockOffsets = new int[_header.blockCount];
+            for (var i = 0; i < _header.blockCount; i++)
+            {
+                input.Position = (input.Position + 0x7F) & ~0x7F;
+                blockOffsets[i] = (int)(input.Position + 4);
+                input.Position += _blockSizes[i];
+            }
+
+            // Create file
+            _origStream = new SubStream(input, blockOffsets[0], input.Length - blockOffsets[0] - 4);
+            var fileStream = new GzStream(input, _header.decompBlockSize, _header.decompSize, blockOffsets.Zip(_blockSizes.Select(x => x - 4)).ToArray());
+            fileName ??= "00000000.bin";
+
+            return new ArchiveFileInfo(fileStream, fileName);
+        }
+
+        public void Save(Stream output, IArchiveFileInfo file)
+        {
+            using var bw = new BinaryWriterX(output);
+
+            // Calculate offsets
+            var sizeOffset = HeaderSize;
+            var blockCount = file.ContentChanged ? (int)Math.Ceiling(file.FileSize / (float)_header.decompBlockSize) : _blockSizes.Count;
+            var dataOffset = (sizeOffset + blockCount * 4 + 0x7F) & ~0x7F;
+
+            // Chunk stream into blocks
+            output.Position = dataOffset;
+            var blockSizes = _blockSizes;
+
+            if (file.ContentChanged)
+            {
+                blockSizes = GzStream.ChunkStream(file.GetFileData().Result, output, _header.decompBlockSize, 0x80);
+                _header.decompSize = (int)file.FileSize;
+            }
+            else
+            {
+                _origStream.Position = 0;
+                _origStream.CopyTo(output);
+            }
+
+            // Write header
+            _header.blockCount = blockSizes.Count;
+
+            output.Position = 0;
+            bw.WriteType(_header);
+
+            // Write block sizes
+            bw.WriteMultiple(blockSizes);
+        }
+    }
+}
---
+++ plugins/KoeiTecmo/plugin_koei_tecmo/Archives/GzSupport.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Buffers.Binary;
+using System.Collections.Generic;
+using System.IO;
+using Komponent.IO.Streams;
+using Kompression.Implementations;
+using Kontract.Kompression;
+
+namespace plugin_koei_tecmo.Archives
+{
+    class GzHeader
+    {
+        public int decompBlockSize;
+        public int blockCount;
+        public int decompSize;
+    }
+
+    class GzStream : Stream
+    {
+        private static readonly ICompression ZLib = Compressions.ZLib.Build();
+
+        private readonly Stream _baseStream;
+        private readonly int _decompBlockSize;
+        private readonly int _decompSize;
+        private readonly IList<(int, int)> _blocks;
+        private readonly Stream[] _decompBlocks;
+
+        private long _position;
+
+        public override bool CanRead => true;
+        public override bool CanSeek => true;
+        public override bool CanWrite => false;
+        public override long Length => _decompSize;
+        public override long Position { get => _position; set => Seek(value, SeekOrigin.Begin); }
+
+        public GzStream(Stream baseStream, int decompBlockSize, int decompSize, IList<(int, int)> blocks)
+        {
+            _baseStream = baseStream;
+            _decompBlockSize = decompBlockSize;
+            _decompSize = decompSize;
+            _blocks = blocks;
+            _decompBlocks = new Stream[blocks.Count];
+        }
+
+        public override void Flush()
+        {
+            _baseStream.Flush();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    _position = offset;
+                    break;
+
+                case SeekOrigin.Current:
+                    _position += offset;
+                    break;
+
+                case SeekOrigin.End:
+                    _position = Length + offset;
+                    break;
+            }
+
+            return _position;
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var length = count = (int)Math.Min(count, Length - Position);
+
+            var block = (int)(Position / _decompBlockSize);
+            var blockPos = Position % _decompBlockSize;
+
+            while (length > 0)
+            {
+                // Determine block size
+                var size = (int)Math.Max(0, Math.Min(_decompBlockSize - blockPos, length));
+
+                // Copy decompressed block content
+                EnsureDecompressedBlock(block);
+                _decompBlocks[block].Position = blockPos;
+                _decompBlocks[block].Read(buffer, offset, size);
+
+                // Update local information
+                blockPos = 0;
+                block++;
+
+                length -= size;
+                offset += size;
+                _position += size;
+            }
+
+            return count;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        private void EnsureDecompressedBlock(int blockIndex)
+        {
+            if (_decompBlocks[blockIndex] != null)
+                return;
+
+            _decompBlocks[blockIndex] = new MemoryStream();
+            ZLib.Decompress(new SubStream(_baseStream, _blocks[blockIndex].Item1, _blocks[blockIndex].Item2), _decompBlocks[blockIndex]);
+        }
+
+        public static int[] ChunkStream(Stream input, Stream output, int decompBlockSize, int alignment)
+        {
+            var blockSizes = new int[input.Length / decompBlockSize + (input.Length % decompBlockSize > 0 ? 1 : 0)];
+            var buffer = new byte[4];
+
+            var position = 0;
+            var blockIndex = 0;
+            while (position < input.Length)
+            {
+                var startPos = output.Position;
+                output.Position += 4;
+
+                // Compress block
+                var blockStream = new SubStream(input, position, Math.Min(decompBlockSize, input.Length - position));
+                ZLib.Compress(blockStream, output);
+
+                var endPos = output.Position;
+                output.Position = startPos;
+
+                BinaryPrimitives.WriteInt32LittleEndian(buffer, (int)(endPos - startPos - 4));
+                output.Write(buffer);
+                blockSizes[blockIndex] = (int)(endPos - startPos);
+
+                output.Position = endPos;
+                while (output.Position % alignment > 0)
+                    output.Position++;
+
+                position += decompBlockSize;
+                blockIndex++;
+            }
+
+            return blockSizes;
+        }
+    }
+}
