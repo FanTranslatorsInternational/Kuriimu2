25b8ad6c7a1b40361971981d9aa210e415e48f63
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-06-27 17:51:09 +0200

    Add NISPACK Load/Save;

---
+++ plugins/NipponIchiSoftware/plugin_nippon_ichi/Archives/DatPlugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Komponent.IO;
+using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+using Kontract.Models.Context;
+using Kontract.Models.IO;
+
+namespace plugin_nippon_ichi.Archives
+{
+    public class DatPlugin : IFilePlugin, IIdentifyFiles
+    {
+        public Guid PluginId => Guid.Parse("89c7658b-6371-4be9-96b4-db9b9eb77be9");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] { "*.DAT" };
+        public PluginMetadata Metadata { get; }
+
+        public DatPlugin()
+        {
+            Metadata = new PluginMetadata("DAT", "onepiecefreak", "Main resource in Hayarigami games.");
+        }
+
+        public async Task<bool> IdentifyAsync(IFileSystem fileSystem, UPath filePath, IdentifyContext identifyContext)
+        {
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+
+            using var br = new BinaryReaderX(fileStream);
+            return br.ReadString(7) == "NISPACK";
+        }
+
+        public IPluginState CreatePluginState(IFileManager fileManager)
+        {
+            return new DatState();
+        }
+    }
+}
---
+++ plugins/NipponIchiSoftware/plugin_nippon_ichi/Archives/DatSupport.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Komponent.IO.Attributes;
+using Kontract.Models.Archive;
+
+namespace plugin_nippon_ichi.Archives
+{
+    class DatHeader
+    {
+        [FixedLength(8)] 
+        public string magic = "NISPACK\0";
+        public int zero0;
+        public int fileCount;
+    }
+
+    class DatEntry
+    {
+        [FixedLength(0x20)]
+        public string name;
+        public int offset;
+        public int size;
+        public uint unk1;
+    }
+
+    class DatArchiveFileInfo : ArchiveFileInfo
+    {
+        public DatEntry Entry { get; }
+
+        public DatArchiveFileInfo(Stream fileData, string filePath, DatEntry entry) : base(fileData, filePath)
+        {
+            Entry = entry;
+        }
+    }
+}
