368c33ffcbe840cb61ad00b44df170c99885162d
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-06-27 12:47:45 +0200

    Add CRAE Load/Save;

---
+++ plugins/KoeiTecmo/plugin_koei_tecmo/Archives/CraePlugin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Komponent.IO;
+using Kontract.Interfaces.FileSystem;
+using Kontract.Interfaces.Managers;
+using Kontract.Interfaces.Plugins.Identifier;
+using Kontract.Interfaces.Plugins.State;
+using Kontract.Models;
+using Kontract.Models.Context;
+using Kontract.Models.IO;
+
+namespace plugin_koei_tecmo.Archives
+{
+    public class CraePlugin : IFilePlugin, IIdentifyFiles
+    {
+        public Guid PluginId => Guid.Parse("a7d2ed59-6a9a-49a9-880f-ba206d6cf029");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] { "*.gz" };
+        public PluginMetadata Metadata { get; }
+
+        public CraePlugin()
+        {
+            Metadata = new PluginMetadata("CRAE", "onepiecefreak", "Main archive resource in Blue Reflection.");
+        }
+
+        public async Task<bool> IdentifyAsync(IFileSystem fileSystem, UPath filePath, IdentifyContext identifyContext)
+        {
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+
+            using var br = new BinaryReaderX(fileStream);
+            return br.ReadString(4) == "CRAE";
+        }
+
+        public IPluginState CreatePluginState(IFileManager fileManager)
+        {
+            return new CraeState();
+        }
+    }
+}
---
+++ plugins/KoeiTecmo/plugin_koei_tecmo/Archives/CraeSupport.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Komponent.IO.Attributes;
+
+namespace plugin_koei_tecmo.Archives
+{
+    class CraeHeader
+    {
+        [FixedLength(4)]
+        public string magic;
+        public int unk1;
+        public int dataSize;
+        public int entryOffset;
+        public int dataOffset;
+        public int fileCount;
+        public int unk2;
+    }
+
+    class CraeEntry
+    {
+        public int offset;
+        public int size;
+        [FixedLength(0x30)]
+        public string name;
+    }
+
+    class CraeSupport
+    {
+    }
+}
