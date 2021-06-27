897c72ab955b200793a8ce1f55dd4b504b0d6feb
onepiecefreak3 <realonepiecefreak@gmx.de>
2021-06-27 15:23:05 +0200

    Add IMG0 Load/Save for Superflat Games;

---
+++ plugins/SuperflatGames/superflat_games/Images/Img.cs
@@ -0,0 +1,48 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Text;
+using Komponent.IO;
+using Kontract.Models.Image;
+
+namespace superflat_games.Images
+{
+    class Img
+    {
+        private static readonly int HeaderSize = Tools.MeasureType(typeof(ImgHeader));
+
+        public ImageInfo Load(Stream input)
+        {
+            using var br = new BinaryReaderX(input);
+
+            // Read image data
+            var imgHeader = br.ReadType<ImgHeader>();
+            var imgData = br.ReadBytes(imgHeader.size);
+
+            // Read tex info
+            var texHeader = br.ReadType<ImgHeader>();
+            var texInfo = br.ReadType<ImgEntry>();
+
+            return new ImageInfo(imgData, 0, new Size(texInfo.width, texInfo.height));
+        }
+
+        public void Save(Stream output, ImageInfo imageInfo)
+        {
+            using var bw = new BinaryWriterX(output);
+
+            // Calculate offsets
+            var texOffset = HeaderSize + imageInfo.ImageData.Length;
+
+            // Write image data
+            bw.WriteType(new ImgHeader { magic = "IMG0", size = imageInfo.ImageData.Length });
+            bw.Write(imageInfo.ImageData);
+
+            // Write tex header
+            bw.WriteType(new ImgHeader { magic = "TEXR", size = 0x10 });
+            bw.WriteType(new ImgEntry { width = imageInfo.ImageSize.Width, height = imageInfo.ImageSize.Height });
+
+            // Write end header
+            bw.WriteString("!END", Encoding.ASCII, false, false);
+            bw.WritePadding(3);
+        }
+    }
+}
---
+++ plugins/SuperflatGames/superflat_games/Images/ImgSupport.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Kanvas;
+using Komponent.IO.Attributes;
+using Kontract.Kanvas;
+using Kontract.Models.Image;
+using Kontract.Models.IO;
+
+namespace superflat_games.Images
+{
+    class ImgHeader
+    {
+        [FixedLength(4)]
+        public string magic;
+        public int size;
+        public int zero0;
+    }
+
+    class ImgEntry
+    {
+        public int width;
+        public int height;
+        public int zero0;
+        public int zero1;
+    }
+
+    class ImgSupport
+    {
+        private static readonly IDictionary<int, IColorEncoding> Formats = new Dictionary<int, IColorEncoding>
+        {
+            [0] = ImageFormats.Rgba8888(ByteOrder.BigEndian)
+        };
+
+        public static EncodingDefinition GetEncodingDefinition()
+        {
+            var definition = new EncodingDefinition();
+            definition.AddColorEncodings(Formats);
+
+            return definition;
+        }
+    }
+}
