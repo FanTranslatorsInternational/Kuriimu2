e7ddf1eeede4ab0f4faeb52a3a844d3e3efbba52
caleb.mabry.15@cnu.edu <caleb.mabry.15@cnu.edu>
2021-12-09 00:22:26 -0500

    Used some existing examples to try and clean things up

---
+++ plugins/SquareEnix/plugin_square_enix/Archives/Bin.cs
@@ -0,0 +1,33 @@
+﻿using Komponent.IO;
+using Komponent.IO.Streams;
+using Kontract.Models.Archive;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace plugin_square_enix.Archives
+{
+    class Bin
+    {
+        public IList<IArchiveFileInfo> Load(Stream input)
+        {
+            var _entries = new List<IArchiveFileInfo>();
+            var reader = new BinaryReaderX(input);
+
+            var header = reader.ReadType<Binheader>();
+            var binFiles = reader.ReadMultiple<BinTableEntry>(header.fileCount);
+            for (int i = 0; i < binFiles.Count; i++)
+            {
+                using (var sub = new SubStream(input, binFiles[i].offset, binFiles[i].fileSize))
+                    _entries.Add(new ArchiveFileInfo(sub, $"file{i}"));
+            }
+            return _entries;
+        }
+        public void Save(Stream output, IList<IArchiveFileInfo> files)
+        {
+
+        }
+    }
+}
--- plugins/SquareEnix/plugin_square_enix/Archives/BinState.cs
+++ plugins/SquareEnix/plugin_square_enix/Archives/BinState.cs
@@ -7,34 +7,37 @@ using Kontract.Models.Context;
 using Kontract.Models.IO;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace plugin_square_enix.Archives
 {
-    public class BinState : IArchiveState, ILoadFiles
+    public class BinState : IArchiveState, ILoadFiles, ISaveFiles
     {
-        // Exposes the loaded files from the archive format to any consuming user interface
+        private readonly Bin _bin;
+        
         public IList<IArchiveFileInfo> Files { get; private set; }
-
-        // Indicates if the contents of the format were changed
+        
         public bool ContentChanged { get; private set; }
+        
+        public BinState()
+        {
+            _bin = new Bin();
+        }
 
         public async Task Load(IFileSystem fileSystem, UPath filePath, LoadContext context)
         {
-            var _entries = new List<IArchiveFileInfo>();
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+            Files = _bin.Load(fileStream);
+        }
 
-            var input = await fileSystem.OpenFileAsync(filePath);
-            var reader = new BinaryReaderX(input);
+        public Task Save(IFileSystem fileSystem, UPath savePath, SaveContext saveContext)
+        {
+            var output = fileSystem.OpenFile(savePath, FileMode.Create);
+            _bin.Save(output, Files);
 
-            var header = reader.ReadType<Binheader>();
-            var binFiles = reader.ReadMultiple<BinTableEntry>(header.fileCount);
-            for (int i = 0; i < binFiles.Count; i++)
-            {
-                using (var sub = new SubStream(input, binFiles[i].offset, binFiles[i].fileSize))
-                _entries.Add(new ArchiveFileInfo(sub, $"file{i}"));
-            }
-            Files = _entries;
+            return Task.CompletedTask;
         }
 
     }
