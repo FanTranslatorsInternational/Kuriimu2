14911a39a6bf33b9c5a497ea67a0ebaf025362cb
Megaflan <megaflan@outlook.es>
2025-02-26 22:17:00 +0100

    BAM (Atlus) [.NET 8]

---
+++ plugins/Atlus/plugin_atlus/N3DS/Archive/BamPlugin.cs
@@ -0,0 +1,37 @@
+﻿using Komponent.IO;
+using Konnect.Contract.DataClasses.FileSystem;
+using Konnect.Contract.DataClasses.Plugin;
+using Konnect.Contract.DataClasses.Plugin.File;
+using Konnect.Contract.Enums.Plugin.File;
+using Konnect.Contract.FileSystem;
+using Konnect.Contract.Management.Files;
+using Konnect.Contract.Plugin.File;
+
+namespace plugin_atlus.N3DS.Archive
+{
+    public class BamPlugin : IFilePlugin, IIdentifyFiles
+    {
+        public Guid PluginId => Guid.Parse("f0fac450-6567-4bf6-a820-d9c32134062d");
+        public PluginType PluginType => PluginType.Archive;
+        public string[] FileExtensions => new[] { "*.bam" };
+        public PluginMetadata Metadata { get; } = new()
+        {
+            Name = "BAM",
+            Author = "onepiecefreak",
+            LongDescription = "The BAM resource container for Atlus games on 3DS."
+        };
+
+        public async Task<bool> IdentifyAsync(IFileSystem fileSystem, UPath filePath, IdentifyContext identifyContext)
+        {
+            var fileStream = await fileSystem.OpenFileAsync(filePath);
+            using var br = new BinaryReaderX(fileStream);
+
+            return br.ReadString(4) == "ATBC";
+        }
+
+        public IFilePluginState CreatePluginState(IPluginFileManager pluginManager)
+        {
+            return new BamState();
+        }
+    }
+}
