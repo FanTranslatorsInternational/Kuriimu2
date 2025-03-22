e9ebb384e229b860d8c47986037bf211d6fcaee3
onepiecefreak3 <realonepiecefreak@gmx.de>
2025-03-22 16:12:14 +0100

    Add file tree algorithms to Konnect;

---
+++ src/lib/Konnect/DataClasses/FileSystem/DirectoryEntry.cs
@@ -0,0 +1,63 @@
+﻿using Konnect.Contract.DataClasses.FileSystem;
+using Konnect.Contract.Plugin.File.Archive;
+
+namespace Konnect.DataClasses.FileSystem
+{
+    public class DirectoryEntry
+    {
+        private DirectoryEntry _parent;
+
+        public string Name { get; set; }
+
+        public UPath AbsolutePath => CreateAbsolutePath();
+
+        public IList<DirectoryEntry> Directories { get; }
+
+        public IList<IArchiveFile> Files { get; }
+
+        public DirectoryEntry(string name)
+        {
+            Name = name;
+            Directories = new List<DirectoryEntry>();
+            Files = new List<IArchiveFile>();
+        }
+
+        /// <summary>
+        /// Adds or merges a directory entry into this one.
+        /// </summary>
+        /// <param name="entry"></param>
+        public void AddDirectory(DirectoryEntry entry)
+        {
+            var existingDir = Directories.FirstOrDefault(x => x.Name == entry.Name);
+            if (existingDir == null)
+            {
+                entry._parent = this;
+                Directories.Add(entry);
+                return;
+            }
+
+            foreach (var dir in entry.Directories)
+                existingDir.AddDirectory(dir);
+            foreach (var file in entry.Files)
+                if (!existingDir.Files.Contains(file))
+                    existingDir.Files.Add(file);
+        }
+
+        /// <summary>
+        /// Remove this entry from its parent
+        /// </summary>
+        public void Remove()
+        {
+            _parent?.Directories.Remove(this);
+            _parent = null;
+        }
+
+        private UPath CreateAbsolutePath()
+        {
+            if (_parent == null)
+                return Name;
+
+            return _parent.AbsolutePath / Name;
+        }
+    }
+}
---
+++ src/lib/Konnect/Extensions/ListExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Konnect.Contract.Plugin.File.Archive;
+using Konnect.DataClasses.FileSystem;
+
+namespace Konnect.Extensions
+{
+    public static class ListExtensions
+    {
+        public static DirectoryEntry ToTree(this IList<IArchiveFile> files)
+        {
+            var root = new DirectoryEntry(string.Empty);
+
+            foreach (IArchiveFile file in files)
+            {
+                DirectoryEntry parent = root;
+
+                foreach (string part in file.FilePath.GetDirectory().Split())
+                {
+                    DirectoryEntry? entry = parent.Directories.FirstOrDefault(x => x.Name == part);
+                    if (entry == null)
+                    {
+                        entry = new DirectoryEntry(part);
+                        parent.AddDirectory(entry);
+                    }
+
+                    parent = entry;
+                }
+
+                parent.Files.Add(file);
+            }
+
+            return root;
+        }
+    }
+}
