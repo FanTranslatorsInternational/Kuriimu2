81ca8fae1b1b8127375ceaef80a97343f0ac2e9a
onepiecefreak3 <realonepiecefreak@gmx.de>
2025-03-31 00:51:47 +0200

    Add SizePadding methods for Size;

--- src/lib/Kanvas/SizePadding.cs
+++ src/lib/Kanvas/SizePadding.cs
@@ -1,7 +1,23 @@
-﻿namespace Kanvas
+﻿using SixLabors.ImageSharp;
+
+namespace Kanvas
 {
     public static class SizePadding
     {
+        /// <summary>
+        /// Rounds up a given value to the next power of two.
+        /// </summary>
+        /// <param name="size">The size to round up.</param>
+        /// <param name="steps">The amount of power of two's to round up to.</param>
+        /// <returns>The rounded up value.</returns>
+        /// <remarks>Eg. 34 with 1 step is 64, 121 with 2 steps is 256.</remarks>
+        public static Size PowerOfTwo(Size size, int steps = 1)
+        {
+            return new Size(
+                PowerOfTwo(size.Width, steps),
+                PowerOfTwo(size.Height, steps));
+        }
+
         /// <summary>
         /// Rounds up a given value to the next power of two.
         /// </summary>
@@ -9,7 +25,7 @@
         /// <param name="steps">The amount of power of two's to round up to.</param>
         /// <returns>The rounded up value.</returns>
         /// <remarks>Eg. 34 with 1 step is 64, 121 with 2 steps is 256.</remarks>
-        public static int PowerOfTwo(int value, int steps)
+        public static int PowerOfTwo(int value, int steps = 1)
         {
             return 2 << (int)Math.Log(value - 1, 2) << (steps - 1);
         }
