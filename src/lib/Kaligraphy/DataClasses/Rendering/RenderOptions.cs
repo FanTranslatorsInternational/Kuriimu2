4cc51acf645933a629966161be7fe9cfb27cd6f2
onepiecefreak3 <realonepiecefreak@gmx.de>
2025-08-10 01:40:47 +0200

    Bump Kaligraphy;

--- src/lib/Kaligraphy/DataClasses/Rendering/RenderOptions.cs
+++ src/lib/Kaligraphy/DataClasses/Rendering/RenderOptions.cs
@@ -6,6 +6,7 @@ namespace Kaligraphy.DataClasses.Rendering
     {
         public bool DrawBoundingBoxes { get; set; }
 
+
         public int VisibleLines { get; set; }
         public int OutlineRadius { get; set; }
 
