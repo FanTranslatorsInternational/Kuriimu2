cf5f5170170c7eb3738cb54dbb1f18a1334c95e4
onepiecefreak3 <realonepiecefreak@gmx.de>
2025-08-31 18:36:11 +0200

    For manual plugin selection provide static lists;

--- src/lib/Konnect.Contract/DataClasses/Management/Files/Events/ManualSelectionEventArgs.cs
+++ src/lib/Konnect.Contract/DataClasses/Management/Files/Events/ManualSelectionEventArgs.cs
@@ -6,8 +6,8 @@ namespace Konnect.Contract.DataClasses.Management.Files.Events
     public class ManualSelectionEventArgs(IFilePlugin[] allFilePlugins, IFilePlugin[] filteredFilePlugins, SelectionStatus status)
         : EventArgs
     {
-        public IEnumerable<IFilePlugin> FilePlugins { get; } = allFilePlugins;
-        public IEnumerable<IFilePlugin> FilteredFilePlugins { get; } = filteredFilePlugins;
+        public IFilePlugin[] FilePlugins { get; } = allFilePlugins;
+        public IFilePlugin[] FilteredFilePlugins { get; } = filteredFilePlugins;
         public SelectionStatus SelectionStatus { get; } = status;
 
         public IFilePlugin? Result { get; set; }
