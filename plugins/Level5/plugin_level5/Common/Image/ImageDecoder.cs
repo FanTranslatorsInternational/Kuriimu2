23e6bef980c4966140aaa967d95451d46e22d64c
onepiecefreak3 <realonepiecefreak@gmx.de>
2026-05-19 23:02:58 +0200

    add encoding 0x1F for Level5 switch textures;

--- plugins/Level5/plugin_level5/Common/Image/ImageDecoder.cs
+++ plugins/Level5/plugin_level5/Common/Image/ImageDecoder.cs
@@ -221,6 +221,7 @@ namespace plugin_level5.Common.Image
             result.AddColorEncoding(0x03, ImageFormats.Rgb888());
             result.AddColorEncoding(0x0E, ImageFormats.A8());
             result.AddColorEncoding(0x1D, ImageFormats.Dxt1());
+            result.AddColorEncoding(0x1F, ImageFormats.Dxt5());
 
             return result;
         }
