59b9d6c7e81167168fc651fe7fb6b8e3644fd3f1
onepiecefreak3 <realonepiecefreak@gmx.de>
2026-06-07 17:55:50 +0200

    Fix fonts not being opened on mobile Level5 games;

--- plugins/Level5/plugin_level5/Common/Image/ImageDecoder.cs
+++ plugins/Level5/plugin_level5/Common/Image/ImageDecoder.cs
@@ -231,6 +231,7 @@ namespace plugin_level5.Common.Image
             var result = new EncodingDefinition();
 
             result.AddColorEncoding(0x03, ImageFormats.Rgb888());
+            result.AddColorEncoding(0x0E, ImageFormats.A8());
 
             return result;
         }
